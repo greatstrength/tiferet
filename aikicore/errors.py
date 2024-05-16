5790bed0c10ebcbc00715954e589f1f4b713ff3b
Ashatz <andrew@greatstrength.me>
2024-05-15 19:40:26 -0700

    Error : Add - App Error

---
+++ aikicore/errors.py
@@ -0,0 +1,7 @@
+class AppError(Exception):
+
+    def __init__(self, error_name: str, lang: str = 'en_US', **format_args):
+        super().__init__(self)
+        self.error_name = error_name
+        self.lang = lang
+        self.format_args = format_args
