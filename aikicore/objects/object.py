3111633ffc5baf5a0d75bf31964d25f7e35ca146
Ashatz <andrew@greatstrength.me>
2024-05-15 19:20:25 -0700

    Core Object : Add - Object : Entity

--- aikicore/objects/object.py
+++ aikicore/objects/object.py
@@ -3,3 +3,6 @@ from schematics import Model, types as t
 class ModelObject(Model):
     name = t.StringType()
     description = t.StringType()
+
+class Entity(ModelObject):
+    id = t.StringType(required=True)
