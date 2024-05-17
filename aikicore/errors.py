95bbe9c7d59a68a2607e960e325973d035297c9f
Ashatz <andrew@greatstrength.me>
2024-05-16 17:57:26 -0700

    Misc - Errors : App Error - Remove Lang Parameter and Update Format Args Parameter

--- aikicore/errors.py
+++ aikicore/errors.py
@@ -1,9 +1,8 @@
 class AppError(Exception):
 
-    def __init__(self, error_name: str, lang: str = 'en_US', **format_args):
+    def __init__(self, error_name: str, *format_args):
         super().__init__(self)
         self.error_name = error_name
-        self.lang = lang
         self.format_args = format_args
 
 
