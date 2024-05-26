8ee7b6cc745ba368f31187e2fd249d2c7259378b
Ashatz <andrew@greatstrength.me>
2024-05-26 10:18:51 -0700

    Data Objects : Add - Feature : Feature Data

--- aikicore/data/feature.py
+++ aikicore/data/feature.py
@@ -8,3 +8,17 @@ class FeatureHandlerData(FeatureHandler, DataObject):
 
     def map(self, role: str = 'to_object', **kwargs):
         return super().map(FeatureHandler, role, **kwargs)
+    
+
+class FeatureData(Feature, DataObject):
+
+    class Options(DefaultOptions):
+        roles = {
+            'to_object.yaml': blacklist('handlers', 'group'),
+            'to_data.yaml': blacklist('group')
+        }
+
+    handlers = t.ListType(t.ModelType(FeatureHandlerData), deserialize_from=['handlers', 'functions'])
+
+    def map(self, role: str = 'to_object.yaml', **kwargs):
+        return super().map(Feature, role, **kwargs)
