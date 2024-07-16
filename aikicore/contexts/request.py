c9fa47cf2b3fee89cff8d838a2610922da57e44d
Ashatz <andrew@greatstrength.me>
2024-07-16 09:44:13 -0700

    Enhanced Request Context

--- aikicore/contexts/request.py
+++ aikicore/contexts/request.py
@@ -1,10 +1,18 @@
 from typing import Dict, Any
 
+from . import app
+
+
 class RequestContext(object):
 
+    feature_id: str = None
     headers: Dict[str, str] = None
     data: Dict[str, Any] = None
+    context: app.AppContext = None
+    result: Any = None
 
-    def __init__(self, headers: Dict[str, str], data: Dict[str, Any]):
+    def __init__(self, feature_id: str, headers: Dict[str, str], data: Dict[str, Any], context: app.AppContext):
+        self.feature_id = feature_id
         self.headers = headers
         self.data = data
+        self.context = context
