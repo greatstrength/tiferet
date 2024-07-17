fbd78c5ce747ec7cddd5ce8b7a2e0cf9f41cf481
Ashatz <andrew@greatstrength.me>
2024-07-16 18:03:02 -0700

    App Context Enhancements

--- aikicore/contexts/app.py
+++ aikicore/contexts/app.py
@@ -1,42 +1,30 @@
-from ..containers import *
-from ..config import *
-from ..errors import *
-from ..objects import *
-from .request import *
+from typing import Any
+
+from ..configs.errors import AppError
+from ..objects.error import Error
+from ..objects.object import ModelObject
+
+from . import request as r
+
 
 class AppContext():
 
     name: str = None
-    interface: str = None
-    container: Container = None
-    config: AppConfiguration = None
     lang: str = None
 
-    def __init__(self, name: str, interface: str, config: AppConfiguration, container: Container, lang: str = 'en_US'):
+    def __init__(self, name: str, lang: str = 'en_US'):
         self.name = name
-        self.interface = interface
         self.lang = lang
-        self.config = config
-        self.container = container
 
-    def get_feature_handler(self): 
-        from importlib import import_module
+    def create_request(self, request: Any, **kwargs) -> r.RequestContext:
+        return r.RequestContext(request)
 
-        # Create feature handler.
-        handler = import_module(
-            DEFAULT_EXECUTE_FEATURE_HANDLER_PATH)
-        setattr(handler, 'feature_cache', self.container.feature_cache())
-        return handler
-    
-    def create_request(self, request: Any, **kwargs) -> RequestContext: 
-        return RequestContext(request)
-    
     def map_response(self, result):
         # Handle list scenario
         if type(result) == list:
             result = []
             for item in result:
-                if isinstance(item, Model):
+                if isinstance(item, ModelObject):
                     result.append(item.to_primitive())
                 else:
                     result.append(item)
@@ -44,15 +32,16 @@ class AppContext():
         if not result:
             return {}
         # Convert schematics models to primitive dicts.
-        if isinstance(result, Model):
+        if isinstance(result, ModelObject):
             return result.to_primitive()
         return result
-    
+
     def handle_error(self, error: AppError, lang: str = 'en_US', error_type: type = Error, **kwargs):
         error_cache = self.container.error_cache()
 
         # Get error.
-        error = error_cache.get(error.error_name, lang=lang, error_type=error_type)
+        error = error_cache.get(
+            error.error_name, lang=lang, error_type=error_type)
 
         return error
 
