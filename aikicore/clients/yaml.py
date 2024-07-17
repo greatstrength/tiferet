de3c67c8b9d6f50a38bdd5e91fae00fb219597bd
Ashatz <andrew@greatstrength.me>
2024-07-16 18:02:44 -0700

    Yaml Client for Now.

--- aikicore/clients/yaml.py
+++ aikicore/clients/yaml.py
@@ -1,10 +1,13 @@
 import yaml
 
-def load(path: str, start_node: function = lambda data: data):
-    with open(path, 'r') as file:
-        data = yaml.safe_load(file)
-        return start_node(data)
-    
-def save(path: str, data: dict):
-    with open(path, 'w') as file:
-        yaml.safe_dump(data, file)
+
+class YamlClient(object):
+
+    def load(path: str, start_node: function = lambda data: data):
+        with open(path, 'r') as file:
+            data = yaml.safe_load(file)
+            return start_node(data)
+
+    def save(path: str, data: dict):
+        with open(path, 'w') as file:
+            yaml.safe_dump(data, file)
