5a8f9f4bdc9c23447012ab1cc6f759bfe68a2b4c
Ashatz <andrew@greatstrength.me>
2024-07-18 11:18:47 -0700

    Yaml Client Enhancements

--- aikicore/clients/yaml.py
+++ aikicore/clients/yaml.py
@@ -1,13 +1,28 @@
+import os
 import yaml
+
+from ..objects.data import DataObject
 
 
 class YamlClient(object):
 
-    def load(path: str, start_node: function = lambda data: data):
-        with open(path, 'r') as file:
+    def __init__(self, base_path: str):
+        if not base_path:
+            base_path = os.getcwd()
+        self.base_path = base_path
+
+    def load(self, start_node: function = lambda data: data):
+        with open(self.base_path, 'r') as file:
             data = yaml.safe_load(file)
             return start_node(data)
 
-    def save(path: str, data: dict):
-        with open(path, 'w') as file:
-            yaml.safe_dump(data, file)
+    def save(self, data: DataObject, start_node: function = lambda data: data):
+        with open(self.base_path, 'r') as file:
+            yaml_data = yaml.safe_load(file)
+
+        replace_node = start_node(yaml_data)
+        replace_node = data.to_primitive('to_data.yaml')
+
+        with open(self.base_path, 'w') as file:
+            yaml.safe_dump(yaml_data, file)
+
