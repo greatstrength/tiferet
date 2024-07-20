adb3e717ea069905244f6b04fdef6daecdc477c4
Ashatz <andrew@greatstrength.me>
2024-07-20 07:15:45 -0700

    Yaml Client is Looking Good

--- aikicore/clients/yaml.py
+++ aikicore/clients/yaml.py
@@ -1,22 +1,22 @@
-import os
+
 import yaml
 
 from ..objects.data import DataObject
 
 
-def load(self, path: str, start_node: function = lambda data: data):
-    with open(self.base_path, 'r') as file:
+def load(path: str, start_node = lambda data: data):
+    with open(path, 'r') as file:
         data = yaml.safe_load(file)
         return start_node(data)
 
 
-def save(self, data: DataObject, start_node: function = lambda data: data):
-    with open(self.base_path, 'r') as file:
+def save(path: str, data: DataObject, start_node = lambda data: data):
+    with open(path, 'r') as file:
         yaml_data = yaml.safe_load(file)
 
     replace_node = start_node(yaml_data)
     replace_node = data.to_primitive('to_data.yaml')
 
-    with open(self.base_path, 'w') as file:
+    with open(path, 'w') as file:
         yaml.safe_dump(yaml_data, file)
 
