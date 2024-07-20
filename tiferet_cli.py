0b64c81beefb6fcc2c58822cb599a0302c176c3d
Ashatz <andrew@greatstrength.me>
2024-07-20 07:15:34 -0700

    Startup Script is Looking Good

--- tiferet_cli.py
+++ tiferet_cli.py
@@ -1,5 +1,6 @@
 from aikicore.contexts.app import AppContext
 from aikicore.contexts.cli import CliInterfaceContext
+from aikicore.contexts.container import ContainerContext
 
 APP_NAME = 'tiferet-console'
 APP_INTERFACE = 'cli'
@@ -8,8 +9,11 @@ APP_LANG = 'en_US'
 def main():
     '''Main entry point for the Tiferet console application.'''
 
+    # Load the application container.
+    container = ContainerContext()
+
     # Create the application context.
-    context = AppContext(APP_NAME,lang=APP_LANG)
+    context = AppContext(APP_NAME, container, lang=APP_LANG)
     
     # Create the CLI interface context.
     cli = CliInterfaceContext(context)
