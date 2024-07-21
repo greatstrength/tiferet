c1625e0f1c70db2f75de45571dcb84273ccba3aa
Ashatz <andrew@greatstrength.me>
2024-07-21 13:24:01 -0700

    Initial Injector Prototype Working

--- aikicore/contexts/container.py
+++ aikicore/contexts/container.py
@@ -1,20 +1,16 @@
+from dependencies import Injector
+
+from ..repositories.cli import YamlRepository
 
 class ContainerContext(object):
 
     def __init__(self):
-        pass
+        self.container = type('Container', (Injector,), {
+            'cli_interface_repo': YamlRepository,
+            'base_path': 'app.yml',
+        })
 
-    def cli_interface_repo(self, flag: str = 'yaml', base_path: str = None):
-        if flag in ['yaml', 'yml']:
-            from ..repositories.cli import YamlRepository
-            return YamlRepository(base_path)
+    def get_service(self, service: str):
+        return getattr(self.container, service)
 
-    def error_cache(self, flag: str = 'yaml'):
-        from ..repositories.error import ErrorCache
-        if flag in ['yaml', 'yml']:
-            return ErrorCache(self.yaml_client(), self.config.error_cache_path, mapper_role='to_object.yaml')
-
-    def feature_cache(self, flag: str = 'yaml'):
-        from ..repositories.feature import FeatureCache
-        if flag in ['yaml', 'yml']:
-            return FeatureCache(self.yaml_client(), self.config.feature_cache_path, mapper_role='to_object.yaml')
+    
