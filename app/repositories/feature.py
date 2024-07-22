fb7942339e718edfbf597e31e8f15b1ac90333ca
Ashatz <andrew@greatstrength.me>
2024-07-21 17:49:10 -0700

    Create Feature Repository

--- app/repositories/feature.py
+++ app/repositories/feature.py
@@ -4,6 +4,12 @@ from ..data import *
 DEFAULT_MAPPER_ROLE = 'to_object'
 
 
+class FeatureRepository(object):
+
+    def get(self, id: str) -> Feature:
+        raise NotImplementedError()
+
+
 class FeatureCache():
 
     cache: Dict[str, FeatureGroupData] = {}
@@ -11,7 +17,8 @@ class FeatureCache():
     def __init__(self, client, cache_path: str, mapper_role: str = DEFAULT_MAPPER_ROLE):
         self.client = client
         self.mapper_role = mapper_role
-        data = self.client.load(cache_path, lambda data: data['features']['groups'])
+        data = self.client.load(
+            cache_path, lambda data: data['features']['groups'])
         for group_name, group_data in data.items():
             self.cache[group_name] = FeatureGroupData(
                 dict(group_name=group_name, **group_data))
@@ -22,5 +29,6 @@ class FeatureCache():
         feature_data: FeatureData = group_data.features.get(feature_name)
         group = group_data.map(name=group_name)
         handlers = [handler.map() for handler in feature_data.handlers]
-        feature = feature_data.map(name=feature_name, group=group, handlers=handlers, role=self.mapper_role)
+        feature = feature_data.map(
+            name=feature_name, group=group, handlers=handlers, role=self.mapper_role)
         return feature
