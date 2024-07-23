d77b0247392705e425fb30247da03641605a052d
Ashatz <andrew@greatstrength.me>
2024-07-23 16:00:21 -0700

    Tiferet CLI - Add Environment Variable Retrieval to Main

--- tiferet_cli.py
+++ tiferet_cli.py
@@ -1,6 +1,7 @@
 from app.contexts.app import AppContext
 from app.contexts.cli import CliInterfaceContext
 from app.contexts.container import ContainerContext
+from app.services import app as app_service
 
 APP_NAME = 'tiferet-cli'
 APP_INTERFACE = 'cli'
@@ -12,8 +13,11 @@ APP_DEPENDENCY_FLAG = 'yaml'
 def main():
     '''Main entry point for the Tiferet console application.'''
 
+    # Load environment variables.
+    env = app_service.load_environment_variables(APP_ENV_BASE_KEY)
+
     # Load the application container.
-    container = ContainerContext(APP_DEPENDENCY_FLAG)
+    container = ContainerContext(APP_DEPENDENCY_FLAG, **env.get('container'))
 
     # Create the application context.
     context = AppContext(
