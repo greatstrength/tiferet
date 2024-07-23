6fdeda757b70b93a55f152fc44e983ba2864f7b0
Ashatz <andrew@greatstrength.me>
2024-07-23 15:59:31 -0700

    Container Context - Fix Bugs and Add Enhancements.

--- app/contexts/container.py
+++ app/contexts/container.py
@@ -1,6 +1,5 @@
 from typing import Any, List, Dict
 
-from ..services import container as container_service
 from ..repositories.container import ContainerRepository
 from ..objects.container import ContainerAttribute
 
@@ -34,12 +33,12 @@ class ContainerContext(object):
             except:
                 setattr(self, dependency, dependencies.get(dependency))
 
-    def load_container_repository(self, module_path: str, class_name: str, **kwargs):
+    def load_container_repository(self, container_repo_module_path: str, container_repo_class_name: str, **kwargs):
 
         # Load container repository.
-        return self.import_dependency(module_path, class_name)(**kwargs)
+        return self.import_dependency(container_repo_module_path, container_repo_class_name)(**kwargs)
 
-    def import_dependency(module_path: str, class_name: str):
+    def import_dependency(self, module_path: str, class_name: str, **kwargs) -> Any:
         from importlib import import_module
 
         return getattr(import_module(module_path), class_name)
