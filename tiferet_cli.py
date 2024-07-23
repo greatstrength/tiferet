e4fa18c667fb119614ddc0e5bc806e8fd01ac34e
Ashatz <andrew@greatstrength.me>
2024-07-23 14:26:07 -0700

    Tiferet CLI - Update App Context Initialization

--- tiferet_cli.py
+++ tiferet_cli.py
@@ -2,10 +2,12 @@ from app.contexts.app import AppContext
 from app.contexts.cli import CliInterfaceContext
 from app.contexts.container import ContainerContext
 
-APP_NAME = 'tiferet-console'
+APP_NAME = 'tiferet-cli'
 APP_INTERFACE = 'cli'
 APP_LANG = 'en_US'
+APP_ENV_BASE_KEY = 'TIFERET'
 APP_DEPENDENCY_FLAG = 'yaml'
+
 
 def main():
     '''Main entry point for the Tiferet console application.'''
@@ -14,13 +16,19 @@ def main():
     container = ContainerContext(APP_DEPENDENCY_FLAG)
 
     # Create the application context.
-    context = AppContext(APP_NAME, container, lang=APP_LANG)
-    
+    context = AppContext(
+        name=APP_NAME, 
+        container=container,
+        env_base_key=APP_ENV_BASE_KEY, 
+        lang=APP_LANG
+    )
+
     # Create the CLI interface context.
     cli = CliInterfaceContext(context)
 
     # Run the CLI interface.
     cli.run(interface='cli')
+
 
 if __name__ == '__main__':
     main()
