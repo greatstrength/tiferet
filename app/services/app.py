cb3099ed83260dc130965220268efc74015666e2
Ashatz <andrew@greatstrength.me>
2024-07-23 15:59:54 -0700

    App Service - Load Environment Variables Updates

--- app/services/app.py
+++ app/services/app.py
@@ -4,3 +4,34 @@ from ..contexts.request import RequestContext
 
 def create_request(self, request: Any, **kwargs) -> RequestContext:
     return RequestContext(request)
+
+
+def load_environment_variables(env_base_key: str) -> Any:
+    
+    # Import os module.
+    import os
+
+    # Create result dictionary.
+    result = {}
+
+    # Iterate over environment variables.
+    for key, value in os.environ.items():
+
+        # Check if key is a valid environment variable.
+        try:
+            app, group, variable = key.split('__')
+        except:
+            continue
+
+        # Check if key is a valid environment variable.
+        if app != env_base_key:
+            continue
+
+        # Add environment variable to result.
+        group = group.lower()
+        if group not in result:
+            result[group] = {}
+        result[group][variable.lower()] = value
+
+    # Return result.
+    return result
