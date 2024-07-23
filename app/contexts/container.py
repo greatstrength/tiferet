6d6f32e511bbe4a6fa02e3e38dd6b5feadf36ee9
Ashatz <andrew@greatstrength.me>
2024-07-23 14:47:30 -0700

    Container Context - Add Container Repository and Cleanup

--- app/contexts/container.py
+++ app/contexts/container.py
@@ -1,14 +1,40 @@
+from typing import Any, Dict
+
 from ..services import container as container_service
+from ..repositories.container import ContainerRepository
+
 
 class ContainerContext(object):
 
+    container_repo: ContainerRepository = None
+
     def __init__(self, flag: str, **kwargs):
+
+        # Load container repository.
+        self.load_container_repository(**kwargs)
+
+        # Load container dependencies.
         dependencies = container_service.load_dependencies(flag)
-        self.container = container_service.create_container(dependencies)
+
+        # Create container.
+        container = self.create_container(dependencies)
+
+        # Load container dependencies.
         for dependency in dependencies:
             try:
-                setattr(self, dependency, getattr(self.container, dependency))
+                setattr(self, dependency, getattr(container, dependency))
             except:
                 setattr(self, dependency, dependencies.get(dependency))
 
-    
+    def load_container_repository(self, module_path: str, class_name: str, **kwargs):
+
+        # Load container repository.
+        self.container_repo = container_service.import_dependency(module_path, class_name)(**kwargs)
+
+    def create_container(self, dependencies: Dict[str, Any]):
+        
+        # Import dependencies.
+        from dependencies import Injector
+
+        # Create container.
+        return type('Container', (Injector,), dependencies)
