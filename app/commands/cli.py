dfad6a45bdf9d05207f04d246ee72a2f94f7f904
Ashatz <andrew@greatstrength.me>
2024-07-22 17:10:54 -0700

    Add CLI Command - Initial Full Command

--- app/commands/cli.py
+++ app/commands/cli.py
@@ -1,9 +1,35 @@
+from ..repositories.cli import CliInterfaceRepository
+from ..objects.cli import CliInterface
+from ..objects.cli import CliCommand
 
 
 class AddCliCommand(object):
 
-    def __init__(self):
+    def __init__(self, cli_interface_repo: CliInterfaceRepository):
+        self.cli_interface_repo = cli_interface_repo
         pass
 
-    def execute(self, feature_id: str, name: str, help: str, **kwargs):
-        pass
+    def execute(self, interface_id: str, feature_id: str, name: str, group_id: str, help: str, **kwargs):
+
+        # Get CLI interface using the interface ID.
+        cli_interface: CliInterface = self.cli_interface_repo.get(interface_id)
+
+        # Assert that the CLI interface exists.
+        assert cli_interface is not None, f'CLI_INTERFACE_NOT_FOUND: {interface_id}'
+
+        # Assert that the feature does not already exist.
+        assert cli_interface.command_exists(
+            feature_id), f'FEATURE_ALREADY_EXISTS: {feature_id}'
+
+        # Create the new CLI command.
+        command = CliCommand.new(
+            id=feature_id, name=name, feature_id=feature_id, group_id=group_id, help=help)
+        
+        # Add the command to the CLI interface.
+        cli_interface.add_command(command)
+
+        # Save the CLI interface.
+        self.cli_interface_repo.save(cli_interface)
+
+        # Return the new command.
+        return command
