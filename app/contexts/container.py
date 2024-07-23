62532c3996fb107d114dea5bf4e6471bec842ef6
Ashatz <andrew@greatstrength.me>
2024-07-23 15:25:54 -0700

    Container Context - Add More Template Methods

--- app/contexts/container.py
+++ app/contexts/container.py
@@ -1,7 +1,8 @@
-from typing import Any, Dict
+from typing import Any, List, Dict
 
 from ..services import container as container_service
 from ..repositories.container import ContainerRepository
+from ..objects.container import ContainerAttribute
 
 
 class ContainerContext(object):
@@ -11,10 +12,17 @@ class ContainerContext(object):
     def __init__(self, flag: str, **kwargs):
 
         # Load container repository.
-        self.load_container_repository(**kwargs)
+        container_repo: ContainerRepository = self.load_container_repository(
+            **kwargs)
+
+        # Load container attributes.
+        attributes = container_repo.list_attributes()
 
         # Load container dependencies.
-        dependencies = container_service.load_dependencies(flag)
+        dependencies = self.load_dependencies(
+            attributes=attributes,
+            flag=flag
+        )
 
         # Create container.
         container = self.create_container(dependencies)
@@ -29,10 +37,30 @@ class ContainerContext(object):
     def load_container_repository(self, module_path: str, class_name: str, **kwargs):
 
         # Load container repository.
-        self.container_repo = container_service.import_dependency(module_path, class_name)(**kwargs)
+        return self.import_dependency(module_path, class_name)(**kwargs)
+
+    def import_dependency(module_path: str, class_name: str):
+        from importlib import import_module
+
+        return getattr(import_module(module_path), class_name)
+
+    def load_dependencies(self, attributes: List[ContainerAttribute], flag: str, **kwargs) -> Dict[str, Any]:
+        # Create result dictionary.
+        result = {}
+
+        # Load container configuration data.
+        for attribute in attributes:
+            if attribute.type == 'dependency':
+                result[attribute.id] = self.import_dependency(
+                    **attribute.get_data_value(flag))
+            elif attribute.type == 'attribute':
+                result[attribute.id] = attribute.get_data_value(flag).value
+
+        # Return result.
+        return result
 
     def create_container(self, dependencies: Dict[str, Any]):
-        
+
         # Import dependencies.
         from dependencies import Injector
 
