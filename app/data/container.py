ac4931fc356c6bfade4446197d1acce3a8432f12
Ashatz <andrew@greatstrength.me>
2024-07-25 19:56:06 -0700

    Container Attribute Data - Update Map Method to Include Flag Parameter

--- app/data/container.py
+++ app/data/container.py
@@ -3,18 +3,30 @@ from schematics.transforms import wholelist, whitelist, blacklist
 
 from ..objects.data import DataObject
 from ..objects.container import ContainerAttribute
+from ..objects.container import AttributeValue
+from ..objects.container import DependencyValue
+from ..objects.container import CONTAINER_ATTRIBUTE_TYPE_ATTRIBUTE
+from ..objects.container import CONTAINER_ATTRIBUTE_TYPE_DEPENDENCY
 
 
 class ContainerAttributeData(ContainerAttribute, DataObject):
 
     class Options():
         roles = {
-            'to_object.yaml': wholelist(),
+            'to_object.yaml': blacklist('data'),
             'to_data.yaml': wholelist()
         }
+        serialize_when_none = False
 
-    def map(self, role: str, **kwargs):
-        return super().map(ContainerAttribute, role, **kwargs)
+    data = t.DictType(t.DictType(t.StringType), default={}, required=True)
+
+    def map(self, role: str, flag: str, **kwargs):
+        data = self.data[flag]
+        if self.type == CONTAINER_ATTRIBUTE_TYPE_ATTRIBUTE:
+            data = AttributeValue(data)
+        elif self.type == CONTAINER_ATTRIBUTE_TYPE_DEPENDENCY:
+            data = DependencyValue(data)
+        return super().map(ContainerAttribute, role, data=data, **kwargs)
 
     @staticmethod
     def new(id: str, type: str, data: dict):
