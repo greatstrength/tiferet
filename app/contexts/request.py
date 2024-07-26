90e04be17252f76e19d5e3fe2a4c8c3635b03026
Ashatz <andrew@greatstrength.me>
2024-07-25 23:33:50 -0700

    Request Context - Remove App Context

--- app/contexts/request.py
+++ app/contexts/request.py
@@ -1,18 +1,14 @@
 from typing import Dict, Any
 
-from . import app
-
 
 class RequestContext(object):
 
     feature_id: str = None
     headers: Dict[str, str] = None
     data: Dict[str, Any] = None
-    context: app.AppContext = None
     result: Any = None
 
-    def __init__(self, feature_id: str, headers: Dict[str, str], data: Dict[str, Any], context: app.AppContext, **kwargs):
+    def __init__(self, feature_id: str, headers: Dict[str, str], data: Dict[str, Any], **kwargs):
         self.feature_id = feature_id
         self.headers = headers
         self.data = data
-        self.context = context
