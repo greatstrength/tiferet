4a7c776114fed401ffb7d544be08bad4bdb93a25
Ashatz <andrew@greatstrength.me>
2024-07-25 20:45:45 -0700

    Container Service - Minor Touch-Ups

--- app/services/container.py
+++ app/services/container.py
@@ -8,13 +8,13 @@ from ..objects.container import CONTAINER_ATTRIBUTE_TYPE_ATTRIBUTE
 from ..objects.container import CONTAINER_ATTRIBUTE_TYPE_DEPENDENCY
 
 
-def import_dependency(self, module_path: str, class_name: str):
+def import_dependency(module_path: str, class_name: str):
 
     # Import module.
     return getattr(import_module(module_path), class_name)
 
 
-def create_container(self, attributes: List[ContainerAttribute]):
+def create_container(attributes: List[ContainerAttribute]):
 
     # Create depenedencies dictionary.
     dependencies = {}
@@ -22,7 +22,7 @@ def create_container(self, attributes: List[ContainerAttribute]):
     # Load container configuration data.
     for attribute in attributes:
         if attribute.type == CONTAINER_ATTRIBUTE_TYPE_DEPENDENCY:
-            dependencies[attribute.id] = self.import_dependency(
+            dependencies[attribute.id] = import_dependency(
                 **attribute.data.to_primitive())
         elif attribute.type == CONTAINER_ATTRIBUTE_TYPE_ATTRIBUTE:
             dependencies[attribute.id] = attribute.data.value
