f1568011346e9d81248937661e908c7a2bb410a3
Ashatz <andrew@greatstrength.me>
2024-07-26 11:42:44 -0700

    Initializer Script - Finalize This Now

--- tiferet_cli.py
+++ tiferet_cli.py
@@ -1,42 +1,26 @@
-from app.contexts.app import AppContext
+from typing import Dict, Any
+
 from app.contexts.cli import CliInterfaceContext
-from app.contexts.container import ContainerContext
-from app.contexts.feature import FeatureContext
+from app.containers.app import AppContainer
 from app.services import app as app_service
 
-APP_NAME = 'tiferet-cli'
-APP_INTERFACE = 'cli'
-APP_LANG = 'en_US'
-APP_ENV_BASE_KEY = 'TIFERET'
-APP_DEPENDENCY_FLAG = 'yaml'
 
+APP_ENV_BASE_KEY = 'TIFERET'
 
 def main():
     '''Main entry point for the Tiferet console application.'''
 
     # Load environment variables.
-    env = app_service.load_environment_variables(APP_ENV_BASE_KEY)
+    env: Dict[str, Any] = app_service.load_environment_variables(APP_ENV_BASE_KEY)
 
     # Load the application container.
-    container = app_service.create_app_container(env)
-
-    # Create the application context.
-    context = AppContext(
-        name=APP_NAME, 
-        container=container,
-        interface=APP_INTERFACE,
-        env_base_key=APP_ENV_BASE_KEY, 
-        lang=APP_LANG
-    )
-
-    # Add the Feature Context to the application context.
-    context.features = FeatureContext(container)
+    container: AppContainer = app_service.create_app_container(env)
 
     # Create the CLI interface context.
-    cli = CliInterfaceContext(context)
+    cli_interface_context: CliInterfaceContext = container.cli_interface_context
 
     # Run the CLI interface.
-    cli.run()
+    cli_interface_context.run()
 
 
 if __name__ == '__main__':
