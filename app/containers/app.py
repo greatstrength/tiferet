00866d0385c95d1e6cb198a0631f2887832e6809
Ashatz <andrew@greatstrength.me>
2024-07-25 19:28:58 -0700

    App Container - Initiation Phase

--- app/containers/app.py
+++ app/containers/app.py
@@ -3,11 +3,22 @@ from typing import List
 from dependencies import Injector
 
 from ..objects.container import ContainerAttribute
+from ..repositories.container import ContainerRepository
 
 
 class AppContainer(object):
 
-    container: Injector = None
+    def __init__(self, container_repo_module_path: str, container_repo_class_name: str, **kwargs):
+        
+        # Load container repository.
+        container_repo: ContainerRepository = self.load_container_repository(
+            container_repo_module_path, container_repo_class_name, **kwargs)
+        
+        # Get container attributes.
+        attributes: List[ContainerAttribute] = container_repo.list_attributes()
 
-    def __init__(self, attributes: List[ContainerAttribute]):
-        pass
+        # Create container.
+        container = self.create_container(attributes)
+
+        # Load container dependencies.
+        self.set_attributes(attributes)
