885845dd994dc0030b46126ef7b657de76307d13
Ashatz <andrew@greatstrength.me>
2024-07-26 19:24:07 -0700

    App Container - Final Touch-ups For Now

--- app/containers/app.py
+++ app/containers/app.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Any
 
 from ..objects.container import ContainerAttribute
 from ..objects.container import CONTAINER_ATTRIBUTE_TYPE_ATTRIBUTE as ATTRIBUTE
@@ -9,33 +9,44 @@ from ..services import container as container_service
 
 class AppContainer(object):
 
-    def __init__(self, container_repo_module_path: str, container_repo_class_name: str, app_container_flag: str, **kwargs):
+    def __init__(self, env_variables: Dict[str, Any]):
 
-        # Load container repository.
-        container_repo: ContainerRepository = self.load_container_repository(
-            container_repo_module_path, container_repo_class_name, **kwargs)
-
-        # Get container attributes.
-        attributes: List[ContainerAttribute] = container_repo.list_attributes(
-            'app', app_container_flag)
+        # Load container repository and list attributes.
+        attributes = self.list_attributes('app', **env_variables)
 
         # Create container.
         container = self.create_container(attributes)
 
         # Load container dependencies.
-        self.set_attributes(attributes, container)
+        self.set_attributes(attributes, container, app_variables=env_variables.get('app', {}))
 
-    def load_container_repository(self, container_repo_module_path: str, container_repo_class_name: str, **kwargs):
+    def load_container_repository(self, module_path: str, class_name: str, **kwargs):
 
         # Load container repository.
-        return container_service.import_dependency(container_repo_module_path, container_repo_class_name)(**kwargs)
+        return container_service.import_dependency(module_path, class_name)(**kwargs)
+    
+    def list_attributes(self, container_type: str, **kwargs):
 
-    def create_container(self, attributes: List[ContainerAttribute]):
+        # Load container repository.
+        container_repo: ContainerRepository = self.load_container_repository(**kwargs.get('container_repo', {}))
+
+        # Get container attributes.
+        attributes: List[ContainerAttribute] = container_repo.list_attributes(container_type, **kwargs.get('container', {}))
+    
+        # Add app variables as attributes.
+        for key, value in kwargs.get('app').items():
+            attributes.append(ContainerAttribute.new(id=key, type=ATTRIBUTE, data={'value': value}))
+
+        # Return attributes.
+        return attributes
+    
+
+    def create_container(self, attributes: List[ContainerAttribute], **kwargs):
 
         # Create container.
         return container_service.create_container(attributes, app_container=self)
 
-    def set_attributes(self, attributes: List[ContainerAttribute], container):
+    def set_attributes(self, attributes: List[ContainerAttribute], container, app_variables: Dict[str, str], **kwargs):
 
         # Load container dependencies.
         for attribute in attributes:
@@ -46,3 +57,7 @@ class AppContainer(object):
             # Set attribute.
             elif attribute.type == ATTRIBUTE:
                 setattr(self, attribute.id, attribute.data.value)
+        
+        # Load app variables as attributes.
+        for key, value in app_variables.items():
+            setattr(self, key, value)
