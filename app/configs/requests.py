59ccd0af6fcd55757590ff01f0264b19898e0b8f
Ashatz <andrew@greatstrength.me>
2024-08-02 11:41:34 -0700

    Add CLI Command - Feature Request Data

--- app/configs/requests.py
+++ app/configs/requests.py
@@ -1,6 +1,11 @@
 from schematics import types as t
 from schematics import Model
 
+CLI_ARGUMENT_TYPES = ['command', 'parent_argument']
+CLI_ARGUMENT_TYPE_DEFAULT = 'command'
+CLI_ARGUMENT_DATA_TYPES = ['str', 'int', 'float']
+CLI_ARGUMENT_DATA_TYPE_DEFAULT = 'str'
+
 class AddCliCommand(Model):
     
     interface_id = t.StringType(required=True)
@@ -8,3 +13,20 @@ class AddCliCommand(Model):
     command_key = t.StringType(required=True)
     name = t.StringType(required=True)
     help = t.StringType(required=True, deserialize_from=['help', 'description'])
+
+
+class AddCliArgument(Model):
+
+    name = t.StringType(required=True)
+    interface_id = t.StringType(required=True)
+    help = t.StringType(required=True, deserialize_from=['help', 'description'])
+    type = t.StringType(default=CLI_ARGUMENT_TYPE_DEFAULT, choices=CLI_ARGUMENT_TYPES)
+    feature_id = t.StringType()
+    flags = t.ListType(t.StringType(), default=[])
+    data_type = t.StringType(default=CLI_ARGUMENT_DATA_TYPE_DEFAULT, choices=CLI_ARGUMENT_DATA_TYPES)
+    required = t.BooleanType()
+    default = t.StringType()
+    positional = t.BooleanType()
+    choices = t.ListType(t.StringType(), default=[])
+    nargs = t.IntType()
+    action = t.StringType()
