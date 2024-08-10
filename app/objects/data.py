613aefad0324a1b446ac5d322584b36cc845e46d
Ashatz <andrew@greatstrength.me>
2024-08-10 01:12:15 -0700

    Data Objects - Configurations, Documentation and Reformatting

--- app/objects/data.py
+++ app/objects/data.py
@@ -3,22 +3,46 @@ from schematics.types.serializable import serializable
 from schematics.transforms import wholelist, whitelist, blacklist
 
 
-class DefaultOptions():
-    serialize_when_none = False
-    roles = {
-        'to_object': wholelist(),
-        'to_data': wholelist()
-    }
-
-
 class ModelData(Model):
+    '''
+    A data representation of a model object.
+    '''
 
-    def map(self, type: type, role: str = 'to_object', **kwargs):
-        return type(dict(
+    def map(self, type: type, role: str = 'to_object', **kwargs) -> 'ModelData':
+        '''
+        Maps the model data to a model object.
+
+        :param type: The type of model object to map to.
+        :type type: type
+        :param role: The role for the mapping.
+        :type role: str
+        :param kwargs: Additional keyword arguments.
+        :type kwargs: dict
+        :return: A new model object.
+        :rtype: ModelData
+        '''
+
+        # Map the model data to a model data.
+        _data = type(dict(
             **kwargs,
             **self.to_primitive(role=role)
         ), strict=False)
-    
+
+        # Validate and return the model data.
+        _data.validate()
+        return _data
+
     @staticmethod
-    def new(data: dict, **kwargs):
+    def new(data: dict, **kwargs) -> 'ModelData':
+        '''
+        Initializes a new ModelData object.
+
+        :param data: The data for the model object.
+        :type data: dict
+        :param kwargs: Additional keyword arguments.
+        :type kwargs: dict
+        :return: A new ModelData object.
+        :rtype: ModelData
+        '''
+
         return ModelData(data, **kwargs)
