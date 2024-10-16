8ea1d4ad6bc154393146fc032dc058e11987edcb
Ashatz <andrew@greatstrength.me>
2024-10-15 23:07:48 -0700

    Initializer - Update Function - Main

--- tiferet_cli.py
+++ tiferet_cli.py
@@ -1,8 +1,6 @@
 from typing import Dict, Any
 
-from app.contexts.cli import CliInterfaceContext
-from app.containers.app import AppContainer
-from app.services import app as app_service
+from app.contexts import EnvironmentContext
 
 # Define the base key for the application environment variables.
 APP_ENV_BASE_KEY = 'TIFERET'
@@ -11,18 +9,9 @@ APP_ENV_BASE_KEY = 'TIFERET'
 def main():
     '''Main entry point for the Tiferet console application.'''
 
-    # Load environment variables.
-    env: Dict[str, Any] = app_service.load_environment_variables(
-        APP_ENV_BASE_KEY)
-
-    # Load the application container.
-    container: AppContainer = app_service.create_app_container(env)
-
-    # Create the CLI interface context.
-    cli_interface_context: CliInterfaceContext = container.cli_interface_context
-
-    # Run the CLI interface.
-    cli_interface_context.run(container=container)
+    # Load the environment context.
+    EnvironmentContext(
+        env_base_key=APP_ENV_BASE_KEY)
 
 
 if __name__ == '__main__':
