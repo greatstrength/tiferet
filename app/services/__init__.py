598ee49cd78bb2225661e4f10301fa8009fc8371
Ashatz <andrew@greatstrength.me>
2024-10-18 19:16:24 -0700

    Tech Debt - Fix Bug

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,4 +1,4 @@
-import cli as cli_service
-import container as container_service
-import object as object_service
-import sync as sync_service
+from . import cli as cli_service
+from . import container as container_service
+from . import object as object_service
+from . import sync as sync_service
