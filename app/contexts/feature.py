b52e8ef04e0a726e4ad7a1ab8c1314178c07d95b
Ashatz <andrew@greatstrength.me>
2024-11-01 01:29:45 -0700

    Feature Context Tune Ups

--- app/contexts/feature.py
+++ app/contexts/feature.py
@@ -1,33 +1,37 @@
-from typing import Any, Dict, List
+# *** imports
 
+# ** core
+from typing import Dict
+
+# ** app
+from .container import ContainerContext
+from .request import RequestContext
 from ..objects.feature import Feature
 from ..repositories.feature import FeatureRepository
-from ..configs.errors import InvalidRequestData
-from ..contexts.container import ContainerContext
-
-from .request import RequestContext
 
 
+# *** contexts
+
+# ** context: feature_context
 class FeatureContext(object):
 
-    feature_repo: FeatureRepository
+    # * field: features
+    features: Dict[str, Feature] = None 
 
-    class SessionContext(object):
+    # * field: container
+    container: ContainerContext = None
 
-        data: Dict[str, Any] = {}
-        result: Any = None
-        error: str = None
-
-        def __init__(self, session_id: str):
-            self.session_id = session_id
-
+    # * method: init
     def __init__(self, feature_repo: FeatureRepository, container: ContainerContext):
 
-        # Set the feature repository and container.
-        self.feature_repo = feature_repo
-        self.container = container
+        # Set the features.
+        self.features = {feature.id: feature for feature in feature_repo.list()}
 
-    def execute(self, request: RequestContext, debug: bool = False, **kwargs) -> SessionContext:
+        # Set the container context.
+        self.container = container
+        
+    # * method: execute
+    def execute(self, request: RequestContext, debug: bool = False, **kwargs):
         '''
         Execute the feature request.
         
@@ -37,90 +41,31 @@ class FeatureContext(object):
         :type debug: bool
         :param kwargs: Additional keyword arguments.
         :type kwargs: dict
-        :return: The result of the feature execution.
-        :rtype: Any
         '''
 
-        # Create a session context object.
-        session = self.SessionContext(request.headers.get('session_id'))
+        # Iterate over the feature commands.
+        for command in self.features[request.feature_id].commands:
 
-        # Get feature from cache.
-        feature: Feature = self.feature_repo.get(request.feature_id)
-
-        # Validate feature request data.
-        self.validate_request_data(request, feature)
-
-        for handler in feature.handlers:
-
-            # Get the command handler function.
-            command = self.get_command_handler(
-                attribute_id=handler.attribute_id
-            )
+            # Get the feature command handler instance.
+            handler = self.container.get_dependency(command.attribute_id)
 
             # Execute the handler function.
+            # Handle assertion errors if pass on error is not set.
             try:
-                result = command.execute(
+                result = handler.execute(
                     **request.data,
-                    **handler.params,
-                    **session.data,
+                    **command.params,
                     **kwargs
                 )
-            # Handle assertion errors.
             except AssertionError as e:
-
-                # Set the error message.
-                session.error = str(e)
-
-                # Break if exit on error is set.
-                if not handler.pass_on_error:
-                    break
-
-                # Continue to the next handler if continue on error is set.
-                continue
+                if not command.pass_on_error:
+                    raise e 
 
             # Return the result to the session context if return to data is set.
-            if handler.return_to_data:
-                session.data[handler.data_key] = result
+            if command.return_to_data:
+                request.data[command.data_key] = result
                 continue
 
             # Return the result to the session context if return to result is set.
             if result:
-                session.result = result
-
-        return session
-
-    def validate_request_data(self, request: RequestContext, feature: Feature):
-
-        # Load the request model validation object.
-        request_model = self.import_request(feature.request_type_path)
-
-        # Create the request object.
-        request_obj = request_model(request.data)
-
-        # Validate the request object.
-        try:
-            request_obj.validate()
-
-            # Set the request data if validation passes.
-            request.data = request_obj.to_primitive()
-
-        # Raise an invalid request data error if validation fails.
-        except Exception as e:
-            raise InvalidRequestData(e.messages)
-
-    def get_command_handler(self, attribute_id: str) -> Any:
-
-        # Return the handler function using Feature Container Attribute ID.
-        return self.container.get_dependency('feature', attribute_id)
-
-    def import_request(self, request_path: str):
-
-        # Split the request path into module and class name.
-        request_path, class_name = request_path.rsplit('.', 1)
-
-        # Import the module.
-        from importlib import import_module
-        module = import_module(request_path)
-
-        # Return the request class.
-        return getattr(module, class_name)
+                request.result = result
