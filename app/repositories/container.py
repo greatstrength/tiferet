85316cc2597ab8378e1c2b57bce8a1b6cc749c6e
Ashatz <andrew@greatstrength.me>
2024-11-04 01:34:48 -0700

    Repos - Container - Interface and Yaml Proxy Touch-ups.

--- app/repositories/container.py
+++ app/repositories/container.py
@@ -1,15 +1,29 @@
+# *** imports
+
+# ** core
 from typing import List
 
-from ..objects.container import ContainerAttribute
-from ..data.container import ContainerAttributeData
+# ** app
+from ..domain.container import ContainerAttribute
+from ..data.container import ContainerAttributeYamlData
 from ..clients import yaml as yaml_client
 
 
+# *** repositories
+
+## * repository: container_repository
 class ContainerRepository(object):
     '''
     Container repository interface.
     '''
 
+    # * field: role
+    read_role: str = None
+
+    # * field: write_role
+    write_role: str = None
+
+    # * method: attribute_exists
     def attribute_exists(self, group_id: str, id: str, **kwargs) -> bool:
         '''
         Verifies if the container attribute exists.
@@ -26,40 +40,18 @@ class ContainerRepository(object):
 
         raise NotImplementedError()
 
-    def list_attributes(self, container_type: str, flags: List[str], **kwargs) -> List[ContainerAttribute]:
+    # * method: list_attributes
+    def list_attributes(self) -> List[ContainerAttribute]:
         '''
         List the container attributes.
 
-        :param container_type: The container type.
-        :type container_type: str
-        :param flags: The infrastructure flags to get the attributes under.
-        :type flags: List[str]
-        :param kwargs: Additional keyword arguments.
-        :type kwargs: dict
         :return: The list of attributes.
         :rtype: List[ContainerAttribute]
         '''
 
         raise NotImplementedError()
 
-    def get_attribute(self, group_id: str, id: str, flag: str, **kwargs) -> ContainerAttribute:
-        '''
-        Get the container attribute.
-
-        :param group_id: The group id.
-        :type group_id: str
-        :param id: The attribute id.
-        :type id: str
-        :param flag: The infrastructure flag to get the attribute under.
-        :type flag: str
-        :param kwargs: Additional keyword arguments.
-        :type kwargs: dict
-        :return: The container attribute.
-        :rtype: ContainerAttribute
-        '''
-
-        raise NotImplementedError()
-
+    # * method: save_attribute
     def save_attribute(self, group_id: str, attribute: ContainerAttribute, flag: str, **kwargs):
         '''
         Save the container attribute.
@@ -77,22 +69,36 @@ class ContainerRepository(object):
         raise NotImplementedError
 
 
-class YamlRepository(ContainerRepository):
+# ** proxy: yaml_repository
+class YamlProxy(ContainerRepository):
     '''
-    Yaml repository for container attributes.
+    Yaml proxy for container attributes.
     '''
 
-    def __init__(self, container_yaml_base_path: str):
+    # * init
+    def __init__(self, container_config_file: str, read_role: str = 'to_object.yaml', write_role: str = 'to_data.yaml'):
         '''
-        Initialize the yaml repository.
+        Initialize the yaml proxy.
         
-        :param container_yaml_base_path: The base path to the yaml file.
-        :type container_yaml_base_path: str
+        :param container_config_file: The YAML file path for the container configuration.
+        :type container_config_file: str
+        :param read_role: The read role for the yaml proxy.
+        :type read_role: str
+        :param write_role: The write role for the yaml proxy.
+        :type write_role: str
         '''
 
-        self.base_path = container_yaml_base_path
+        # Set the container configuration file.
+        self.config_file = container_config_file
 
-    def attribute_exists(self, group_id: str, id: str, **kwargs) -> bool:
+        # Set the read role.
+        self.read_role = read_role
+
+        # Set the write role.
+        self.write_role = write_role
+
+    # * method: attribute_exists
+    def attribute_exists(self, id: str, **kwargs) -> bool:
         '''
         Verifies if the container attribute exists within the yaml file.
         
@@ -106,97 +112,46 @@ class YamlRepository(ContainerRepository):
 
         # Load the attribute data from the yaml configuration file.
         data = yaml_client.load(
-            self.base_path,
-            start_node=lambda data: data.get('container').get('attrs').get(group_id))
+            self.config_file,
+            start_node=lambda data: data.get('attrs'))
 
         # Return whether the attribute exists.
         return any([attribute_id == id for attribute_id in data])
 
-    def list_attributes(self, group_id: str, flags: List[str], **kwargs) -> List[ContainerAttribute]:
+    # * method: list_attributes
+    def list_attributes(self) -> List[ContainerAttribute]:
         '''
         List the attributes from the yaml file.
 
-        :param group_id: The context group id.
-        :type group_id: str
-        :param flags: The infrastructure flags to get the attributes under.
         :type flags: List[str]
         :return: The list of attributes.
         '''
 
         # Load the attribute data from the yaml configuration file.
         data = yaml_client.load(
-            self.base_path,
-            create_data=lambda data: [ContainerAttributeData.from_yaml_data(
+            self.config_file,
+            create_data=lambda data: [ContainerAttributeYamlData.new(
                 id=id, **attribute_data) for id, attribute_data in data.items()],
-            start_node=lambda data: data.get('container').get('attrs').get(group_id))
+            start_node=lambda data: data.get('attrs'))
+        
+        # Return the list of attributes.
+        return [attribute.map(self.read_role) for attribute in data]
 
-        # Return the attribute objects.
-        result = []
-        for item in data:
-            item = item.map('to_object.yaml', flags=flags)
-            if not item:
-                continue
-            result.append(item)
-
-        return [item.map(role='to_object.yaml', flags=flags) for item in data]
-
-    def get_attribute(self, group_id: str, id: str, flag: str) -> ContainerAttribute:
-        '''
-        Get the attribute from the yaml file.
-
-        :param group_id: The group id.
-        :type group_id: str
-        :param id: The attribute id.
-        :type id: str
-        :param flag: The infrastructure flag to get the attribute under.
-        :type flag: str
-        '''
-
-        # Load the attribute data from the yaml configuration file.
-        data = yaml_client.load(
-            self.base_path,
-            create_data=lambda data: ContainerAttributeData.from_yaml_data(
-                id, **data),
-            start_node=lambda data: data.get('attrs').get(group_id).get(id))
-
-        # Exit if the attribute data is not found.
-        if not data:
-            return None
-
-        # Return the attribute object.
-        return data.map('to_object.yaml', flag=flag)
-
-    def save_attribute(self, group_id: str, attribute: ContainerAttribute, flag: str, **kwargs) -> None:
+    # * method: save_attribute
+    def save_attribute(self, attribute: ContainerAttribute):
         '''
         Save the attribute to the yaml file.
 
-        :param group_id: The group id.
-        :type group_id: str
         :param attribute: The attribute to save.
         :type attribute: ContainerAttribute
-        :param flag: The infrastructure flag to save the attribute under.
-        :type flag: str
         '''
 
-        # Load existing container attribute
-        data = yaml_client.load(
-            self.base_path,
-            create_data=lambda data: ContainerAttributeData.from_yaml_data(
-                **data, id=attribute.id),
-            start_node=lambda data: data.get('container').get(
-                'attrs').get(group_id).get(attribute.id)
-        )
-
-        # Update the attribute data for the given flag if the attribute exists.
-        if data:
-            data.data[flag] = attribute.data.to_primitive()
-        else:
-            data = ContainerAttributeData.new(
-                **attribute.to_primitive(), flag=flag)
+        # Create a new container attribute data object.
+        data = ContainerAttributeYamlData.from_model(attribute)
 
         # Update the attribute data.
         yaml_client.save(
-            self.base_path,
-            data,
-            f'container.attrs.{group_id}.{attribute.id}'
+            self.config_file,
+            data.to_primitive(role=self.write_role),
+            f'container/attrs/{attribute.id}'
         )
