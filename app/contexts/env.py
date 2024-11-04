6c2ac2369034b09c0e466d5fe1ef7455ef8bc4a0
Ashatz <andrew@greatstrength.me>
2024-11-04 16:58:43 -0700

    Context - Env - Enhancements and things to look over.

--- app/contexts/env.py
+++ app/contexts/env.py
@@ -1,15 +1,33 @@
-import typing
+# *** imports
 
-from ..contexts.app import AppContext
-from ..objects.container import DataAttribute
-from ..services import container_service
-from ..repositories.container import ContainerRepository
+# ** core
+from typing import Tuple
+
+# ** app
+from ..configs import *
+from ..contexts.app import AppInterfaceContext
 from ..contexts.container import ContainerContext
 
 
-class EnvironmentContext(object):
+# *** contexts
 
-    def __init__(self, env_base_key: str, **kwargs):
+# ** context: environment_context
+class EnvironmentContext(Model):
+    '''
+    An environment context is a class that is used to create and run the app interface context.
+    '''
+
+    # * attribute: containers
+    containers = DictType(
+        ModelType(ContainerContext),
+        default={},
+        metadata=dict(
+            description='The container contexts keyed by app interface.'
+        ),
+    )
+
+    # * method: init
+    def __init__(self, interface_id: str, app_config: str, container_config: str, **kwargs):
         '''
         Initialize the environment context.
 
@@ -17,143 +35,28 @@ class EnvironmentContext(object):
         :type env_base_key: str
         '''
 
-        # Load the environment variables.
-        env_variables = self.load_environment_variables(env_base_key)
-
-        # Create the container context.
-        container = self.create_container(env_variables)
+        # Load the container contexts.
+        self.load_containers(**kwargs)
 
         # Load the app context.
-        app_context = self.load_app_context(container)
+        container, app_context = self.load_app_context(interface_id)
 
         # Run the app context.
-        app_context.run(
-            container=container)
+        app_context.run(container=container, **kwargs)
 
-    def load_environment_variables(self, env_base_key: str) -> typing.Dict[str, typing.Any]:
+    # * method: load_containers
+    def load_containers(self, **kwargs):
         '''
-        Load the environment variables.
+        Load the container contexts by app interface.
 
-        :param env_base_key: The base key for the environment variables.
-        :type env_base_key: str
-        :return: The environment variables.
-        :rtype: dict
-        '''
-
-        # Load the environment variables.
-        import os
-        result = {}
-        for key, value in os.environ.items():
-            # Check if key is a valid environment variable.
-            try:
-                app, group, variable = key.split('__')
-            except:
-                continue
-            # Check if key is a valid environment variable.
-            if app != env_base_key:
-                continue
-            # Add environment variable to result.
-            group = group.lower()
-            if group not in result:
-                result[group] = {}
-            result[group][variable.lower()] = value
-        return result
-
-    def load_container_repository(self,
-        module_path: str = 'app.repositories.container',
-        class_name: str = 'YamlRepository',
-        **kwargs):
-        '''
-        Load the container repository.
-
-        :param module_path: The module path for the container repository.
-        :type module_path: str
-        :param class_name: The class name for the container repository.
-        :type class_name: str
-        :return: The container repository.
-        :rtype: ContainerRepository
-        '''
-
-        # Load container repository.
-        return container_service.import_dependency(module_path, class_name)(**kwargs)
-    
-    def load_app_info(self, **kwargs) -> typing.List[DataAttribute]:
-        '''
-        Load the app data attributes.
-
-        :param kwargs: The app data attributes.
-        :type kwargs: dict
-        :return: The app data attributes.
-        :rtype: list
-        '''
-
-        # Load the app data attributes.
-        result = []
-        for key, value in kwargs.items():
-            result.append(
-                DataAttribute.new(
-                    id=key.replace('-', '_'),
-                    type='data',
-                    data={'value': value})
-            )
-        return result
-    
-    def load_attributes(self, container_type: str, container_repo: ContainerRepository, flags: str = 'yaml, python', **kwargs):
-        '''
-        Load the container attributes.
-
-        :param container_type: The container type.
-        :type container_type: str
-        :param container_repo: The container repository.
-        :type container_repo: ContainerRepository
-        :param flags: The container flags.
-        :type flags: str
         :param kwargs: Additional keyword arguments.
         :type kwargs: dict
-        :return: The container attributes.
-        :rtype: list
         '''
 
-        # Get container attributes.
-        flags = flags.split(', ')
-        return container_repo.list_attributes(container_type, flags)
-
-    def create_container(self,
-        app_container: typing.Dict[str, str] = None,
-        container_repo: typing.Dict[str, str] = None,
-        app: typing.Dict[str, str] = None) -> ContainerContext:
-        '''
-        Create the app container.
-
-        :param container: The container environment variables.
-        :type container: dict
-        :param container_repo: The container repository environment variables.
-        :type container_repo: dict
-        :param app: The app environment variables.
-        :type app: dict
-        :return: The app container.
-        :rtype: AppContainer
-        '''
-
-        # Load the container repository.
-        repo_data = dict(container_yaml_base_path='app.yml') if not container_repo else container_repo
-        container_repo = self.load_container_repository(**repo_data)
-        
-        # Set the container environment variables.
-        container = ContainerContext(container_repo=container_repo, **app_container)
-
-        # Set the app environment variables.
-        app_data = dict(
-            app_name='tiferet-cli',
-            app_interface='cli') if not app else app
-        attributes = self.load_app_info(**app_data)
-
-        # Add App container to the container context and return it.
-        container.add_container('app', attributes)
-        container.add_container('feature')
-        return container
-
-    def load_app_context(self, container: ContainerContext) -> AppContext:
+        pass
+    
+    # * method: load_app_context
+    def load_app_context(self, interface_id: str) -> Tuple[ContainerContext, AppInterfaceContext]:
         '''
         Load the app context.
 
@@ -163,5 +66,11 @@ class EnvironmentContext(object):
         :rtype: AppContext
         '''
 
-        # Create the app context.
-        return container.get_dependency('app', 'cli_interface_context')
+        # Get the app container.
+        container: ContainerContext = self.containers[interface_id]
+
+        # Load the app context.
+        app_context = container.get_dependency('app_interface_context')
+
+        # Return the app context.
+        return container, app_context
