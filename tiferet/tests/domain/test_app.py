bdd33dad5d6f91488a5b324049038afdb455c7d8
Ashatz <andrew@greatstrength.me>
2024-11-19 14:35:13 -0700

    Finish Up App Domain Unit Tests

--- tiferet/tests/domain/test_app.py
+++ tiferet/tests/domain/test_app.py
@@ -31,23 +31,57 @@ def test_app_interface_new():
 
 def test_app_interface_get_dependency():
 
-    # Create a new mock app interface repository.
-    from ..repos import MockAppRepository
-    app_repo = MockAppRepository(
-        interfaces=[
-            AppInterface.new(
-                id='test',
-                name='test interface',
-                description='test description',
-                feature_flag='test feature flag',
-                data_flag='test data flag',
-                dependencies=[
-                     AppDependency.new(
-                            attribute_id='app_context',
-                            module_path='tests.contexts.app',
-                            class_name='TestAppInterfaceContext',
-                     ),
-                ],
-            ),
-        ],
+    # Create a new app interface.
+    from ..configs.app import test_app_interface as app_interface
+
+    # Get the app dependency.
+    app_dependency = app_interface.get_dependency('app_context')
+
+    # Assert the app dependency is valid.
+    assert app_dependency.attribute_id == 'app_context'
+    assert app_dependency.module_path == 'tests.contexts.app'
+    assert app_dependency.class_name == 'TestAppInterfaceContext'
+
+
+def test_app_interface_get_dependency_invalid():
+
+    # Create a new app interface.
+    from ..configs.app import test_app_interface as app_interface
+
+    # Assert the app dependency is invalid.
+    assert app_interface.get_dependency('invalid') is None
+
+
+def test_app_repository_configuration_new_default():
+
+    # Create a new app repository configuration.
+    from ...domain.app import AppRepositoryConfiguration
+
+    # Create a new app repository configuration object.
+    app_repository_configuration = AppRepositoryConfiguration.new()
+
+    # Assert the app repository configuration is valid.
+    assert app_repository_configuration.module_path == 'tiferet.repos.app'
+    assert app_repository_configuration.class_name == 'YamlProxy'
+    assert app_repository_configuration.params == dict(
+        app_config_file='app/configs/app.yml')
+
+
+def test_app_repository_configuration_new_custom():
+
+    # Create a new app repository configuration.
+    from ...domain.app import AppRepositoryConfiguration
+
+    # Create a new app repository configuration object.
+    app_repository_configuration = AppRepositoryConfiguration.new(
+        module_path='test.module.path',
+        class_name='TestClassName',
+        params=dict(
+            test_param='test value',
+        ),
     )
+
+    # Assert the app repository configuration is valid.
+    assert app_repository_configuration.module_path == 'test.module.path'
+    assert app_repository_configuration.class_name == 'TestClassName'
+    assert app_repository_configuration.params == dict(test_param='test value')
