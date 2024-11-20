83153ce3a01daac4b41f8e1c2f3bd8d942770199
Ashatz <andrew@greatstrength.me>
2024-11-20 15:13:17 -0700

    Domain - Tests - App Tests Fixtures and Enhancements.

---
+++ tiferet/domain/tests/__init__.py
@@ -0,0 +1,12 @@
+# *** imports
+
+# ** app
+
+# * app
+from ..app import AppDependency
+from ..app import AppInterface
+from ..app import AppRepositoryConfiguration
+
+# * container
+from ..container import ContainerAttribute
+from ..container import ContainerDependency
--- tiferet/domain/tests/test_app.py
+++ tiferet/domain/tests/test_app.py
@@ -1,7 +1,62 @@
-def test_app_dependency_new():
+# *** imports
 
-    # Create a new app dependency.
-    from ...configs.tests import app_dependency
+# ** infra
+
+import pytest
+
+# ** app
+from . import *
+
+
+# *** fixtures
+
+# ** fixture: app_dependency
+@pytest.fixture(scope="module")
+def app_dependency():
+    return AppDependency.new(
+        attribute_id='app_context',
+        module_path='tests.contexts.app',
+        class_name='TestAppInterfaceContext',
+    )
+
+
+# ** fixture: app_interface
+@pytest.fixture(scope="module")
+def app_interface(app_dependency):
+    return AppInterface.new(
+        id='test',
+        name='test interface',
+        description='test description',
+        feature_flag='test feature flag',
+        data_flag='test data flag',
+        dependencies=[
+            app_dependency,
+        ],
+    )
+
+
+# ** fixture: app_repository_configuration
+@pytest.fixture(scope="module")
+def app_repository_configuration():
+    return AppRepositoryConfiguration.new()
+
+
+# ** fixture: app_repository_configuration_custom
+@pytest.fixture(scope="module")
+def app_repository_configuration_custom():
+    return AppRepositoryConfiguration.new(
+        module_path='test.module.path',
+        class_name='TestClassName',
+        params=dict(
+            test_param='test value',
+        ),
+    )
+
+
+# *** tests
+
+# ** test: test_app_dependency_new
+def test_app_dependency_new(app_dependency):
 
     # Assert the app dependency is valid.
     assert app_dependency.attribute_id == 'app_context'
@@ -9,10 +64,8 @@ def test_app_dependency_new():
     assert app_dependency.class_name == 'TestAppInterfaceContext'
 
 
-def test_app_interface_new():
-
-    # Create a new app interface.
-    from ...configs.tests import app_interface
+# ** test: test_app_interface_new
+def test_app_interface_new(app_interface):  
 
     # Assert the app interface is valid.
     assert app_interface.id == 'test'
@@ -25,10 +78,9 @@ def test_app_interface_new():
     assert app_interface.dependencies[0].module_path == 'tests.contexts.app'
     assert app_interface.dependencies[0].class_name == 'TestAppInterfaceContext'
 
-def test_app_interface_get_dependency():
 
-    # Get the app interface.
-    from ...configs.tests import app_interface
+# ** test: test_app_interface_get_dependency
+def test_app_interface_get_dependency(app_interface):
 
     # Get the app dependency.
     app_dependency = app_interface.get_dependency('app_context')
@@ -38,22 +90,16 @@ def test_app_interface_get_dependency():
     assert app_dependency.module_path == 'tests.contexts.app'
     assert app_dependency.class_name == 'TestAppInterfaceContext'
 
-def test_app_interface_get_dependency_invalid():
 
-    # Get the app interface.
-    from ...configs.tests import app_interface
+# ** test: test_app_interface_get_dependency_invalid
+def test_app_interface_get_dependency_invalid(app_interface):
 
     # Assert the app dependency is invalid.
     assert app_interface.get_dependency('invalid') is None
 
 
-def test_app_repository_configuration_new_default():
-    
-    # Create a new app repository configuration object.
-    from ..app import AppRepositoryConfiguration
-
-    # Create a new app repository configuration object.
-    app_repository_configuration = AppRepositoryConfiguration.new()
+# ** test: test_app_repository_configuration_new
+def test_app_repository_configuration_new(app_repository_configuration):
 
     # Assert the app repository configuration is valid.
     assert app_repository_configuration.module_path == 'tiferet.repos.app'
@@ -62,21 +108,10 @@ def test_app_repository_configuration_new_default():
         app_config_file='app/configs/app.yml')
 
 
-def test_app_repository_configuration_new_custom():
-    
-    # Create a new app repository configuration object.
-    from ..app import AppRepositoryConfiguration
-
-    # Create a new app repository configuration object.
-    app_repository_configuration = AppRepositoryConfiguration.new(
-        module_path='test.module.path',
-        class_name='TestClassName',
-        params=dict(
-            test_param='test value',
-        ),
-    )
+# ** test: test_app_repository_configuration_new_custom
+def test_app_repository_configuration_new_custom(app_repository_configuration_custom):
 
     # Assert the app repository configuration is valid.
-    assert app_repository_configuration.module_path == 'test.module.path'
-    assert app_repository_configuration.class_name == 'TestClassName'
-    assert app_repository_configuration.params == dict(test_param='test value')
+    assert app_repository_configuration_custom.module_path == 'test.module.path'
+    assert app_repository_configuration_custom.class_name == 'TestClassName'
+    assert app_repository_configuration_custom.params == dict(test_param='test value')
