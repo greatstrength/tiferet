6f01e62b4766323ee52306786c372afb3647fbac
Ashatz <andrew@greatstrength.me>
2024-11-27 10:21:54 -0700

    Misc - Add Container Context Fixtures

--- tiferet/contexts/tests/__init__.py
+++ tiferet/contexts/tests/__init__.py
@@ -1,5 +1,8 @@
 # *** imports
 
+# infra
+import pytest
+
 # ** app
 from ..app import *
 from ..container import *
@@ -7,12 +10,34 @@ from ..env import *
 from ..error import *
 from ..feature import *
 from ..request import *
-from...domain.error import Error, ErrorMessage
-from ...domain.tests.test_error import error, error_message, formatted_error_message, error_with_formatted_message
+from ...domain.tests import *
+from ...repos.tests import *
 
 
 # *** test_models
 
 # ** test_model: test_model
 class TestModel(Model):
-        test_field = StringType(required=True)
+    test_field = StringType(required=True)
+
+
+# *** fixtures
+@pytest.fixture(scope='session')
+def container_repo(mock_container_repo, test_repo_container_attribute, test_feature_command_container_attribute):
+    return mock_container_repo(
+        attributes=[
+            test_repo_container_attribute,
+            test_feature_command_container_attribute
+        ]
+    )
+
+
+# ** fixture: container_context
+@pytest.fixture(scope='session')
+def container_context(container_repo):
+    return ContainerContext(
+        interface_id="test_interface",
+        container_repo=container_repo,
+        feature_flag="test",
+        data_flag="test"
+    )
