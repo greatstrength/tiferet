641de9aa642db7161a66ed46be65dfacc3121419
Ashatz <andrew@greatstrength.me>
2024-11-27 09:49:30 -0700

    Misc - Fix Domain Container Tests

--- tiferet/domain/tests/__init__.py
+++ tiferet/domain/tests/__init__.py
@@ -1,5 +1,8 @@
 # *** imports
 
+# ** infra
+import pytest
+
 # ** app
 
 # * configs
@@ -28,3 +31,71 @@ from ..error import Error
 # * feature
 from ..feature import FeatureCommand
 from ..feature import Feature
+
+
+# *** fixtures
+
+# ** fixture: container_dependency
+@pytest.fixture(scope='session')
+def test_proxy_container_dependency():
+    return ContainerDependency.new(
+        module_path='tiferet.repos.tests',
+        class_name='TestProxy',
+        flag='test',
+        parameters={'config_file': 'test.yml'}
+    )
+
+
+@pytest.fixture(scope='session')
+def test_feature_command_container_dependency():
+    return ContainerDependency.new(
+        module_path='tiferet.commands.tests',
+        class_name='TestFeatureCommand',
+        flag='test',
+        parameters={'config_file': 'test.yml'}
+    )
+
+@pytest.fixture(scope='session')
+def test_feature_command_core_container_dependency():
+    return ContainerDependency.new(
+        module_path='tiferet.commands.tests',
+        class_name='TestFeatureCommand',
+        flag='core',
+        parameters={'config_file': 'core.yml'}
+    )
+
+
+# ** fixture: test_repo_container_attribute
+@pytest.fixture(scope='session')
+def test_repo_container_attribute(test_proxy_container_dependency):
+    return ContainerAttribute.new(
+        id='test_repo',
+        type='data',
+        dependencies=[test_proxy_container_dependency],
+    )
+
+
+# ** fixture: container_attribute_empty
+@pytest.fixture(scope='session')
+def container_attribute_empty():
+    return ContainerAttribute.new(
+        id='test_repo',
+        type='data',
+        dependencies=[],
+    )
+
+
+# ** fixture: test_feature_command_container_attribute
+@pytest.fixture(scope='session')
+def test_feature_command_container_attribute(
+    test_feature_command_container_dependency, 
+    test_feature_command_core_container_dependency
+):
+    return ContainerAttribute.new(
+        id='test_repo',
+        type='feature',
+        dependencies=[
+            test_feature_command_container_dependency,
+            test_feature_command_core_container_dependency
+        ],
+    )
--- tiferet/domain/tests/test_container.py
+++ tiferet/domain/tests/test_container.py
@@ -1,119 +1,81 @@
 # *** imports
 
-# ** infra
-import pytest
-
 # ** app
 from . import *
-
-# *** fixtures
-
-# ** fixture: container_dependency
-@pytest.fixture
-def container_dependency():
-    return ContainerDependency.new(
-        module_path='tests.repos.test',
-        class_name='YamlProxy',
-        flag='test',
-        parameters={'config_file': 'test.yml'}
-    )
-
-# ** fixture: container_attribute
-@pytest.fixture
-def container_attribute(container_dependency):
-    return ContainerAttribute.new(
-        id='test_repo',
-        type='data',
-        dependencies=[container_dependency],
-    )
-
-
-# ** fixture: container_attribute_empty
-@pytest.fixture
-def container_attribute_empty():
-    return ContainerAttribute.new(
-        id='test_repo',
-        type='data',
-        dependencies=[],
-    )
 
 
 # *** tests
 
 # ** test: container_dependency_new
-def test_container_dependency_new(container_dependency):
+def test_container_dependency_new(test_proxy_container_dependency):
 
     # Assert the container dependency is valid.
-    assert container_dependency.module_path == 'tests.repos.test'
-    assert container_dependency.class_name == 'YamlProxy'
-    assert container_dependency.flag == 'test'
-    assert container_dependency.parameters == {'config_file': 'test.yml'}
+    assert test_proxy_container_dependency.module_path == 'tiferet.repos.tests'
+    assert test_proxy_container_dependency.class_name == 'TestProxy'
+    assert test_proxy_container_dependency.flag == 'test'
+    assert test_proxy_container_dependency.parameters == {
+        'config_file': 'test.yml'}
 
 
 # ** test: container_attribute_new
-def test_container_attribute_new(container_attribute):
+def test_container_attribute_new(test_repo_container_attribute, test_proxy_container_dependency):
 
     # Assert the container attribute is valid.
-    assert container_attribute.id == 'test_repo'
-    assert container_attribute.type == 'data'
-    assert len(container_attribute.dependencies) == 1
-    assert container_attribute.dependencies[0].module_path == 'tests.repos.test'
-    assert container_attribute.dependencies[0].class_name == 'YamlProxy'
-    assert container_attribute.dependencies[0].flag == 'test'
-    assert container_attribute.dependencies[0].parameters == {'config_file': 'test.yml'}
+    assert test_repo_container_attribute.id == 'test_repo'
+    assert test_repo_container_attribute.type == 'data'
+    assert len(test_repo_container_attribute.dependencies) == 1
+    assert test_repo_container_attribute.dependencies[0] == test_proxy_container_dependency
 
 
 # ** test: test_container_attribute_get_dependency
-def test_container_attribute_get_dependency(container_attribute):
+def test_container_attribute_get_dependency(
+    test_repo_container_attribute,
+    test_proxy_container_dependency
+):
 
     # Get the container dependency.
-    container_dependency = container_attribute.get_dependency('test')
+    container_dependency = test_repo_container_attribute.get_dependency('test')
 
     # Assert the container dependency is valid.
-    assert container_dependency.module_path == 'tests.repos.test'
-    assert container_dependency.class_name == 'YamlProxy'
-    assert container_dependency.flag == 'test'
-    assert container_dependency.parameters == {'config_file': 'test.yml'}
+    assert container_dependency == test_proxy_container_dependency
 
 
 # ** test: test_container_attribute_get_dependency_invalid
-def test_container_attribute_get_dependency_invalid(container_attribute):
+def test_container_attribute_get_dependency_invalid(test_repo_container_attribute):
 
     # Assert the container dependency is invalid.
-    assert container_attribute.get_dependency('invalid') is None
+    assert test_repo_container_attribute.get_dependency('invalid') is None
 
 
-# ** test: test_container_attribute_set_dependency
-def test_container_attribute_set_dependency(container_attribute_empty, container_dependency):
+# ** test: test_container_attribute_se  t_dependency
+def test_container_attribute_set_dependency(
+    container_attribute_empty,
+    test_proxy_container_dependency
+):
 
     # Assert that the container attribute has no dependencies.
     assert len(container_attribute_empty.dependencies) == 0
 
     # Set the container dependency.
-    container_attribute_empty.set_dependency(container_dependency)
+    container_attribute_empty.set_dependency(test_proxy_container_dependency)
 
     # Assert the container dependency is valid.
     assert len(container_attribute_empty.dependencies) == 1
-    assert container_attribute_empty.dependencies[0].module_path == 'tests.repos.test'
-    assert container_attribute_empty.dependencies[0].class_name == 'YamlProxy'
-    assert container_attribute_empty.dependencies[0].flag == 'test'
+    assert container_attribute_empty.dependencies[0] == test_proxy_container_dependency
 
 
 # ** test: test_container_attribute_set_dependency_exists
-def test_container_attribute_set_dependency_exists(container_attribute, container_dependency):
+def test_container_attribute_set_dependency_exists(test_repo_container_attribute, test_proxy_container_dependency):
 
     # Modify the container dependency.
-    container_dependency.module_path = 'tests.repos.super_test'
-    container_dependency.class_name = 'SuperYamlProxy'
+    test_proxy_container_dependency.module_path = 'tests.repos.super_test'
+    test_proxy_container_dependency.class_name = 'SuperYamlProxy'
 
     # Set the container dependency.
-    container_attribute.set_dependency(container_dependency)
+    test_repo_container_attribute.set_dependency(test_proxy_container_dependency)
 
     # Get the container dependency.
-    container_dependency = container_attribute.get_dependency('test')
+    container_dependency = test_repo_container_attribute.get_dependency('test')
 
     # Assert the container dependency is valid.
-    assert container_dependency.module_path == 'tests.repos.super_test'
-    assert container_dependency.class_name == 'SuperYamlProxy'
-    assert container_dependency.flag == 'test'
-    assert container_dependency.parameters == {'config_file': 'test.yml'}
+    assert container_dependency == test_proxy_container_dependency
