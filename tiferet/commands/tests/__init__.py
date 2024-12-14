636c383edadc23d372a4e1cd30afad40d173ab23
Ashatz <andrew@greatstrength.me>
2024-12-14 15:02:07 -0700

    Misc - Add Test Feature Command Class with Environment Variables

--- tiferet/commands/tests/__init__.py
+++ tiferet/commands/tests/__init__.py
@@ -24,3 +24,15 @@ class TestFeatureCommand(object):
         # Return the result.
         return (param1, param2)
     
+
+# ** class test_feature_command_with_env_var
+class TestFeatureCommandWithEnvVar(object):
+    '''
+    A test feature command class with an environment variable.
+    '''
+
+    def execute(self, test_env_var: str, **kwargs) -> str:
+
+        # Return the environment variable.
+        return (True, test_env_var)
+    
