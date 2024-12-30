8da6614c9890a2d7760280eaccdc955a50f0daa5
Ashatz <andrew@greatstrength.me>
2024-12-30 13:57:50 -0700

    Misc - Model Test Cleanup as Needed

--- tiferet/models/tests/__init__.py
+++ tiferet/models/tests/__init__.py
@@ -6,29 +6,17 @@ import pytest
 # ** app
 
 # * configs
-from ...configs import StringType
-
-# * core
-from ..core import ModelObject
 from ..core import Entity
 from ..core import ValueObject
-from ..core import DataObject
 
 # * app
-from ..app import AppDependency
-from ..app import AppInterface
+from ..app import *
 
 # * container
-from ..container import ContainerAttribute
-from ..container import ContainerDependency
-
-# * error
-from ..error import ErrorMessage
-from ..error import Error
+from ..container import *
 
 # * feature
-from ..feature import FeatureCommand
-from ..feature import Feature
+from ..feature import *
 
 
 # *** constants
@@ -138,8 +126,9 @@ def test_app_interface(
 
 # ** fixture: container_dependency (container)
 @pytest.fixture(scope='session')
-def test_proxy_container_dependency():
-    return ContainerDependency.new(
+def container_dependency():
+    return ValueObject.new(
+        ContainerDependency,
         module_path='tiferet.repos.tests',
         class_name='TestProxy',
         flag='test',
@@ -150,9 +139,10 @@ def test_proxy_container_dependency():
 # ** fixture: test_feature_command_container_dependency (container)
 @pytest.fixture(scope='session')
 def test_feature_command_container_dependency():
-    return ContainerDependency.new(
+    return ValueObject.new(
+        ContainerDependency,
         module_path='tiferet.commands.tests',
-        class_name='TestFeatureCommand',
+        class_name='TestServiceCommand',
         flag='test',
     )
 
@@ -160,9 +150,10 @@ def test_feature_command_container_dependency():
 # ** fixture: test_feature_command_core_container_dependency (container)
 @pytest.fixture(scope='session')
 def test_feature_command_core_container_dependency():
-    return ContainerDependency.new(
+    return ValueObject.new(
+        ContainerDependency,
         module_path='tiferet.commands.tests',
-        class_name='TestFeatureCommand',
+        class_name='TestServiceCommand',
         flag='core',
     )
 
@@ -170,26 +161,29 @@ def test_feature_command_core_container_dependency():
 # ** fixture: test_feature_command_with_env_var_parameter_container_dependency (container)
 @pytest.fixture(scope='session')
 def test_feature_command_with_env_var_parameter_container_dependency():
-    return ContainerDependency.new(
+    return ValueObject.new(
+        ContainerDependency,
         module_path='tiferet.commands.tests',
-        class_name='TestFeatureCommandWithEnvVar',
+        class_name='TestServiceCommandWithEnvVar',
         flag='test',
     )
 
 # ** fixture: test_repo_container_attribute (container)
 @pytest.fixture(scope='session')
-def test_repo_container_attribute(test_proxy_container_dependency):
-    return ContainerAttribute.new(
+def container_attribute(container_dependency):
+    return Entity.new(
+        ContainerAttribute,
         id='test_repo',
         type='data',
-        dependencies=[test_proxy_container_dependency],
+        dependencies=[container_dependency],
     )
 
 
 # ** fixture: container_attribute_empty (container)
 @pytest.fixture(scope='session')
 def container_attribute_empty():
-    return ContainerAttribute.new(
+    return Entity.new(
+        ContainerAttribute,
         id='test_repo',
         type='data',
         dependencies=[],
@@ -202,7 +196,8 @@ def test_feature_command_container_attribute(
     test_feature_command_container_dependency, 
     test_feature_command_core_container_dependency
 ):
-    return ContainerAttribute.new(
+    return Entity.new(
+        ContainerAttribute,
         id='test_feature_command',
         type='feature',
         dependencies=[
@@ -217,93 +212,18 @@ def test_feature_command_container_attribute(
 def test_feature_command_with_env_var_parameter_container_attribute(
     test_feature_command_with_env_var_parameter_container_dependency
 ):
-    return ContainerAttribute.new(
+    return Entity.new(
+        ContainerAttribute,
         id='test_feature_command_with_env_var_parameter',
         type='feature',
         dependencies=[test_feature_command_with_env_var_parameter_container_dependency]
     )
 
 
-# ** fixture: test_error_message (error)
-@pytest.fixture(scope='session')
-def test_error_message():
-    return ErrorMessage.new(
-        lang='en_US',
-        text='An error occurred.'
-    )
-
-
-# ** fixture: test_formatted_error_message (error)
-@pytest.fixture(scope='session')
-def test_formatted_error_message():
-    return ErrorMessage.new(
-        lang='en_US',
-        text='An error occurred: {}'
-    )
-
-
-# ** fixture: test_error (error)
-@pytest.fixture(scope='session')
-def test_error(test_error_message):
-    return Error.new(
-        name='My Error',
-        message=[test_error_message]
-    )
-
-
-# ** fixture: test_error_with_multiple_messages (error)
-@pytest.fixture(scope='session')
-def test_error_with_multiple_messages(test_error_message):
-    return Error.new(
-        name='Multi Language Error',
-        message=[
-            test_error_message,
-            ErrorMessage.new(lang='fr_FR', text='Une erreur est survenue.')
-        ]
-    )
-
-
-# ** fixture: test_error_with_formatted_message (error)
-@pytest.fixture(scope='session')
-def test_error_with_formatted_message(test_formatted_error_message):
-    return Error.new(
-        name='Formatted Error',
-        message=[
-            test_formatted_error_message
-        ]
-    )
-
-
-# ** fixture: test_error_with_custom_id_and_code (error)
-@pytest.fixture(scope='session')
-def test_error_with_custom_id_and_code():
-    return Error.new(
-        name='Custom Error',
-        id='CUSTOM_ERROR',
-        error_code='CUSTOM_ERR',
-        message=[ErrorMessage.new(lang='en_US', text='An error occurred.')]
-    )
-
-
-# ** fixture: error_with_multiple_args (error)
-@pytest.fixture(scope='session')
-def test_error_with_multiple_args():
-    return Error.new(
-        name="MULTI_FORMATTED_ERROR",
-        error_code="MULTI_FORMATTED_ERROR",
-        message=[
-            ErrorMessage.new(
-                lang="en_US",
-                text="An error occurred: {0} - {1}."
-            )
-        ]
-    )
-
-
 # ** fixture: test_feature_command (feature)
 @pytest.fixture(scope='session')
 def test_feature_command():
-    return FeatureCommand.new(
+    return ServiceCommand.new(
         name='Test Feature Command',
         attribute_id='test_feature_command',
         params={'param1': 'value1'},
@@ -313,7 +233,7 @@ def test_feature_command():
 # ** fixture: feature_command_return_to_data (feature)
 @pytest.fixture(scope='session')
 def test_feature_command_return_to_data():
-    return FeatureCommand.new(
+    return ServiceCommand.new(
         name='Test Feature Command Return To Data',
         attribute_id='test_feature_command',
         params={'param1': 'value1'},
@@ -326,7 +246,7 @@ def test_feature_command_return_to_data():
 # ** fixtures: test_feature_command_to_add (feature)
 @pytest.fixture(scope='session')
 def test_feature_command_to_add():
-    return FeatureCommand.new(
+    return ServiceCommand.new(
         name='Additional Command',
         attribute_id='test_feature_command',
         params={'param1': 'value1a'},
@@ -337,7 +257,7 @@ def test_feature_command_to_add():
 # ** fixture: test_feature_command_with_env_var_parameter (feature)
 @pytest.fixture(scope='session')
 def test_feature_command_with_env_var_parameter():
-    return FeatureCommand.new(
+    return ServiceCommand.new(
         name='Test Feature Command With Env Var Parameter',
         attribute_id='test_feature_command_with_env_var_parameter',
         params={'test_env_var': '$env.TEST_ENV_VAR'}
@@ -347,7 +267,7 @@ def test_feature_command_with_env_var_parameter():
 
 @pytest.fixture(scope='session')
 def test_feature_command_with_pass_on_error():
-    return FeatureCommand.new(
+    return ServiceCommand.new(
         name='Test Feature Command With Pass On Error',
         attribute_id='test_feature_command',
         params={'param1': 'value1'},
@@ -360,7 +280,7 @@ def test_feature_command_with_pass_on_error():
 # ** fixture: test_feature_command_with_throw_and_pass_on_error (feature)
 @pytest.fixture(scope='session')
 def test_feature_command_with_throw_and_pass_on_error():
-    return FeatureCommand.new(
+    return ServiceCommand.new(
         name='Test Feature Command With Throw And Pass On Error',
         attribute_id='test_feature_command',
         params={'param1': 'value1', 'throw_error': 'True'},
