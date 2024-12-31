0eadbe0d83d4cc1cc6deac9214f8c186fcf4087f
Ashatz <andrew@greatstrength.me>
2024-12-31 14:07:48 -0700

    Proxies - Tests - Add Tests and Cleanup as Needed

---
+++ tiferet/proxies/tests/test_app.py
@@ -0,0 +1,72 @@
+# *** imports
+
+# ** infra
+import pytest
+
+# ** app
+from ..app_yaml import *
+from ...models.tests.test_app import *
+
+
+# *** fixtures
+
+# ** fixture: app_config_file
+@pytest.fixture
+def app_config_file():
+    return 'tiferet/configs/tests/test.yml'
+
+
+# ** fixture: app_yaml_proxy
+@pytest.fixture
+def app_yaml_proxy(app_config_file):
+    return AppYamlProxy(
+        app_config_file=app_config_file
+    )
+
+
+# *** tests
+
+# ** test: app_yaml_proxy_list_interfaces
+def test_app_yaml_proxy_list_interfaces(app_yaml_proxy, app_interface):
+    '''
+    Test the app YAML proxy list interfaces method.
+    '''
+    
+    # List the interfaces.
+    interfaces = app_yaml_proxy.list_interfaces()
+    
+    # Check the interfaces.
+    assert interfaces
+    assert len(interfaces) == 1
+    assert interfaces[0].id == app_interface.id
+    assert interfaces[0].name == app_interface.name
+
+
+# ** test: app_yaml_proxy_get_interface
+def test_app_yaml_proxy_get_interface(app_yaml_proxy, app_interface):
+    '''
+    Test the app YAML proxy get interface method.
+    '''
+
+    # Get the interface.
+    interface = app_yaml_proxy.get_interface(app_interface.id)
+
+    # Check the interface.
+    assert interface
+    assert interface.id == app_interface.id
+    assert interface.name == app_interface.name
+    assert interface.description == app_interface.description
+    assert interface.feature_flag == app_interface.feature_flag
+    assert interface.data_flag == app_interface.data_flag
+    assert len(interface.dependencies) == 7
+    assert interface.dependencies[0] == app_interface.dependencies[0]
+
+
+# ** test: app_yaml_proxy_get_interface_not_found
+def test_app_yaml_proxy_get_interface_not_found(app_yaml_proxy):
+
+    # Get the interface.
+    interface = app_yaml_proxy.get_interface('not_found')
+
+    # Check the interface.
+    assert not interface
