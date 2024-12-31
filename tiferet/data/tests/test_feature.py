7d5799000e52575e0c821af84ce0b4467154ee52
Ashatz <andrew@greatstrength.me>
2024-12-31 10:44:17 -0700

    Data - Feature - Feature Data Enhancements and Cleanup

--- tiferet/data/tests/test_feature.py
+++ tiferet/data/tests/test_feature.py
@@ -4,32 +4,19 @@
 import pytest
 
 # ** app
-from . import *
+from ..feature import *
 
 
 # *** fixtures
 
-# ** fixture: feature_command_data
-@pytest.fixture
-def feature_command_data():
-    
-    # Return the feature command data.
-    return ServiceCommandData(dict(
-        name='Test Feature Command',
-        attribute_id='test_feature_command',
-        params={},
-        return_to_data=True,
-        data_key='test_data',
-        pass_on_error=True
-    ))
-
-
-# ** fixture: feature_data
+# ** fixture: feature_data_raw
 @pytest.fixture
 def feature_data():
 
     # Return the feature data.
-    return FeatureData.from_data(**dict(
+    return DataObject.from_data(
+        FeatureData,
+        **dict(
         id='test.test_feature',
         name='Test Feature',
         description='This is a test feature.',
@@ -38,7 +25,7 @@ def feature_data():
             dict(
                 name='Test Feature Command',
                 attribute_id='test_feature_command',
-                params={},
+                params={'test_param': 'test_value'},
                 return_to_data=True,
                 data_key='test_data',
                 pass_on_error=True
@@ -48,65 +35,6 @@ def feature_data():
 
 
 # *** tests
-
-# ** test: test_feature_command_data_init
-def test_feature_command_data_init(feature_command_data):
-    '''
-    Test the feature command data initialization.
-    '''
-    
-    # Assert the feature command data attributes.
-    assert feature_command_data.name == 'Test Feature Command'
-    assert feature_command_data.attribute_id == 'test_feature_command'
-    assert feature_command_data.params == {}
-    assert feature_command_data.return_to_data == True
-    assert feature_command_data.data_key == 'test_data'
-    assert feature_command_data.pass_on_error == True
-
-
-# ** test: test_feature_command_data_map
-def test_feature_command_data_map(feature_command_data):
-    '''
-    Test the feature command data mapping.
-    '''
-    
-    # Map the feature command data to a feature command object.
-    feature_command = feature_command_data.map()
-
-    # Assert the feature command type.
-    assert isinstance(feature_command, ServiceCommand)
-    
-    # Assert the feature command attributes.
-    assert feature_command.name == 'Test Feature Command'
-    assert feature_command.attribute_id == 'test_feature_command'
-    assert feature_command.params == {}
-    assert feature_command.return_to_data == True
-    assert feature_command.data_key == 'test_data'
-    assert feature_command.pass_on_error == True
-
-
-# ** test: test_feature_data_from_data
-def test_feature_data_from_data(feature_data):
-    '''
-    Test the feature data from data method.
-    '''
-    
-    # Assert the feature data attributes.
-    assert feature_data.name == 'Test Feature'
-    assert feature_data.feature_key == 'test_feature'
-    assert feature_data.description == 'This is a test feature.'
-    assert feature_data.group_id == 'test'
-    assert len(feature_data.commands) == 1
-    
-    # Assert the feature command data attributes.
-    feature_command_data = feature_data.commands[0]
-    assert feature_command_data.name == 'Test Feature Command'
-    assert feature_command_data.attribute_id == 'test_feature_command'
-    assert feature_command_data.params == {}
-    assert feature_command_data.return_to_data == True
-    assert feature_command_data.data_key == 'test_data'
-    assert feature_command_data.pass_on_error == True
-
 
 # ** test: test_feature_data_map
 def test_feature_data_map(feature_data):
@@ -130,7 +58,7 @@ def test_feature_data_map(feature_data):
     feature_command = feature.commands[0]
     assert feature_command.name == 'Test Feature Command'
     assert feature_command.attribute_id == 'test_feature_command'
-    assert feature_command.params == {}
+    assert feature_command.params == {'test_param': 'test_value'}
     assert feature_command.return_to_data == True
     assert feature_command.data_key == 'test_data'
     assert feature_command.pass_on_error == True
