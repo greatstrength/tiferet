aaf346a4a6a961a1939e96821f60439579316e03
Ashatz <andrew@greatstrength.me>
2024-12-30 19:45:34 -0700

    47 - Enancement - Add Core Service Command

---
+++ tiferet/commands/core.py
@@ -0,0 +1,36 @@
+# *** imports
+
+# ** app
+from ..configs import *
+
+
+# *** commands
+
+# ** command: service_command
+class ServiceCommand(object):
+    '''
+    A service command class.
+    '''
+
+    def execute(**kwargs) -> Any:
+        '''
+        Execute the service command.
+        '''
+
+        # Not implemented.
+        raise NotImplementedError()
+    
+
+    # ** method: verify
+    def verify(self, expression: bool, error_code: str, *args):
+        '''
+        Verify an expression and raise an error if it is false.
+        '''
+
+        # Format the error message.
+        message = error_code
+        if args:
+            message = '{}: {}'.format(message, ', '.join(args))
+
+        # Verify the expression.
+        assert expression, message
---
+++ tiferet/commands/tests/test_core.py
@@ -0,0 +1,72 @@
+# *** imports
+
+# ** infra
+import pytest
+
+# ** app
+from ..core import *
+
+
+# *** fixtures
+
+# ** fixture: test_service_command
+@pytest.fixture
+def test_service_command() -> ServiceCommand:
+    '''
+    A test service command fixture.
+    '''
+    class TestServiceCommand(ServiceCommand):
+        '''
+        A test service command class.
+        '''
+        
+        def execute(self, param1: str, param2: str, throw_error: bool = False, *error_args, **kwargs) -> Tuple[str, str]:
+
+            # Throw an error if requested.
+            if error_args:
+                self.verify(throw_error == False, 'MY_FORMATTED_ERROR', *error_args)
+            else:
+                self.verify(throw_error == False, 'MY_ERROR')
+
+            # Return the result.
+            return (param1, param2)
+        
+    return TestServiceCommand()
+
+
+# *** tests
+
+# ** test: test_service_command_execute
+def test_service_command_execute(test_service_command):
+    '''
+    Test the service command execute method.
+    '''
+
+    # Execute the command.
+    result: ServiceCommand = test_service_command.execute('param1', 'param2')
+
+    # Verify the result.
+    assert result == ('param1', 'param2')
+
+
+# ** test: test_service_command_execute_with_error
+def test_service_command_execute_with_error(test_service_command):
+    '''
+    Test the service command execute method with an error.
+    '''
+
+    # Execute the command.
+    with pytest.raises(AssertionError):
+        test_service_command.execute('param1', 'param2', throw_error=True)
+
+    # Verify the error message.
+    try:
+        test_service_command.execute('param1', 'param2', throw_error=True)
+    except AssertionError as e:
+        assert str(e) == 'MY_ERROR'
+
+    # Execute the command with formatted error.
+    try:
+        test_service_command.execute('param1', 'param2', 'err_arg1', 'err_arg2', throw_error=True)
+    except AssertionError as e:
+        assert str(e) == 'MY_FORMATTED_ERROR: err_arg1, err_arg2'
