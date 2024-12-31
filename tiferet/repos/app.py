1ef5ca15f18d3eb41ae4ece02715c07e812e6230
Ashatz <andrew@greatstrength.me>
2024-12-31 14:03:24 -0700

    Repos - App - App Repository Interface

--- tiferet/repos/app.py
+++ tiferet/repos/app.py
@@ -1,32 +1,32 @@
 # *** imports
 
 # ** app
-from ..models.app import *
-from ..data.app import AppInterfaceYamlData
-from ..clients import yaml_client
+from ..configs import *
+from ..models.app import AppInterface
 
 
 # *** repository
 
 # ** interface: app_repository
-class AppRepository(object):
+class AppRepository(ABC):
     '''
     An app repository is a class that is used to get an app interface.
     '''
 
     # * method: list_interfaces
-    def list_interfaces(self) -> list[AppInterface]:
+    @abstractmethod
+    def list_interfaces(self) -> List[AppInterface]:
         '''
         List all app interfaces.
 
         :return: The list of app interfaces.
-        :rtype: list[AppInterface]
+        :rtype: List[AppInterface]
         '''
-
         # Not implemented.
         raise NotImplementedError()
 
     # * method: get_interface
+    @abstractmethod
     def get_interface(self, id: str) -> AppInterface:
         '''
         Get the app interface.
@@ -36,72 +36,5 @@ class AppRepository(object):
         :return: The app interface.
         :rtype: AppInterface
         '''
-
         # Not implemented.
         raise NotImplementedError()
-
-
-# ** proxy: app_yaml_proxy
-class AppYamlProxy(AppRepository):
-
-    # * field: config_file
-    config_file: str = None
-
-    # * method: init
-    def __init__(self, app_config_file: str):
-        '''
-        Initialize the YAML proxy.
-
-        :param app_config_file: The application configuration file.
-        :type app_config_file: str
-        '''
-
-        # Set the configuration file.
-        self.config_file = app_config_file
-
-    # * method: list_interfaces
-    def list_interfaces(self) -> list[AppInterface]:
-        '''
-        List all app interfaces.
-
-        :return: The list of app interfaces.
-        :rtype: list[AppInterface]
-        '''
-
-        # Load the app interface data from the yaml configuration file and map it to the app interface object.
-        interfaces = yaml_client.load(
-            self.config_file,
-            create_data=lambda data: [
-                AppInterfaceYamlData.from_data(
-                    id=interface_id,
-                    **record
-                ).map() for interface_id, record in data.items()],
-            start_node=lambda data: data.get('interfaces'))
-
-        # Return the list of app interface objects.
-        return interfaces
-
-    # * method: get_interface
-    def get_interface(self, id: str) -> AppInterface:
-        '''
-        Get the app interface.
-
-        :param id: The app interface id.
-        :type id: str
-        :return: The app interface.
-        :rtype: AppInterface
-        '''
-
-        # Load the app interface data from the yaml configuration file.
-        _data: AppInterface = yaml_client.load(
-            self.config_file,
-            create_data=lambda data: AppInterfaceYamlData.from_data(
-                id=id, **data),
-            start_node=lambda data: data.get('interfaces').get(id))
-
-        # Return the app interface object.
-        # If the data is None, return None.
-        try:
-            return _data.map()
-        except AttributeError:
-            return None
