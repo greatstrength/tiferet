452b0c3db7f9e519121ea7da981d2d6ffcab969d
Ashatz <andrew@greatstrength.me>
2024-12-31 19:58:12 -0700

    Configs - Errors - Add Custom Arguments

--- tiferet/configs/errors.py
+++ tiferet/configs/errors.py
@@ -11,15 +11,17 @@ class TiferetError(Exception):
     A base exception for Tiferet.
     '''
 
-    def __init__(self, error_code: str, message: str):
+    def __init__(self, error_code: str, message: str, *args):
         '''
         Initialize the exception.
         :param message: The message.
         :type message: str
         '''
 
-        # Set the message.
+        # Set the error code and arguments.
         self.error_code = error_code
+        self.args = args
+        
         super().__init__(
             json.dumps(dict(
                 error_code=error_code,
