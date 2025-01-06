4199a6067800e4b57d2f9ce3e7a8402a9a6936ba
Ashatz <andrew@greatstrength.me>
2025-01-06 11:17:53 -0700

    Commands - Tests - Cleanup as Needed

--- tiferet/commands/tests/test_core.py
+++ tiferet/commands/tests/test_core.py
@@ -24,9 +24,9 @@ def test_service_command() -> ServiceCommand:
 
             # Throw an error if requested.
             if error_args:
-                self.verify(throw_error == False, 'MY_FORMATTED_ERROR', *error_args)
+                self.verify(throw_error == False, 'MY_FORMATTED_ERROR', 'An error occurred: {}', *error_args)
             else:
-                self.verify(throw_error == False, 'MY_ERROR')
+                self.verify(throw_error == False, 'MY_ERROR', 'An error occurred.')
 
             # Return the result.
             return (param1, param2)
@@ -56,17 +56,15 @@ def test_service_command_execute_with_error(test_service_command):
     '''
 
     # Execute the command.
-    with pytest.raises(AssertionError):
+    with pytest.raises(TiferetError) as e:
         test_service_command.execute('param1', 'param2', throw_error=True)
+    
+    # Verify the error.
+    assert e.value.error_code == 'MY_ERROR'
 
-    # Verify the error message.
-    try:
-        test_service_command.execute('param1', 'param2', throw_error=True)
-    except AssertionError as e:
-        assert str(e) == 'MY_ERROR'
+    # Execute the command with error arguments.
+    with pytest.raises(TiferetError) as e:
+        test_service_command.execute('param1', 'param2', throw_error=True, error_args=['arg1', 'arg2'])
 
-    # Execute the command with formatted error.
-    try:
-        test_service_command.execute('param1', 'param2', throw_error=True, error_args=['err_arg1', 'err_arg2'])
-    except AssertionError as e:
-        assert str(e) == 'MY_FORMATTED_ERROR: err_arg1, err_arg2'
+    # Verify the error.
+    assert e.value.error_code == 'MY_FORMATTED_ERROR'
