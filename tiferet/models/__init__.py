754e892ff3da5aebf489a646aaf173c3148201b5
Ashatz <andrew@greatstrength.me>
2025-01-06 10:51:23 -0700

    Misc - Remove Feature Models from Global Import

--- tiferet/models/__init__.py
+++ tiferet/models/__init__.py
@@ -2,4 +2,3 @@ from .core import *
 from .app import AppInterface, AppDependency
 from .container import ContainerAttribute, ContainerDependency
 from .error import Error, ErrorMessage
-from .feature import Feature
