72f8784c61f5ab902178b926e9eff145b98c1bfb
Ashatz <andrew@greatstrength.me>
2025-04-17 11:01:24 -0700

    Configs - Exceptions - Add Module and Import Updates

--- tiferet/configs/__init__.py
+++ tiferet/configs/__init__.py
@@ -1,11 +1,10 @@
 # *** imports
 
 # ** core
-from typing import List, Dict, Any, Tuple
 import os
 import json
 from abc import ABC, abstractmethod
 
 # ** app
-from .types import *
-from .errors import *
+from .error import *
+from .exceptions import *
