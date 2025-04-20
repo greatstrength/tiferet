8248985a117c11c4d5b17d074decf32a54b39a0e
Ashatz <andrew@greatstrength.me>
2025-04-20 00:48:11 -0700

    Proxies - Container - Add Mock Container Proxy

---
+++ tiferet/proxies/tests/container_mock.py
@@ -0,0 +1,21 @@
+# *** imports
+
+# ** app
+from ...models.container import *
+from ...repos import ContainerRepository
+
+
+# *** mocks
+
+# ** mock: mock_container_proxy
+class MockContainerProxy(ContainerRepository):
+
+    def __init__(self, attributes: List[ContainerAttribute] = [], constants: Dict[str, str] = {}):
+        self.attributes = attributes
+        self.constants = constants
+
+    def get_attribute(self, attribute_id, type):
+        return next((attribute for attribute in self.attributes if attribute.id == attribute_id), None)
+
+    def list_all(self):
+        return self.attributes, self.constants
