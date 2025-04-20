ddc9359b164ca8a99f0077748af271066926e614
Ashatz <andrew@greatstrength.me>
2025-04-19 21:18:17 -0700

    Configs - App - Add New Configurations

---
+++ tiferet/configs/app.py
@@ -0,0 +1,64 @@
+# *** constants
+
+# ** constant: default_container_context_dependency
+DEFAULT_CONTAINER_CONTEXT_DEPENDENCY = dict(
+    attribute_id='container_context',
+    module_path='tiferet.contexts.container',
+    class_name='ContainerContext',
+)
+
+
+# ** constant: error_context_dependency
+DEFAULT_ERROR_CONTEXT_DEPENDENCY = dict(
+    attribute_id='error_context',
+    module_path='tiferet.contexts.error',
+    class_name='ErrorContext',
+)
+
+
+# ** constant: default_feature_context_dependency
+DEFAULT_FEATURE_CONTEXT_DEPENDENCY = dict(
+    attribute_id='feature_context',
+    module_path='tiferet.contexts.feature',
+    class_name='FeatureContext',
+)
+
+
+# ** constant: default_app_context_dependency
+DEFAULT_APP_CONTEXT_DEPENDENCY = dict(
+    attribute_id='app_context',
+    module_path='tiferet.contexts.app',
+    class_name='AppInterfaceContext',
+)
+
+
+# ** constant: default_container_repo_dependency
+DEFAULT_CONTAINER_REPO_DEPENDENCY = dict(
+    attribute_id='container_repo',
+    module_path='tiferet.proxies.container_yaml',
+    class_name='ContainerYamlProxy',
+)
+
+
+# ** constant: default_error_repo_dependency
+DEFAULT_ERROR_REPO_DEPENDENCY = dict(
+    attribute_id='error_repo',
+    module_path='tiferet.proxies.error_yaml',
+    class_name='ErrorYamlProxy',
+)
+
+
+# ** constant: default_feature_repo_dependency
+DEFAULT_FEATURE_REPO_DEPENDENCY = dict(
+    attribute_id='feature_repo',
+    module_path='tiferet.proxies.feature_yaml',
+    class_name='FeatureYamlProxy',
+)
+
+
+# ** constant: default_app_repo_dependency
+DEFAULT_APP_REPO_DEPENDENCY = dict(
+    attribute_id='app_repo',
+    module_path='tiferet.proxies.app_yaml',
+    class_name='AppYamlProxy',
+)
