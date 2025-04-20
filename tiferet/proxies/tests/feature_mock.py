d8624d4d0bc5136a0958c221f087db98a952309d
Ashatz <andrew@greatstrength.me>
2025-04-20 00:47:27 -0700

    Proxies - Feature - Add Mock Feature Repository

---
+++ tiferet/proxies/tests/feature_mock.py
@@ -0,0 +1,27 @@
+# *** imports
+
+# ** app
+from ...models.feature import *
+from ...repos import FeatureRepository
+
+
+# *** mocks
+
+# ** mock: mock_feature_proxy
+class MockFeatureProxy(FeatureRepository):
+
+    # * method: init
+    def __init__(self, features: List[Feature] = []):
+        self.features = features
+
+    # * method: list
+    def list(self) -> List[Feature]:
+        return self.features
+    
+    # * method: get
+    def get(self, feature_id: str) -> Feature:
+        return next((feature for feature in self.features if feature.id == feature_id), None)
+    
+    # * method: exists
+    def exists(self, feature_id: str) -> bool:
+        return self.get(feature_id) is not None
