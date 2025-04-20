5162fdf6ef3e945a01b05354d2f91c66d77b8f15
Ashatz <andrew@greatstrength.me>
2025-04-19 21:44:44 -0700

    Configs - Request - Add Test Request

---
+++ tiferet/configs/tests/test_request.py
@@ -0,0 +1,8 @@
+# *** constants
+
+# ** constant: test_request_context
+TEST_REQUEST_CONTEXT = dict(
+    feature_id='test_group.test_feature',
+    headers={'Content-Type': 'application/json'},
+    data={'param2': 'value2'}
+)
