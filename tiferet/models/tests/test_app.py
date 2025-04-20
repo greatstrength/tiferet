c0a7f19f3ecb7299ab399a8cb91bfb6027931dca
Ashatz <andrew@greatstrength.me>
2025-04-19 21:28:12 -0700

    Models - App - Test Cleanup

--- tiferet/models/tests/test_app.py
+++ tiferet/models/tests/test_app.py
@@ -5,53 +5,27 @@ import pytest
 
 # ** app
 from ..app import *
+from ...configs.app import *
+from ...configs.tests.test_app import *
 
 
 # *** fixtures
-# ** fixture: app_context_dependency
-@pytest.fixture
-def app_dependency():
-    return ValueObject.new(
-        AppDependency,
-        attribute_id='app_context',
-        module_path='tiferet.contexts.app',
-        class_name='AppInterfaceContext',
-    )
 
 # ** fixture: app_interface
 @pytest.fixture
-def app_interface(
-    app_dependency
-):
+def app_interface():
+    
     return Entity.new(
         AppInterface,
-        id='test',
-        name='Test Interface',
-        description='The test interface.',
-        feature_flag='test',
-        data_flag='test',
+        **TEST_APP_INTERFACE,
         dependencies=[
-            app_dependency,
+            DEFAULT_APP_CONTEXT_DEPENDENCY,
         ],
     )
 
 
+
 # *** tests
-
-# ** test: test_app_interface
-def test_app_interface(app_interface):
-
-    # Assert the app interface is valid.
-    assert app_interface.id == 'test'
-    assert app_interface.name == 'Test Interface'
-    assert app_interface.description == 'The test interface.'
-    assert app_interface.feature_flag == 'test'
-    assert app_interface.data_flag == 'test'
-    assert len(app_interface.dependencies) == 1
-    assert app_interface.dependencies[0].attribute_id == 'app_context'
-    assert app_interface.dependencies[0].module_path == 'tiferet.contexts.app'
-    assert app_interface.dependencies[0].class_name == 'AppInterfaceContext'
-    
 
 # ** test: test_app_interface_get_dependency
 def test_app_interface_get_dependency(app_interface):
@@ -60,9 +34,9 @@ def test_app_interface_get_dependency(app_interface):
     app_dependency = app_interface.get_dependency('app_context')
 
     # Assert the app dependency is valid.
-    assert app_dependency.attribute_id == 'app_context'
-    assert app_dependency.module_path == 'tiferet.contexts.app'
-    assert app_dependency.class_name == 'AppInterfaceContext'
+    assert app_dependency.attribute_id == DEFAULT_APP_CONTEXT_DEPENDENCY.get('attribute_id')
+    assert app_dependency.module_path == DEFAULT_APP_CONTEXT_DEPENDENCY.get('module_path')
+    assert app_dependency.class_name == DEFAULT_APP_CONTEXT_DEPENDENCY.get('class_name')
 
 
 # ** test: test_app_interface_get_dependency_invalid
