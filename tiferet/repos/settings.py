6a218ebc0b095939b4bb068846169d7ea8b03081
Ashatz <andrew@greatstrength.me>
2025-04-19 23:38:03 -0700

    Repos - Settings - Add Repository Class

---
+++ tiferet/repos/settings.py
@@ -0,0 +1,15 @@
+# *** imports
+
+# ** infra
+from abc import ABC, abstractmethod
+
+
+# *** classes
+
+# ** class: repository
+class Repository(ABC):
+    '''
+    The repository interface as an abstract base class.
+    '''
+
+    pass
