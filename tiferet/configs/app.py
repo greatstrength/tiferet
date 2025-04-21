c5de1b40d82ed3bfff539681dc11dbb98e72840d
Ashatz <andrew@greatstrength.me>
2025-04-21 13:00:07 -0700

    Misc - Config Reformat

--- tiferet/configs/app.py
+++ tiferet/configs/app.py
@@ -13,43 +13,20 @@ DEFAULT_APP_REPO_PARAMETERS = dict(
     app_config_file='tiferet/configs/app.yml'
 )
 
-# ** constant: default_container_context_dependency
-DEFAULT_CONTAINER_CONTEXT_DEPENDENCY = dict(
-    attribute_id='container_context',
-    module_path='tiferet.contexts.container',
-    class_name='ContainerContext',
-)
-
-
-# ** constant: error_context_dependency
-DEFAULT_ERROR_CONTEXT_DEPENDENCY = dict(
-    attribute_id='error_context',
-    module_path='tiferet.contexts.error',
-    class_name='ErrorContext',
-)
-
-
-# ** constant: default_feature_context_dependency
-DEFAULT_FEATURE_CONTEXT_DEPENDENCY = dict(
-    attribute_id='feature_context',
-    module_path='tiferet.contexts.feature',
-    class_name='FeatureContext',
-)
-
-
-# ** constant: default_app_context_dependency
-DEFAULT_APP_CONTEXT_DEPENDENCY = dict(
-    attribute_id='app_context',
-    module_path='tiferet.contexts.app',
-    class_name='AppInterfaceContext',
-)
-
 
 # ** constant: default_container_repo_dependency
 DEFAULT_CONTAINER_REPO_DEPENDENCY = dict(
     attribute_id='container_repo',
     module_path='tiferet.proxies.container_yaml',
     class_name='ContainerYamlProxy',
+)
+
+
+# ** constant: default_container_context_dependency
+DEFAULT_CONTAINER_CONTEXT_DEPENDENCY = dict(
+    attribute_id='container_context',
+    module_path='tiferet.contexts.container',
+    class_name='ContainerContext',
 )
 
 
@@ -61,11 +38,27 @@ DEFAULT_ERROR_REPO_DEPENDENCY = dict(
 )
 
 
+# ** constant: error_context_dependency
+DEFAULT_ERROR_CONTEXT_DEPENDENCY = dict(
+    attribute_id='error_context',
+    module_path='tiferet.contexts.error',
+    class_name='ErrorContext',
+)
+
+
 # ** constant: default_feature_repo_dependency
 DEFAULT_FEATURE_REPO_DEPENDENCY = dict(
     attribute_id='feature_repo',
     module_path='tiferet.proxies.feature_yaml',
     class_name='FeatureYamlProxy',
+)
+
+
+# ** constant: default_feature_context_dependency
+DEFAULT_FEATURE_CONTEXT_DEPENDENCY = dict(
+    attribute_id='feature_context',
+    module_path='tiferet.contexts.feature',
+    class_name='FeatureContext',
 )
 
 
@@ -75,3 +68,34 @@ DEFAULT_APP_REPO_DEPENDENCY = dict(
     module_path='tiferet.proxies.app_yaml',
     class_name='AppYamlProxy',
 )
+
+
+# ** constant: default_app_context_dependency
+DEFAULT_APP_CONTEXT_DEPENDENCY = dict(
+    attribute_id='app_context',
+    module_path='tiferet.contexts.app',
+    class_name='AppInterfaceContext',
+)
+
+# ** constant: default_app_context_dependencies
+DEFAULT_APP_CONTEXT_DEPENDENCIES = [
+    DEFAULT_CONTAINER_CONTEXT_DEPENDENCY,
+    DEFAULT_ERROR_CONTEXT_DEPENDENCY,
+    DEFAULT_FEATURE_CONTEXT_DEPENDENCY,
+    DEFAULT_CONTAINER_REPO_DEPENDENCY,
+    DEFAULT_ERROR_REPO_DEPENDENCY,
+    DEFAULT_FEATURE_REPO_DEPENDENCY,
+    DEFAULT_APP_REPO_DEPENDENCY,
+]
+
+
+# ** constant: default_container_config_file
+DEFAULT_CONTAINER_CONFIG_FILE = 'tiferet/configs/container.yml'
+
+
+# ** constant: default_feature_config_file
+DEFAULT_FEATURE_CONFIG_FILE = 'tiferet/configs/features.yml'
+
+
+# ** constant: default_error_config_file
+DEFAULT_ERROR_CONFIG_FILE = 'tiferet/configs/errors.yml'
