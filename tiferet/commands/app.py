84627f62e1d1f2ef46a8619db276ff59aa4a5eb0
Ashatz <andrew@greatstrength.me>
2025-04-21 19:33:48 -0700

    Commands - App - Import App Repository and List App Interfaces Commands

---
+++ tiferet/commands/app.py
@@ -0,0 +1,91 @@
+# *** imports
+
+# ** app
+from .settings import *
+from ..repos.app import *
+from ..contexts import import_dependency
+
+
+# *** commands:
+
+
+# ** command: import_app_repository
+class ImportAppRepository(ServiceCommand):
+    '''
+    A command to import the app repository.
+    '''
+
+    # * method: execute
+    def execute(self,
+                app_repo_module_path: str,
+                app_repo_class_name: str,
+                **kwargs
+                ) -> AppRepository:
+        '''
+        Execute the command.
+
+        :param app_repo_module_path: The app repository module path.
+        :type app_repo_module_path: str
+        :param app_repo_class_name: The app repository class name.
+        :type app_repo_class_name: str
+        :param kwargs: The command arguments.
+        :type kwargs: dict
+        :return: None
+        '''
+
+        # Import the app repository.
+        try:
+            
+            # Import the app repository class.
+            return import_dependency(
+                app_repo_module_path, 
+                app_repo_class_name
+            )(**kwargs)
+        
+        # Handle the import error.
+        # Raise an error if the import fails.
+        except TiferetError as e:
+            raise_error(
+                'APP_REPOSITORY_IMPORT_FAILED',
+                f'Failed to import app repository: {e}.',
+                str(e)
+            )
+
+
+# ** command: list_app_interfaces
+class ListAppInterfaces(ServiceCommand):
+    '''
+    A command to list the app interfaces.
+    '''
+
+    def __init__(self, app_repo: AppRepository):
+        '''
+        Initialize the command.
+
+        :param app_repo: The app repository.
+        :type app_repo: AppRepository
+        '''
+
+        # Set the app repository.
+        self.app_repo = app_repo
+
+    # * method: execute
+    def execute(self, **kwargs) -> List[AppInterface]:
+        '''
+        Execute the command.
+
+        :param kwargs: The command arguments.
+        :type kwargs: dict
+        :return: The command result.
+        :rtype: List[AppInterface]
+        '''
+
+        # List the app interfaces.
+        try:
+            return self.app_repo.list_interfaces()
+        except Exception as e:
+            raise_error(
+                'APP_INTERFACE_LOADING_FAILED',
+                f'Failed to load app interfaces: {e}.',
+                str(e)
+            )
