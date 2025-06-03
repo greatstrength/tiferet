4c0e3b49dc6656cc62a14bd636fca9b66ac3b195
Ashatz <andrew@greatstrength.me>
2025-06-03 14:43:56 -0700

    Configs - App - General Pruning and Cleanup

--- tiferet/configs/app.py
+++ tiferet/configs/app.py
@@ -1,80 +1,34 @@
 # *** constants
 
-# ** constant: default_app_repo_module_path
-DEFAULT_APP_REPO_MODULE_PATH = 'tiferet.proxies.app_yaml'
-
-
-# ** constant: default_app_repo_class_name
-DEFAULT_APP_REPO_CLASS_NAME = 'AppYamlProxy'
-
-
-# ** constant: default_app_repo_parameters
-DEFAULT_APP_REPO_PARAMETERS = dict(
-    app_config_file='tiferet/configs/app.yml'
-)
-
-
 # ** constant: default_app_manager
 DEFAULT_APP_MANAGER_SETTINGS = dict(
-    repo_module_path=DEFAULT_APP_REPO_MODULE_PATH,
-    repo_class_name=DEFAULT_APP_REPO_CLASS_NAME,
-    repo_params=DEFAULT_APP_REPO_PARAMETERS
+    repo_module_path='tiferet.proxies.app_yaml',
+    repo_class_name='AppYamlProxy',
+    repo_params='tiferet/configs/app.yml',
 )
 
 
-# ** constant: default_container_repo_dependency
-DEFAULT_CONTAINER_REPO_DEPENDENCY = dict(
-    attribute_id='container_repo',
-    module_path='tiferet.proxies.container_yaml',
-    class_name='ContainerYamlProxy',
-)
-
-
-# ** constant: default_container_context_dependency
-DEFAULT_CONTAINER_CONTEXT_DEPENDENCY = dict(
+# ** constant: default_container_service_dependency
+DEFAULT_CONTAINER_SERVICE_DEPENDENCY = dict(
     attribute_id='container_context',
     module_path='tiferet.contexts.container',
     class_name='ContainerContext',
 )
 
 
-# ** constant: default_error_repo_dependency
-DEFAULT_ERROR_REPO_DEPENDENCY = dict(
-    attribute_id='error_repo',
-    module_path='tiferet.proxies.error_yaml',
-    class_name='ErrorYamlProxy',
-)
-
-
-# ** constant: error_context_dependency
-DEFAULT_ERROR_CONTEXT_DEPENDENCY = dict(
+# ** constant: error_service_dependency
+DEFAULT_ERROR_SERVICE_DEPENDENCY = dict(
     attribute_id='error_context',
     module_path='tiferet.contexts.error',
     class_name='ErrorContext',
 )
 
 
-# ** constant: default_feature_repo_dependency
-DEFAULT_FEATURE_REPO_DEPENDENCY = dict(
-    attribute_id='feature_repo',
-    module_path='tiferet.proxies.feature_yaml',
-    class_name='FeatureYamlProxy',
-)
-
-
-# ** constant: default_feature_context_dependency
-DEFAULT_FEATURE_CONTEXT_DEPENDENCY = dict(
+# ** constant: default_feature_service_dependency
+DEFAULT_FEATURE_SERVICE_DEPENDENCY = dict(
     attribute_id='feature_context',
     module_path='tiferet.contexts.feature',
     class_name='FeatureContext',
-)
-
-
-# ** constant: default_app_repo_dependency
-DEFAULT_APP_REPO_DEPENDENCY = dict(
-    attribute_id='app_repo',
-    module_path='tiferet.proxies.app_yaml',
-    class_name='AppYamlProxy',
 )
 
 
@@ -88,31 +42,35 @@ DEFAULT_APP_CONTEXT_DEPENDENCY = dict(
 
 # ** constant: default_app_context_dependencies
 DEFAULT_APP_CONTEXT_DEPENDENCIES = [
-    DEFAULT_CONTAINER_CONTEXT_DEPENDENCY,
-    DEFAULT_ERROR_CONTEXT_DEPENDENCY,
-    DEFAULT_FEATURE_CONTEXT_DEPENDENCY,
-    DEFAULT_CONTAINER_REPO_DEPENDENCY,
-    DEFAULT_ERROR_REPO_DEPENDENCY,
-    DEFAULT_FEATURE_REPO_DEPENDENCY,
-    DEFAULT_APP_REPO_DEPENDENCY,
+    DEFAULT_CONTAINER_SERVICE_DEPENDENCY,
+    DEFAULT_ERROR_SERVICE_DEPENDENCY,
+    DEFAULT_FEATURE_SERVICE_DEPENDENCY,
+    dict(
+        attribute_id='container_repo',
+        module_path='tiferet.proxies.container_yaml',
+        class_name='ContainerYamlProxy',
+    ),
+    dict(
+        attribute_id='error_repo',
+        module_path='tiferet.proxies.error_yaml',
+        class_name='ErrorYamlProxy',
+    ),
+    dict(
+        attribute_id='feature_repo',
+        module_path='tiferet.proxies.feature_yaml',
+        class_name='FeatureYamlProxy',
+    ),
+    dict(
+        attribute_id='app_repo',
+        module_path='tiferet.proxies.app_yaml',
+        class_name='AppYamlProxy',
+    ),
 ]
-
-
-# ** constant: default_container_config_file
-DEFAULT_CONTAINER_CONFIG_FILE = 'tiferet/configs/container.yml'
-
-
-# ** constant: default_feature_config_file
-DEFAULT_FEATURE_CONFIG_FILE = 'tiferet/configs/features.yml'
-
-
-# ** constant: default_error_config_file
-DEFAULT_ERROR_CONFIG_FILE = 'tiferet/configs/errors.yml'
 
 
 # ** constant: default_app_context_constants
 DEFAULT_APP_CONTEXT_CONSTANTS = dict(
-    container_config_file=DEFAULT_CONTAINER_CONFIG_FILE,
-    feature_config_file=DEFAULT_FEATURE_CONFIG_FILE,
-    error_config_file=DEFAULT_ERROR_CONFIG_FILE,
+    container_config_file='tiferet/configs/container.yml',
+    feature_config_file='tiferet/configs/features.yml',
+    error_config_file='tiferet/configs/errors.yml',
 )
--- tiferet/configs/tests/test_app.py
+++ tiferet/configs/tests/test_app.py
@@ -10,38 +10,6 @@ from ...models.app import *
 
 # *** constants
 
-# ** constant: test_app_repo_dependency
-TEST_APP_REPO_DEPENDENCY = dict(
-    attribute_id='app_repo',
-    module_path='tiferet.proxies.app_mock',
-    class_name='MockAppProxy',
-)
-
-
-# ** constant: test_error_repo_dependency
-TEST_ERROR_REPO_DEPENDENCY = dict(
-    attribute_id='error_repo',
-    module_path='tiferet.proxies.tests.error_mock',
-    class_name='MockErrorProxy',
-)
-
-
-# ** constant: test_feature_repo_dependency
-TEST_FEATURE_REPO_DEPENDENCY = dict(
-    attribute_id='feature_repo',
-    module_path='tiferet.proxies.tests.feature_mock',
-    class_name='MockFeatureProxy',
-)
-
-
-# ** constant: test_container_repo_dependency
-TEST_CONTAINER_REPO_DEPENDENCY = dict(
-    attribute_id='container_repo',
-    module_path='tiferet.proxies.tests.container_mock',
-    class_name='MockContainerProxy',
-)
-
-
 # ** constant: test_app_settings
 TEST_APP_SETTINGS = dict(
     id='test',
@@ -51,12 +19,24 @@ TEST_APP_SETTINGS = dict(
     data_flag='test',
     dependencies=[
         DEFAULT_APP_CONTEXT_DEPENDENCY,
-        DEFAULT_ERROR_CONTEXT_DEPENDENCY,
-        DEFAULT_FEATURE_CONTEXT_DEPENDENCY,
-        DEFAULT_CONTAINER_CONTEXT_DEPENDENCY,
-        TEST_ERROR_REPO_DEPENDENCY,
-        TEST_FEATURE_REPO_DEPENDENCY,
-        TEST_CONTAINER_REPO_DEPENDENCY,
+        DEFAULT_ERROR_SERVICE_DEPENDENCY,
+        DEFAULT_FEATURE_SERVICE_DEPENDENCY,
+        DEFAULT_CONTAINER_SERVICE_DEPENDENCY,
+        dict(
+            attribute_id='error_repo',
+            module_path='tiferet.proxies.tests.error_mock',
+            class_name='MockErrorProxy',
+        ),
+        dict(
+            attribute_id='feature_repo',
+            module_path='tiferet.proxies.tests.feature_mock',
+            class_name='MockFeatureProxy',
+        ),
+        dict(
+            attribute_id='container_repo',
+            module_path='tiferet.proxies.tests.container_mock',
+            class_name='MockContainerProxy',
+        ),
     ],
 )
 
