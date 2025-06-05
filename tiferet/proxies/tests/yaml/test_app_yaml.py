5aa2bdd56eaae82c877604f080ffa88a4ed643b5
Ashatz <andrew@greatstrength.me>
2025-06-05 14:13:45 -0700

    Proxies - App - Test Yaml Cleanup

--- tiferet/proxies/tests/yaml/test_app_yaml.py
+++ tiferet/proxies/tests/yaml/test_app_yaml.py
@@ -5,7 +5,6 @@ import pytest
 
 # ** app
 from ...app_yaml import *
-from ....configs.tests.test_app import *
 
 
 # *** fixtures
@@ -23,24 +22,10 @@ def app_yaml_proxy(app_config_file):
         app_config_file=app_config_file
     )
 
-
-# ** fixture: app_settings
+# ** fixture: app_id
 @pytest.fixture
-def app_settings():
-    settings = ModelObject.new(
-        AppSettings,
-        **TEST_APP_SETTINGS,
-    )
-
-    # Set the test_app_yaml_proxy id.
-    settings.id = 'test_app_yaml_proxy'
-    settings.name = 'Test App YAML Proxy'
-    settings.description = 'The context for testing the app yaml proxy.'
-    settings.feature_flag = 'test_app_yaml_proxy'
-    settings.data_flag = 'test_app_yaml_proxy'
-
-    # Return the settings.
-    return settings
+def app_id():
+    return 'test_app_yaml_proxy'
 
 
 # *** tests
@@ -60,21 +45,21 @@ def test_app_yaml_proxy_list_settings(app_yaml_proxy, app_settings):
 
 
 # ** test: app_yaml_proxy_get_settings
-def test_app_yaml_proxy_get_settings(app_yaml_proxy, app_settings):
+def test_app_yaml_proxy_get_settings(app_yaml_proxy, app_id):
     '''
     Test the app YAML proxy get settings method.
     '''
 
-    # Get the interface.
-    settings = app_yaml_proxy.get_settings(app_settings.id)
+    # Get the interface.s
+    settings = app_yaml_proxy.get_settings(app_id)
 
     # Check the interface.
     assert settings
-    assert settings.id == app_settings.id
-    assert settings.name == app_settings.name
-    assert settings.description == app_settings.description
-    assert settings.feature_flag == app_settings.feature_flag
-    assert settings.data_flag == app_settings.data_flag
+    assert settings.id == 'test_app_yaml_proxy'
+    assert settings.name == 'Test App YAML Proxy'
+    assert settings.description == 'The context for testing the app yaml proxy.'
+    assert settings.feature_flag == 'test_app_yaml_proxy'
+    assert settings.data_flag == 'test_app_yaml_proxy'
     assert len(settings.attributes) == 1
     assert settings.attributes[0].attribute_id == 'test_attribute'
     assert settings.attributes[0].module_path == 'test_module_path'
