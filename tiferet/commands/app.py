1e1abd32159608070466ab9fa549a16eb8d0ee71
Andrew <andrew@greatstrength.me>
2025-06-22 10:11:31 -0700

    Commands - Get App Interface Command (App) - 9425431577 (#116)
    * Commands - App - Get App Interface Command
    
    * Commands - App - Get App Interface Command Tests

--- tiferet/commands/app.py
+++ tiferet/commands/app.py
@@ -3,7 +3,7 @@
 # ** app
 from .settings import *
 from .core import import_dependency
-from ..contracts.app import AppRepository
+from ..contracts.app import AppRepository, AppInterface
 
 
 # *** commands:
@@ -54,4 +54,47 @@ class ImportAppRepository(Command):
                 f'Failed to import app repository: {e}.',
                 str(e)
             )
-       
+
+
+# ** command: get_app_interface
+class GetAppInterface(Command):
+    '''
+    A command to get the application interface by its ID.
+    '''
+
+    def __init__(self, app_repo: AppRepository):
+        '''
+        Initialize the LoadAppInterface command.
+
+        :param app_repo: The application repository instance.
+        :type app_repo: AppRepository
+        '''
+        
+        # Set the application repository.
+        self.app_repo = app_repo
+
+    # * method: execute
+    def execute(self, interface_id: str, **kwargs) -> AppInterface:
+        '''
+        Execute the command to load the application interface.
+
+        :param interface_id: The ID of the application interface to load.
+        :type interface_id: str
+        :param kwargs: Additional keyword arguments.
+        :type kwargs: dict
+        :return: The loaded application interface.
+        :rtype: AppInterface
+        :raises TiferetError: If the interface cannot be found.
+        '''
+
+        # Load the application interface.
+        # Raise an error if the interface is not found.
+        interface = self.app_repo.get_interface(interface_id)
+        if not interface:
+            self.raise_error(
+                'APP_INTERFACE_NOT_FOUND',
+                f'App interface with ID {interface_id} not found.'
+            )
+
+        # Return the loaded application interface.
+        return interface
