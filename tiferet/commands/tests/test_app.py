1b90fd18bb0b4b6da8e613e067e597dea99e2273
Andrew <andrew@greatstrength.me>
2025-06-22 10:11:31 -0700

    Commands - Get App Interface Command (App) - 9425431577 (#116)
    * Commands - App - Get App Interface Command
    
    * Commands - App - Get App Interface Command Tests

--- tiferet/commands/tests/test_app.py
+++ tiferet/commands/tests/test_app.py
@@ -2,9 +2,11 @@
 
 # ** infra
 import pytest
+from unittest import mock
 
 # ** app
 from ..app import *
+from ...models.app import Entity, ValueObject, AppInterface, AppAttribute
 
 
 # *** fixtures
@@ -19,6 +21,68 @@ def import_app_repo_cmd():
     :rtype: ImportAppRepository
     '''
     return ImportAppRepository()
+
+
+# ** app_interface
+@pytest.fixture
+def app_interface():
+    '''
+    Fixture to create a mock AppInterface instance.
+    
+    :return: A mock instance of AppInterface.
+    :rtype: AppInterface
+    '''
+    # Create a test AppInterface instance.
+    return Entity.new(
+        AppInterface,
+        id='test',
+        name='Test App',
+        module_path='tiferet.contexts.app',
+        class_name='AppContext',
+        description='The test app.',
+        feature_flag='test',
+        data_flag='test',
+        attributes=[
+            ValueObject.new(
+                AppAttribute,
+                attribute_id='test_attribute',
+                module_path='test_module_path',
+                class_name='test_class_name',
+            ),
+        ],
+    )
+
+
+# ** fixture: app_repo
+@pytest.fixture
+def app_repo(app_interface):
+    '''
+    Fixture to create a mock AppRepository instance.
+    
+    :return: A mock instance of AppRepository.
+    :rtype: AppRepository
+    '''
+
+    # Create a mock AppRepository instance.
+    app_repo = mock.Mock(spec=AppRepository)
+    app_repo.config_file = 'tiferet/configs/test.yml'
+    app_repo.get_interface.return_value = app_interface
+    return app_repo
+
+
+# ** fixture: get_app_interface_cmd
+@pytest.fixture
+def get_app_interface_cmd(app_repo):
+    '''
+    Fixture to create an instance of GetAppInterface command.
+    
+    :param app_repo: The mock AppRepository instance.
+    :type app_repo: AppRepository
+    :return: An instance of GetAppInterface.
+    :rtype: GetAppInterface
+    '''
+    # Create an instance of GetAppInterface with the mock app repository.
+    return GetAppInterface(app_repo=app_repo)
 
 
 # *** tests
@@ -69,3 +133,41 @@ def test_import_app_repo_success(import_app_repo_cmd):
     assert hasattr(app_repo, 'config_file'), 'AppRepository should have app_config_file attribute'
     config_file = getattr(app_repo, 'config_file', None)
     assert config_file == 'tiferet/configs/test.yml', 'AppRepository should have the correct config file'
+
+
+# ** test: test_get_app_interface_not_found
+def test_get_app_interface_not_found(app_repo, get_app_interface_cmd):
+    '''
+    Test the GetAppInterface command when the app interface is not found.
+    
+    :param get_app_interface_cmd: The GetAppInterface command instance.
+    :type get_app_interface_cmd: GetAppInterface
+    '''
+
+    app_repo.get_interface.return_value = None  # Simulate that the interface is not found.
+
+    # Attempt to get an app interface that does not exist.
+    with pytest.raises(TiferetError) as exc_info:
+        get_app_interface_cmd.execute(interface_id='non_existent_id')
+    
+    # Assert that the error message contains the expected text.
+    assert exc_info.value.error_code == 'APP_INTERFACE_NOT_FOUND'
+    assert 'App interface with ID non_existent_id not found.' in str(exc_info.value)
+
+
+# ** test: test_get_app_interface_success
+def test_get_app_interface_success(get_app_interface_cmd, app_interface):
+    '''
+    Test the GetAppInterface command when the app interface is found.
+    
+    :param get_app_interface_cmd: The GetAppInterface command instance.
+    :type get_app_interface_cmd: GetAppInterface
+    :param app_interface: The mock AppInterface instance.
+    :type app_interface: AppInterface
+    '''
+
+    # Execute the command to get the app interface.
+    result = get_app_interface_cmd.execute(interface_id='test')
+
+    # Assert that the returned interface matches the expected app interface.
+    assert result == app_interface, 'Should return the correct AppInterface instance'
