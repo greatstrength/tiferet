ee3662f24e59c438ab9346c5280a327c62ac1ddc
Ashatz <andrew@greatstrength.me>
2025-06-27 19:47:00 -0700

    Contexts - Container Context Enhancements - 9379567341

--- tiferet/contexts/container.py
+++ tiferet/contexts/container.py
@@ -1,50 +1,13 @@
 # *** imports
 
 # ** core
-import os
-from typing import Any
+from typing import Any, List
 
 # ** app
-from ..domain import *
-from ..repos.container import ContainerRepository
-
-
-# *** functions
-
-# ** function: create_injector
-def create_injector(name: str, **dependencies) -> Any:
-    '''
-    Create an injector object with the given dependencies.
-
-    :param name: The name of the injector.
-    :type name: str
-    :param dependencies: The dependencies.
-    :type dependencies: dict
-    :return: The injector object.
-    :rtype: Any
-    '''
-
-    # Create container.
-    from dependencies import Injector
-    return type(f'{name.capitalize()}Container', (Injector,), {**dependencies})
-
-
-# ** function: import_dependency
-def import_dependency(module_path: str, class_name: str) -> Any:
-    '''
-    Import an object dependency from its configured Python module.
-
-    :param module_path: The module path.
-    :type module_path: str
-    :param class_name: The class name.
-    :type class_name: str
-    :return: The dependency.
-    :rtype: Any
-    '''
-
-    # Import module.
-    from importlib import import_module
-    return getattr(import_module(module_path), class_name)
+from .cache import CacheContext
+from ..handlers.container import ContainerService
+from ..commands import *
+from ..commands.dependencies import *
 
 
 # *** contexts
@@ -55,178 +18,117 @@ class ContainerContext(Model):
     A container context is a class that is used to create a container object.
     '''
 
-    # * attribute: interface_id
-    interface_id = StringType(
-        required=True,
-        metadata=dict(
-            description='The interface ID.'
-        ),
-    )
+    # * attribute: cache
+    cache: CacheContext
 
-    # * attribute: attributes
-    attributes = DictType(
-        ModelType(ContainerAttribute), 
-        default={}, 
-        required=True,
-        metadata=dict(
-            description='The container attributes.'
-        ),
-    )
+    # * attribute: container_service
+    container_service: ContainerService
 
-    # * attribute: constants
-    constants = DictType(
-        StringType, 
-        default={},
-        metadata=dict(
-            description='The container constants.'
-        ),
-    )
-
-    # * attribute: feature_flag
-    feature_flag = StringType(
-        required=True,
-        default='core',
-        metadata=dict(
-            description='The feature flag.'
-        ),
-    )
-
-    # * attribute: data_flag
-    data_flag = StringType(
-        required=True,
-        metadata=dict(
-            description='The data flag.'
-        ),
-    )
 
     # * method: init
-    def __init__(self, interface_id: str, container_repo: ContainerRepository, feature_flag: str, data_flag: str):
+    def __init__(self, container_service: ContainerService, cache: CacheContext = None):
         '''
         Initialize the container context.
 
-        :param interface_id: The interface ID.
-        :type interface_id: str
-        :param container_repo: The container repository.
-        :type container_repo: ContainerRepository
-        :param interface_flag: The interface flag.
-        :type interface_flag: str
-        :param feature_flag: The feature flag.
-        :type feature_flag: str 
-        :param data_flag: The data flag.
-        :type data_flag: str
+        :param container_service: The container service to use for executing container requests.
+        :type container_service: ContainerService
+        :param cache: The cache context to use for caching container data.
+        :type cache: CacheContext
         '''
 
-        # Add the attributes and constants as empty dictionaries.
-        attributes = {}
-        constants = {}
-        
-        # Get and set attributes and constants.
-        attrs, consts = container_repo.list_all()
-
-        # Parse the constants.
-        constants.update({key: self.parse_parameter(consts[key]) for key in consts})
-        
-        # Add the attributes to the context.
-        for attr in attrs:
-            
-            # If the attribute already exists, set the dependencies.
-            if attr.id in attributes:
-                for dep in attr.dependencies:
-                    attr.set_dependency(dep)
-                    continue
-
-            # Otherwise, add the attribute.
-            attributes[attr.id] = attr
-
-            # Add any parameters as constants.
-            for dep in attr.dependencies:
-                for key in dep.parameters:
-                    constants[key] = self.parse_parameter(dep.parameters[key])
-
-        # Add the constants and attributes to the context.
-        super().__init__(dict(
-            interface_id=interface_id,
-            feature_flag=feature_flag,
-            data_flag=data_flag,
-            attributes=attributes,
-            constants=constants,
-        ))
-
+        # Assign the attributes.
+        self.container_service = container_service
+        self.cache = cache if cache else CacheContext()
     
-    # * method: parse_environment_parameter
-    def parse_parameter(self, parameter: str) -> str:
-
-        # If the parameter is an environment variable, get the value.
-        if parameter.startswith('$env.'):
-            return os.getenv(parameter[5:])
-        
-        # Otherwise, return the parameter.
-        return parameter
-
-    # * method: get_dependency
-    def get_dependency(self, attribute_id: str):
+    # * method: create_cache_key
+    def create_cache_key(self, flags: List[str] = None) -> str:
         '''
-        Get a dependency from the container.
+        Create a cache key for the container.
 
-        :param attribute_id: The attribute id of the dependency.
-        :type attribute_id: str
-        :return: The attribute value.
-        :rtype: Any
+        :param flags: The feature or data flags to use.
+        :type flags: List[str]
+        :return: The cache key.
+        :rtype: str
         '''
 
-        # Create the injector.
-        injector = self.create_injector()
+        # Create the cache key from the flags.
+        return f"feature_container{'_' + '_'.join(flags) if flags else ''}"
 
-        # Get attribute.
-        return getattr(injector, attribute_id)
-
-    # * method: create_injector
-    def create_injector(self, **kwargs) -> Any:
+    # * method: build_injector
+    def build_injector(self,
+            flags: List[str] = None,
+        ) -> Injector:
         '''
-        Add a container to the context.
+        Build the container injector.
 
-        :param kwargs: Additional keyword arguments.
-        :type kwargs: dict
+        :param flags: The feature or data flags to use.
+        :type flags: List[str]
         :return: The container injector object.
-        :rtype: Any
+        :rtype: Injector
         '''
 
-        # Import dependencies.
+        # Create the cache key for the injector from the flags.
+        cache_key = self.create_cache_key(flags)
+
+        # Check if the injector is already cached.
+        cached_injector = self.cache.get(cache_key)
+        if cached_injector:
+            return cached_injector
+
+        # Get all attributes and constants from the container service.
+        attributes, constants = self.container_service.list_all()
+
+        # Load constants from the attributes.
+        constants = self.container_service.load_constants(attributes, constants, flags)
+
+        # Create the dependencies for the injector.
         dependencies = {}
-        for attribute_id in self.attributes:
-            attribute = self.attributes[attribute_id]
-            flag_map = dict(
-                feature=self.feature_flag,
-                data=self.data_flag,
-            )
-            dependencies[attribute_id] = self.import_dependency(attribute, flag_map[attribute.type])
+        for attr in attributes:
+            try:
+                dependencies[attr.id] = self.container_service.get_dependency_type(attr, flags)
+            except TiferetError as e:
+                raise e
+                    
 
-        # Create container.
-        return create_injector( 
-            self.interface_id, 
-            **self.constants, 
-            **dependencies, 
-            **kwargs)
+        # Create the injector with the dependencies and constants.
+        injector = create_injector.execute(
+            cache_key,
+            dependencies=dependencies,
+            **constants
+        )
 
-    # * method: import_dependency
-    def import_dependency(self, attribute: ContainerAttribute, flag: str) -> Any:
+        # Cache the injector.
+        self.cache.set(cache_key, injector)
+
+        # Return the injector.
+        return injector
+    
+    # * method: get_dependency
+    def get_dependency(self, attribute_id: str, flags: List[str] = []) -> Any:
         '''
-        Import a container attribute dependency from its configured Python module.
+        Get an injector dependency by its attribute ID.
 
-        :param attribute: The container attribute.
-        :type attribute: ContainerAttribute
-        :param flag: The flag for the dependency.
-        :type flag: str
-        :return: The dependency.
+        :return: The container attribute.
         :rtype: Any
         '''
 
-        # Get the dependency.
-        dependency = attribute.get_dependency(flag)
+        # Get the cached injector.
+        injector = self.build_injector(flags)
 
-        # If there is no dependency and the attribute is a feature, import the default feature.
-        if not dependency and attribute.type == 'feature':
-            dependency = attribute.get_dependency('core')
+        # Get the dependency from the injector.
+        dependency = get_dependency.execute(
+            injector=injector,
+            dependency_name=attribute_id,
+        )
+        
+        # Raise an error if the dependency is not found.
+        if not dependency:
+            raise_error.execute(
+                'CONTAINER_DEPENDENCY_NOT_FOUND',
+                f'Dependency with ID {attribute_id} not found in container with flags {flags}.',
+                attribute_id,
+                flags
+            )
 
-        # Import the dependency.
-        return import_dependency(dependency.module_path, dependency.class_name)
+        # Return the dependency.
+        return dependency
