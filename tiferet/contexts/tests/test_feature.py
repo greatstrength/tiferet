4d7d2cd18daf8753ec3b04b3efd72e2a797afdac
Ashatz <andrew@greatstrength.me>
2025-06-30 17:11:59 -0700

    Contexts - Feature Context Enhancements - Tests - 9487946438

--- tiferet/contexts/tests/test_feature.py
+++ tiferet/contexts/tests/test_feature.py
@@ -1,143 +1,262 @@
 # *** imports
 
-# ** core
-import json
 
 # ** infra
 import pytest
 from unittest import mock
 
-# ** app 
+# ** app
 from ..feature import *
-from ...configs import TiferetError
+from ...models.feature import *
 
 
 # *** fixtures
 
-# ** fixture: request_context
+# ** fixture: feature_service
 @pytest.fixture
-def request_context():
-    """
-    Fixture to provide a request context for testing.
-    """
-    return RequestContext(
-        feature_id="test_group.test_feature",
-        headers={"Content-Type": "application/json"},
-        data=dict(
-            test_key="test_value"
-        )
-    )
-
-# ** fixture: feature
-@pytest.fixture
-def feature():
-    """
-    Test feature object.
-    """
-    return Feature.new(
-        id="test_group.test_feature",
-        name="Test Feature",
-        group_id="test_group",
-        feature_key="test_feature",
-        description="A test feature for unit testing.",
-        commands=[
-            ValueObject.new(
-                FeatureCommand,
-                attribute_id="test_feature_command",
-                name="Test Feature Command",
-                parameters=dict(
-                    param1="value1",
-                    param2="value2"
-                ),
-            )
-        ]
-    )
-
-
-# ** fixture: feature_handler
-@pytest.fixture
-def feature_handler(feature, request_context):
-    """
-    Fixture to provide a mock feature handler.
-    """
-
-    # Set the result for the request context.
-    request_context.result = json.dumps(['value1', 'value2'])
-
-    # Mock the feature handler to return the test feature.
-    handler = mock.Mock(spec=FeatureHandler)
-    handler.get_feature.return_value = feature
-    handler.handle_command.return_value = request_context
-    return handler
-
-
-# ** fixture: feature_context
-@pytest.fixture
-def feature_context(feature_handler):
-    """
-    Fixture to provide a FeatureContext instance for testing.
-    """
-    return FeatureContext(
-        feature_handler=feature_handler
-    )
-
-# ** fixture: feature_handler
-@pytest.fixture
-def feature_repo(feature):
-    """
-    Fixture to provide a mock feature repository.
-    """
+def feature_service():
+    """Fixture to provide a mock feature service."""
     
-    # Mock the repository to return the test feature.
-    repo = mock.Mock(spec=FeatureRepository)
-    repo.get.return_value = feature
-    return repo
+    # Create a mock feature service.
+    service = mock.Mock(spec=FeatureService)
+    
+    # Return the mock feature service.
+    return service
 
 
 # ** fixture: container_context
 @pytest.fixture
-def container_context():
-    """
-    Fixture to provide a mock container context.
-    """
-    return mock.Mock(spec=ContainerContext)
+def container_context(test_command):
+    """Fixture to provide a mock container context."""
+    
+    # Create a mock container context.
+    container_context = mock.Mock(spec=ContainerContext)
+
+    # Set the container service to return the test command when requested.
+    container_context.get_dependency.return_value = test_command
+    
+    # Return the mock container context.
+    return container_context
 
 
-# *** tests 
-
-# ** test: test_init_feature_context
-def test_init_feature_context(feature_repo, container_context):
-    """
-    Test initializing the FeatureContext with a feature repository and container context.
-    """
-    feature_context = FeatureContext(
-        feature_repo=feature_repo,
-        container_context=container_context
+# ** fixture: feature_context
+@pytest.fixture
+def feature_context(feature_service, container_context):
+    """Fixture to provide an instance of FeatureContext."""
+    
+    # Create an instance of FeatureContext with the mock feature service and container context.
+    return FeatureContext(
+        feature_service=feature_service, 
+        container=container_context
     )
+
+
+# ** fixture: test_command
+@pytest.fixture
+def test_command():
+
+    class TestCommand(Command):
+        """A mock command for testing purposes."""
+        
+        def execute(self, key: str, param: str = None, **kwargs) -> Any:
+            """Mock execute method that returns a test response."""
+            
+            # Verify that the request exists.
+            self.verify(key, 'KEY_NOT_FOUND', 'No key provided for command execution.')
+            
+            # Mock response data.
+            if not param:
+                return {"status": "success", "data": {"key": key}}
+            return {"status": "success", "data": {"key": key, "param": param}}
+        
+    # Return an instance of the mock command.
+    return TestCommand()
+
+
+# ** fixture: feature
+@pytest.fixture
+def feature():
+
+    return ModelObject.new(
+        Feature,
+        id='test_group.test_feature',
+        group_id='test_group',
+        feature_key='test_feature',
+        name='Test Feature',
+        description='A feature for testing purposes.',
+        commands=[]
+    )
+
+
+# *** tests
+
+# ** test: feature_context_load_feature_command
+def test_feature_context_load_feature_command(feature_context, test_command):
+    """Test loading a feature command from the FeatureContext."""
+
+    # Load the feature command using the feature context.
+    command = feature_context.load_feature_command('test_command')
     
-    # Assert the feature handler is initialized correctly.
-    assert isinstance(feature_context.feature_handler, FeatureHandler)
-    assert feature_context.feature_handler.feature_repo == feature_repo
-    assert feature_context.feature_handler.container_service == container_context
+    # Assert that the loaded command is the same as the test command.
+    assert command == test_command
+    assert command.execute(key='value') == {"status": "success", "data": {"key": "value"}}
 
 
-# ** test: test_init_feature_context_without_repo_or_container
-def test_init_feature_context_without_repo_or_container():
-    """
-    Test initializing the FeatureContext without a feature repository or container context.
-    """
+# ** test: feature_context_load_feature_command_failed
+def test_feature_context_load_feature_command_failed(feature_context, container_context):
+    """Test loading a feature command that does not exist in the FeatureContext."""
+    
+    # Add a side effect to the container context to raise an exception when trying to get a non-existent command.
+    container_context.get_dependency.side_effect = TiferetError(
+        'TEST_ERROR',
+        'Feature command not found in container: non_existent_command',
+    )
+
+    # Attempt to load a non-existent feature command.
     with pytest.raises(TiferetError) as exc_info:
-        FeatureContext()
+        feature_context.load_feature_command('non_existent_command')
     
-    # Assert the exception is raised.
-    assert exc_info.value.error_code == 'FEATURE_CONTEXT_LOADING_FAILED'
-    assert 'A feature repository and container context are required to initialize the feature context.' in str(exc_info.value)
+    # Assert that the exception message is as expected.
+    assert exc_info.value.error_code == 'FEATURE_COMMAND_LOADING_FAILED'
+    assert 'Failed to load feature command attribute: non_existent_command' in str(exc_info.value)
 
-# ** test: test_execute_feature_success
-def test_execute_feature_success(feature_context, request_context):
 
-    # Test executing a feature that sets result
-    feature_context.execute(request_context)
+# ** test: feature_context_handle_command
+def test_feature_context_handle_command(feature_context, test_command):
+    """Test handling a command in the FeatureContext."""
+    
+    # Create a mock request.
+    request = ModelObject.new(Request, data={"key": "value"})
+    
+    # Handle the command using the feature context.
+    feature_context.handle_command(test_command, request)
+    response = request.handle_response()
+    
+    # Assert that the response matches the expected output.
+    assert response == {"status": "success", "data": {"key": "value"}}
 
-    # Assert the result.
-    assert request_context.result == json.dumps(['value1', 'value2'])
+
+# ** test: feature_context_handle_command_with_error
+def test_feature_context_handle_command_with_error(feature_context, test_command):
+    """Test handling a command that raises an error in the FeatureContext."""
+    
+    # Create a mock request that will raise an error.
+    request = ModelObject.new(Request, data={'key': None})
+    
+    # Attempt to handle the command and catch the raised error.
+    with pytest.raises(TiferetError) as exc_info:
+        feature_context.handle_command(test_command, request)
+    
+    # Assert that the exception message is as expected.
+    assert exc_info.value.error_code == 'KEY_NOT_FOUND'
+    assert 'No key provided for command execution.' in str(exc_info.value)
+
+
+# ** test: feature_context_handle_command_with_data_key
+def test_feature_context_handle_command_with_data_key(feature_context, test_command):
+    """Test handling a command with a data key in the FeatureContext."""
+    
+    # Create a mock request with a data key.
+    request = ModelObject.new(Request, data={"key": "value"})
+    
+    # Handle the command using the feature context.
+    feature_context.handle_command(test_command, request, data_key="response_data")
+    
+    # Assert that the response matches the expected output.
+    assert request.data.get('response_data') == {"status": "success", "data": {"key": "value"}}
+
+
+# ** test: feature_context_handle_command_with_pass_on_error
+def test_feature_context_handle_command_with_pass_on_error(feature_context, test_command):
+    """Test handling a command with pass_on_error in the FeatureContext."""
+    
+    # Create a mock request that will raise an error.
+    request = ModelObject.new(Request, data={'key': None})
+    
+    # Handle the command with pass_on_error set to True.
+    feature_context.handle_command(test_command, request, pass_on_error=True)
+    
+    # Assert that the request handled the error without raising an exception.
+    assert not request.handle_response()
+
+
+# ** test: feature_context_execute_feature
+def test_feature_context_execute_feature(feature_context, feature_service, feature):
+
+    # Create a standard feature command with no data key or pass on error.
+    feature_command = ModelObject.new(
+        FeatureCommand,
+        attribute_id='test_command',
+        name='Test Command'
+    )
+
+    # Add it to the feature and set as the feature service's return value.
+    feature.add_command(feature_command)
+    feature_service.get_feature.return_value = feature
+
+    # Create a mock request.
+    request = ModelObject.new(Request, data={"key": "value"})
+
+    # Execute the feature using the feature context.
+    feature_context.execute_feature(feature.id, request)
+
+    # Assert that the request handled the response correctly.
+    assert request.handle_response() == {"status": "success", "data": {"key": "value"}}
+
+
+# ** test: feature_context_execute_feature_with_data_key_parameter
+def test_feature_context_execute_feature_with_request_parameter(feature_context, feature_service, feature):
+    """Test executing a feature with a request parameter in the FeatureContext."""
+    
+    # Create a standard feature command with a data key.
+    feature_command = ModelObject.new(
+        FeatureCommand,
+        attribute_id='test_command',
+        name='Test Command',
+        parameters=dict(
+            param='$r.key',
+        ),
+        data_key='response_data'
+    )
+
+    # Add it to the feature and set as the feature service's return value.
+    feature.add_command(feature_command)
+    feature_service.get_feature.return_value = feature
+    feature_service.parse_parameter.return_value = 'value'
+
+    # Create a mock request.
+    request = ModelObject.new(Request, data={"key": "value"})
+
+    # Execute the feature using the feature context with a data key.
+    feature_context.cache.clear()  # Clear the cache to ensure fresh execution.
+    feature_context.execute_feature(feature.id, request)
+
+    # Assert that the response is stored in the request data under the specified key.
+    assert request.data.get('response_data') == {"status": "success", "data": {"key": "value", "param": "value"}}
+
+
+# ** test: feature_context_execute_feature_with_pass_on_error
+def test_feature_context_execute_feature_with_pass_on_error(feature_context, feature_service, feature):
+    """Test executing a feature with pass_on_error in the FeatureContext."""
+    
+    # Create a standard feature command with pass_on_error set to True.
+    feature_command = ModelObject.new(
+        FeatureCommand,
+        attribute_id='test_command',
+        name='Test Command',
+        pass_on_error=True
+    )
+
+    # Add it to the feature and set as the feature service's return value.
+    feature.add_command(feature_command)
+    feature_service.get_feature.return_value = feature
+
+    # Create a mock request that will raise an error.
+    request = ModelObject.new(Request, data={'key': None})
+
+    # Execute the feature using the feature context.
+    feature_context.cache.clear()  # Clear the cache to ensure fresh execution.
+    feature_context.execute_feature(feature.id, request)
+
+    # Assert that the request handled the error without raising an exception.
+    assert not request.handle_response()
