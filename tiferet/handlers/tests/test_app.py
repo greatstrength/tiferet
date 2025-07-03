4ce867b3a28bb9c934ec11aa2305518c76ad207c
Andrew <andrew@greatstrength.me>
2025-07-03 14:58:44 -0700

    Handlers - Load App Instance with Default Attributes - Tests - 9518951460 (#248)

--- tiferet/handlers/tests/test_app.py
+++ tiferet/handlers/tests/test_app.py
@@ -1,11 +1,14 @@
 # *** imports
 
-# ** core
+# ** infra
 import pytest
+from unittest import mock
 
 # ** app
-from ..app import *
-
+from ..app import AppHandler, AppRepository, TiferetError
+from ...configs.app import DEFAULT_ATTRIBUTES
+from ...contexts.app import AppInterfaceContext
+from ...models.app import *
 
 # *** fixtures
 
@@ -23,6 +26,35 @@ def settings():
         }
     }
 
+# ** app_interface
+@pytest.fixture
+def app_interface():
+    '''
+    Fixture to create a mock AppInterface instance.
+    
+    :return: A mock instance of AppInterface.
+    :rtype: AppInterface
+    '''
+    # Create a test AppInterface instance.
+    return ModelObject.new(
+        AppInterface,
+        id='test',
+        name='Test App',
+        module_path='tiferet.contexts.app',
+        class_name='AppInterfaceContext',
+        description='The test app.',
+        feature_flag='test',
+        data_flag='test',
+        attributes=[ModelObject.new(
+            AppAttribute,
+            attribute_id='container_repo',
+            module_path='tiferet.proxies.yaml.container',
+            class_name='ContainerYamlProxy',
+            parameters={
+                'container_config_file': 'app/configs/container.yml'
+            }
+        )],
+    )
 
 # ** fixture: app_handler
 @pytest.fixture
@@ -30,8 +62,8 @@ def app_handler():
     
     return AppHandler()
 
-
 # *** tests
+
 # ** test: app_handler_load_app_repository_default
 def test_app_handler_load_app_repository_default(app_handler):
     """Test loading the default app repository."""
@@ -43,7 +75,6 @@ def test_app_handler_load_app_repository_default(app_handler):
     assert app_repo
     assert isinstance(app_repo, AppRepository)
 
-
 # ** test: app_handler_load_app_repository_custom
 def test_app_handler_load_app_repository_custom(app_handler, settings):
     """Test loading a custom app repository with specific settings."""
@@ -54,7 +85,6 @@ def test_app_handler_load_app_repository_custom(app_handler, settings):
     # Assert that the app repository is an instance of AppYamlProxy.
     assert app_repo
     assert isinstance(app_repo, AppRepository)
-
 
 # ** test: app_handler_load_app_repository_invalid
 def test_app_handler_load_app_repository_invalid(app_handler):
@@ -70,3 +100,21 @@ def test_app_handler_load_app_repository_invalid(app_handler):
     # Assert that the error message is as expected.
     assert 'Failed to import app repository' in str(exc_info.value)
     assert exc_info.value.error_code == 'APP_REPOSITORY_IMPORT_FAILED'
+
+# ** test: app_handler_load_app_instance
+def test_app_handler_load_app_instance(app_handler, app_interface):
+    """Test loading an app instance with the app handler."""
+    
+    # Load the app interface default attributes.
+    default_attrs = [ModelObject.new(
+        AppAttribute,
+        **attr_data,
+        validate=False
+    ) for attr_data in DEFAULT_ATTRIBUTES]
+    
+    # Load the app instance using the app handler.
+    app_instance = app_handler.load_app_instance(app_interface, default_attrs)
+    
+    # Assert that the app instance is not None and is of type AppInterfaceContext.
+    assert app_instance
+    assert isinstance(app_instance, AppInterfaceContext)
