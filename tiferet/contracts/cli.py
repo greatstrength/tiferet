4dc7ffdaecfc16a23722e66fbbd0ae55c82f0d31
Ashatz <andrew@greatstrength.me>
2025-07-08 08:35:34 -0700

    Context Enhancements - 07/08/2025

--- tiferet/contracts/cli.py
+++ tiferet/contracts/cli.py
@@ -1,5 +1,8 @@
 # *** imports
 
+# ** core
+from typing import List, Dict, Any
+
 # ** app
 from .settings import *
 
@@ -10,14 +13,77 @@ class CliArgument(ModelContract):
     '''
     A contract representing a command line argument.
     '''
-    pass
+    
+    # * attribute: name_or_flags
+    name_or_flags: List[str]
+
+    # * attribute: description
+    description: str
+
+    # * attribute: required
+    required: bool
+
+    # * attribute: default
+    default: str
+
+    # * attribute: choices
+    choices: List[str]
+
+    # * attribute: nargs
+    nargs: str
+
+    # * attribute: action
+    action: str
+
+    # * method: get_type
+    def get_type(self) -> str |int | float:
+        '''
+        Get the type of the argument.
+
+        :return: The type of the argument.
+        :rtype: str | int | float
+        '''
+        raise NotImplementedError('get_type method must be implemented in the CliArgument contract.')
 
 # ** contract: cli_command
 class CliCommand(ModelContract):
     '''
     A contract representing a command in the command line interface.
     '''
-    pass
+    
+    # * attribute: id
+    id: str
+
+    # * attribute: name
+    name: str
+
+    # * attribute: description
+    description: str
+
+    # * attribute: arguments
+    arguments: List[CliArgument]
+
+    # * method: has_argument
+    def has_argument(self, flags: List[str]) -> bool:
+        '''
+        Check if the command has an argument with the given flags.
+
+        :param flags: The flags to check for.
+        :type flags: List[str]
+        :return: True if the command has the argument, False otherwise.
+        :rtype: bool
+        '''
+        raise NotImplementedError('has_argument method must be implemented in the CliCommand contract.')
+    
+    # * method: add_argument
+    def add_argument(self, cli_argument: CliArgument):
+        '''
+        Add an argument to the command.
+
+        :param cli_argument: The CLI argument to add.
+        :type cli_argument: CliArgument
+        '''
+        raise NotImplementedError('add_argument method must be implemented in the CliCommand contract.')
 
 # ** contract: cli_repository
 class CliRepository(Repository):
@@ -39,6 +105,17 @@ class CliRepository(Repository):
         '''
         raise NotImplementedError('get_command method must be implemented in the CLI repository.')
 
+    # * method: get_parent_arguments
+    @abstractmethod
+    def get_parent_arguments(self) -> List[CliArgument]:
+        '''
+        Get the parent arguments for the command line interface.
+
+        :return: A list of parent arguments.
+        :rtype: List[CliArgument]
+        '''
+        raise NotImplementedError('get_parent_arguments method must be implemented in the CLI repository.')
+
 # ** contract: cli_service
 class CliService(Service):
     '''
@@ -59,3 +136,16 @@ class CliService(Service):
         :rtype: CliCommand
         '''
         raise NotImplementedError('get_command method must be implemented in the CLI service.')
+    
+    # * method: parse_arguments
+    @abstractmethod
+    def parse_arguments(self, cli_command: CliCommand) -> Dict[str, Any]:
+        '''
+        Parse the command line arguments for a given CLI command.
+        
+        :param cli_command: The CLI command to parse arguments for.
+        :type cli_command: CliCommand
+        :return: A dictionary of parsed arguments.
+        :rtype: Dict[str, Any]
+        '''
+        raise NotImplementedError('parse_arguments method must be implemented in the CLI service.')
