2d5efb4d142d22763635f4c4b66766bb47575355
Andrew <andrew@greatstrength.me>
2025-09-16 18:21:48 -0700

    Tiferet - Remove Request Models - 10064432363 (#320)
    * Models - Feature - Remove Feature Models
    
    * Models - CLI - Remove Request Models
    
    * Models - Tests (CLI) - Remove Request Models
    
    * Handlers - Tests (Feature) - Add Request Context

--- tiferet/models/feature.py
+++ tiferet/models/feature.py
@@ -1,87 +1,9 @@
 # *** imports
 
-# ** core
-from typing import Any
-import json
-
 # ** app
 from .settings import *
 
-
 # *** models
-
-# ** model: request
-class Request(ValueObject):
-    '''
-    A request object.
-    '''
-
-    # * attribute: headers
-    headers = DictType(
-        StringType(),
-        metadata=dict(
-            description='The request headers.'
-        )
-    )
-
-    # * attribute: data
-    data = DictType(
-        StringType(),
-        metadata=dict(
-            description='The request data.'
-        )
-    )
-
-    # * attribute: result
-    result = StringType(
-        metadata=dict(
-            description='The request result.'
-        )
-    )
-
-    # * method: set_result
-    def set_result(self, result: Any):
-        # Set the result as a serialized empty dictionary if it is None.
-        if not result:
-            self.result = json.dumps({})
-            return
-            
-        # If the result is a Model, convert it to a primitive dictionary and serialize it.
-        if isinstance(result, ModelObject):
-            self.result = json.dumps(result.to_primitive())
-            return
-
-        # If the result is not a list, it must be a dict, so serialize it and set it.
-        if type(result) != list:
-            self.result = json.dumps(result)
-            return
-
-        # If the result is a list, convert each item to a primitive dictionary.
-        result_list = []
-        for item in result:
-            if isinstance(item, ModelObject):
-                result_list.append(item.to_primitive())
-            else:
-                result_list.append(item)
-
-        # Serialize the result and set it.
-        self.result = json.dumps(result_list)
-
-    # * method: handle_response
-    def handle_response(self, **kwargs) -> Any:
-        '''
-        Handle the response.
-
-        :param kwargs: Additional keyword arguments.
-        :type kwargs: dict
-        :return: The response object.
-        :rtype: Any
-        '''
-
-        # Deserialize the result.
-        # Return None if the result is None.
-        return json.loads(self.result) if self.result else None
-
 
 # ** model: feature_command
 class FeatureCommand(ValueObject):
@@ -135,7 +57,6 @@ class FeatureCommand(ValueObject):
             description='Whether to pass on the error if the feature handler fails.'
         )
     )
-
 
 # ** model: feature
 class Feature(Entity):
@@ -226,7 +147,7 @@ class Feature(Entity):
             description=description,
             **kwargs
         )
-    
+
     # * method: add_command
     def add_command(self, command: FeatureCommand, position: int = None):
         '''Adds a service command to the feature.
@@ -242,3 +163,4 @@ class Feature(Entity):
             self.commands.insert(position, command)
         else:
             self.commands.append(command)
+
