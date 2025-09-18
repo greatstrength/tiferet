78a85a3243bceccb5ad5ac8d23baee740e4b248e
Ashatz <andrew@greatstrength.me>
2025-09-17 19:36:43 -0700

    Contracts - Update App Contracts

--- tiferet/contracts/app.py
+++ tiferet/contracts/app.py
@@ -9,7 +9,7 @@ from .settings import *
 # *** contracts
 
 # ** contract: app_attribute
-class AppAttribute(ModelContract):
+class AppAttributeContract(ModelContract):
     '''
     An app dependency contract that defines the dependency attributes for an app interface.
     '''
@@ -26,8 +26,8 @@ class AppAttribute(ModelContract):
     # * attribute: parameters
     parameters: Dict[str, str]
 
-# ** contract: app_interface
-class AppInterface(ModelContract):
+# ** contract: app_interface_contract
+class AppInterfaceContract(ModelContract):
     '''
     An app interface settings contract that defines the settings for an app interface.
     '''
@@ -57,7 +57,7 @@ class AppInterface(ModelContract):
     data_flag: str
 
     # * attribute: attributes
-    attributes: List[AppAttribute]
+    attributes: List[AppAttributeContract]
 
     # * attribute: constants
     constants: Dict[str, Any]
@@ -70,73 +70,26 @@ class AppRepository(Repository):
 
     # * method: get_interface
     @abstractmethod
-    def get_interface(self, app_name: str) -> AppInterface:
+    def get_interface(self, interface_id: str) -> AppInterfaceContract:
         '''
-        Get the app interface settings by name.
+        Get the app interface settings by its ID.
 
-        :param app_name: The name of the app. 
-        :type app_name: str
-        :return: The app interface.
-        :rtype: AppInterface
+        : param interface_id: The app interface ID.
+        :type interface_id: str
+        :return: The app interface settings.
+        :rtype: AppInterfaceContract
         '''
         # Not implemented.
         raise NotImplementedError('get_interface method is required for AppRepository.')
     
     # * method: list_interfaces
     @abstractmethod
-    def list_interfaces(self) -> List[AppInterface]:
+    def list_interfaces(self) -> List[AppInterfaceContract]:
         '''
         List all app inferface settings.
 
         :return: A list of app settings.
-        :rtype: List[AppInterface]
+        :rtype: List[AppInterfaceContract]
         '''
         # Not implemented.
         raise NotImplementedError('list_interfaces method is required for AppRepository.')
-
-# ** interface: app_service
-class AppService(Service):
-    '''
-    An app service is a class that is used to manage app interfaces.
-    '''
-
-    # * method: load_app_repository
-    @abstractmethod
-    def load_app_repository(self, 
-        app_repo_module_path: str,
-        app_repo_class_name: str,
-        app_repo_params: Dict[str, Any],
-        **kwargs
-    ) -> AppRepository:
-        '''
-        Execute the command.
-
-        :param app_repo_module_path: The application repository module path.
-        :type app_repo_module_path: str
-        :param app_repo_class_name: The application repository class name.
-        :type app_repo_class_name: str
-        :param app_repo_params: The application repository parameters.
-        :type app_repo_params: dict
-        :param kwargs: Additional keyword arguments.
-        :type kwargs: dict
-        :return: The application repository instance.
-        :rtype: AppRepository
-        '''
-        # Not implemented.
-        raise NotImplementedError('load_app_repository method is required for AppService.')
-    
-    # * method: load_app_instance
-    @abstractmethod
-    def load_app_instance(self, app_interface: AppInterface, default_attrs: List[AppAttribute]) -> Any:
-        '''
-        Create the app dependency injector.
-
-        :param app_interface: The app interface.
-        :type app_interface: AppInterface
-        :param default_attrs: The default configured attributes for the app.
-        :type default_attrs: List[AppAttribute]
-        :return: The app instance.
-        :rtype: Any
-        '''
-        # Not implemented.
-        raise NotImplementedError('load_app_instance method is required for AppService.')
