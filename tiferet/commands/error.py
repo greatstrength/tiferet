8a2ed34906b1d3e293543986a8122a1fa4952889
Ashatz <andrew@greatstrength.me>
2025-09-17 20:23:53 -0700

    Commands - Add Error Commands

---
+++ tiferet/commands/error.py
@@ -0,0 +1,31 @@
+# *** imports
+
+# ** app
+from ..models.error import (
+    Error
+)
+from ..contracts.error import (
+    ErrorRepository
+)
+
+class AddNewError(object):
+
+    def __init__(self, error_repo: ErrorRepository):
+        self.error_repo = error_repo
+
+    def execute(self, **kwargs) -> Error:
+
+        # Create a new error.
+        error: Error = Error.new(**kwargs)
+
+        # Assert that the error does not already exist.
+        self.verify(self.error_repo.exists(error.id), 
+            'ERROR_ALREADY_EXISTS',
+            args=(error.id,)
+        )
+
+        # Save the error.
+        self.error_repo.save(error)
+
+        # Return the new error.
+        return error
