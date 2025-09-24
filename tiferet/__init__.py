634398076143bd08052cc49afb99e13455582b2a
Andrew <andrew@greatstrength.me>
2025-09-24 15:39:07 -0700

    Tiferet - Modernize Release Deployments - 18038515290 (#323)
    * Workflows - Add Release Build
    
    * App - Add Version to Package
    
    * App - Add Pyproject Toml File
    
    * App - Remove Setup.py
    
    * App - Move Version Module
    :wq
    :q!
    * App - Add Version File
    
    * App - Remove Version from Package File
    
    * App - Edit Package Exports
    :wq
    :qa!
    :q!

--- tiferet/__init__.py
+++ tiferet/__init__.py
@@ -1,6 +1,14 @@
-# *** imports
+# *** exports
 
 # ** app
-from .contexts.app import AppManagerContext as App
-from .commands import *
-from .contracts import *
+# Export the main application context and related modules.
+# Use a try-except block to avoid import errors on build systems.
+try:
+    from .contexts.app import AppManagerContext as App
+    from .commands import *
+    from .contracts import *
+except:
+    pass
+
+# *** version
+__version__ = '1.1.2'
