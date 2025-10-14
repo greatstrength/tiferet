5fd58bc6e669a3588852e2fd4df32a2d0d5677a8
Ashatz <andrew@greatstrength.me>
2025-10-14 11:07:14 -0700

    App - Update Global Exports and Bump Version

--- tiferet/__init__.py
+++ tiferet/__init__.py
@@ -7,14 +7,26 @@
 # Use a try-except block to avoid import errors on build systems.
 try:
     from .contexts import AppManagerContext as App
-    from .models import *
+    from .models import (
+        ModelObject,
+        StringType,
+        IntegerType,
+        BooleanType,
+        FloatType,
+        ListType,
+        DictType,
+        ModelType,
+    )
     from .commands import *
-    from .contracts import *
+    from .contracts import (
+        ModelContract,
+        Repository
+    )
     from .data import *
     from .proxies import *
 except:
     pass
 
 # *** version
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
