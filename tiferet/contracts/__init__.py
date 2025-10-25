5a13260ab4a0a34d8ef2a14a8d99800958055eb0
Ashatz <andrew@greatstrength.me>
2025-10-24 19:07:36 -0700

    Contracts - Update Package Module Exports

--- tiferet/contracts/__init__.py
+++ tiferet/contracts/__init__.py
@@ -11,25 +11,31 @@ from .settings import (
 from .app import (
     AppInterface as AppInterfaceContract,
     AppAttribute as AppAttributeContract,
+    AppRepository
 )
 from .cli import (
     CliArgument as CliArgumentContract,
     CliCommand as CliCommandContract,
+    CliRepository
 )
 from .container import (
     ContainerAttribute as ContainerAttributeContract,
     FlaggedDependency as FlaggedDependencyContract,
+    ContainerRepository
 )
 from .error import (
     Error as ErrorContract,
     ErrorMessage as ErrorMessageContract,
+    ErrorRepository
 )
 from .feature import (
     Feature as FeatureContract,
     FeatureCommand as FeatureCommandContract,
+    FeatureRepository
 )
 from .logging import (
     FormatterContract,
     HandlerContract,
     LoggerContract,
+    LoggingRepository
 )
