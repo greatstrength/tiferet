6f4b449df0c3a1775c6e2d005d9b555c2deca470
Ashatz <andrew@greatstrength.me>
2025-10-26 05:30:28 -0700

    Tiferet - YAML Configuration Proxy Reformatting & Exports - Prototype

--- tiferet/data/__init__.py
+++ tiferet/data/__init__.py
@@ -8,25 +8,37 @@ from .settings import (
 )
 from .app import (
     AppAttributeYamlData,
-    AppInterfaceYamlData
+    AppAttributeYamlData as AppAttributeConfigData,
+    AppInterfaceYamlData,
+    AppInterfaceYamlData as AppInterfaceConfigData,
 )
 from .cli import (
     CliCommandYamlData,
+    CliCommandYamlData as CliCommandConfigData,
 )
 from .container import (
     FlaggedDependencyYamlData,
+    FlaggedDependencyYamlData as FlaggedDependencyConfigData,
     ContainerAttributeYamlData,
+    ContainerAttributeYamlData as ContainerAttributeConfigData,
 )
 from .error import (
     ErrorData,
+    ErrorData as ErrorConfigData,
 )
 from .feature import (
     FeatureData as FeatureYamlData,
+    FeatureData as FeatureConfigData,
     FeatureCommandData as FeatureCommandYamlData,
+    FeatureCommandData as FeatureCommandConfigData,
 )
 from .logging import (
     LoggingSettingsData,
+    LoggingSettingsData as LoggingConfigData,
     FormatterData,
+    FormatterData as FormatterConfigData,
     HandlerData,
+    HandlerData as HandlerConfigData,
     LoggerData,
+    LoggerData as LoggerConfigData,
 )
--- tiferet/proxies/yaml/settings.py
+++ tiferet/proxies/yaml/settings.py
@@ -3,57 +3,103 @@
 # *** imports
 
 # ** core
-from typing import Any
+from typing import Dict, Any, Callable
 
 # ** app
 from ...commands import raise_error
-from ...clients import yaml_client
+from ...middleware import Yaml
 
 # *** classes
 
-# ** class yaml_proxy
-class YamlConfigurationProxy(object):
+# ** class yaml_file_proxy
+class YamlFileProxy(object):
     '''
     A base class for proxies that handle YAML configuration files.
     '''
 
-    # * field: config_file
-    config_file: str = None
+    # * attribute: yaml_file
+    yaml_file: str
+
+    # * attribute: encoding
+    encoding: str
 
     # * method: init
-    def __init__(self, config_file: str):
+    def __init__(self, yaml_file: str, encoding: str = 'utf-8'):
         '''
         Initialize the proxy.
 
         :param config_file: The configuration file.
         :type config_file: str
         '''
-        self.config_file = config_file
+
+        # Verify that the configuration file is a valid YAML file.
+        if not yaml_file or (not yaml_file.endswith('.yaml') and not yaml_file.endswith('.yml')):
+            raise_error.execute(
+                'INVALID_YAML_FILE',
+                f'File {yaml_file} is not a valid YAML file.',
+                yaml_file
+            )
+
+        self.yaml_file = yaml_file
+        self.encoding = encoding
 
     # * method: load_yaml
-    def load_yaml(self, start_node: callable = lambda data: data, create_data: callable = lambda data: data) -> Any:
+    def load_yaml(self, start_node: Callable = lambda data: data, data_factory: Callable = lambda data: data) -> Any:
         '''
         Load data from the YAML configuration file.
 
-        :param start_node: The starting node in the YAML file.
-        :type start_node: str
-        :param create_data: A callable to create data objects from the loaded data.
-        :type create_data: callable
+        :param start_node: A callable to specify the starting node for loading data from the YAML file. Defaults to a lambda that returns the data as is.
+        :type start_node: Callable
+        :param data_factory: A callable to specify how to create data objects from the loaded YAML data. Defaults to a lambda that returns the data as is.
+        :type data_factory: Callable
         :return: The loaded data.
         :rtype: any
         '''
 
         # Load the YAML file using the yaml client.
         try:
-            return yaml_client.load(
-                self.config_file,
-                create_data=create_data,
-                start_node=start_node
-            )
-        except FileNotFoundError as e:
-            # If the file is not found, raise an error.
+
+            # Load the YAML data using the provided start node.
+            with Yaml(self.yaml_file, encoding=self.encoding) as yml_r:
+                yaml_data = yml_r.load_yaml(start_node=start_node)
+
+            # Return the loaded YAML data after processing it with the data factory.
+            return data_factory(yaml_data)
+
+        # Handle any exceptions that occur during YAML loading and raise a custom error.
+        except Exception as e:
             raise_error.execute(
-                'CONFIG_FILE_NOT_FOUND',
-                f'Configuration file {self.config_file} not found.',
-                self.config_file
+                'YAML_FILE_LOAD_ERROR',
+                f'An error occurred while loading the YAML file {self.yaml_file}: {str(e)}',
+                self.yaml_file,
+                str(e)
+            )
+
+    # * method: save_yaml
+    def save_yaml(self, data: Dict[str, Any], data_yaml_path: str = None):
+        '''
+        Save data to the YAML configuration file.
+
+        :param data: The data to save to the YAML file.
+        :type data: Dict[str, Any]
+        :param data_yaml_path: The path within the YAML file where the data should be saved. If None, saves to the root of the YAML file.
+        :type data_yaml_path: str
+        '''
+
+        # Save the YAML data using the yaml client.
+        try:
+            with Yaml(
+                self.yaml_file,
+                mode='w',
+                encoding=self.encoding
+            ) as yml_w:
+                yml_w.save_yaml(data=data, data_yaml_path=data_yaml_path)
+
+        # Handle any exceptions that occur during YAML saving and raise a custom error.
+        except Exception as e:
+            raise_error.execute(
+                'YAML_FILE_SAVE_ERROR',
+                f'An error occurred while saving to the YAML file {self.yaml_file}: {str(e)}',
+                self.yaml_file,
+                str(e)
             )
