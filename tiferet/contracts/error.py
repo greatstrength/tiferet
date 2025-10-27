af550c1c51a9368e3894ad669b6fbbedffdece95
Ashatz <andrew@greatstrength.me>
2025-10-27 12:18:09 -0700

    Tiferet - Read/Write Enhancements - Error Repository & YAML Proxy - Prototype

--- tiferet/contracts/error.py
+++ tiferet/contracts/error.py
@@ -162,6 +162,17 @@ class ErrorRepository(Repository):
         :type error: Error
         '''
         raise NotImplementedError('The save method must be implemented by the error repository.')
+    
+    # * method: delete
+    @abstractmethod
+    def delete(self, id: str) -> None:
+        '''
+        Delete the error by its unique identifier.
+
+        :param id: The unique identifier for the error to delete.
+        :type id: str
+        '''
+        raise NotImplementedError('The delete method must be implemented by the error repository.')
 
 # ** contract: error_service
 class ErrorService(Service):
--- tiferet/proxies/yaml/error.py
+++ tiferet/proxies/yaml/error.py
@@ -15,18 +15,18 @@ from ...commands import (
     raise_error,
     TiferetError
 )
-from ...data import ErrorData
+from ...data import DataObject, ErrorConfigData
 from ...contracts import (
     ErrorContract,
     ErrorRepository
 )
 from ...clients import yaml as yaml_client
-from .settings import YamlConfigurationProxy
+from .settings import YamlFileProxy
 
 # *** proxies
 
 # ** proxy: yaml_proxy
-class ErrorYamlProxy(ErrorRepository, YamlConfigurationProxy):
+class ErrorYamlProxy(ErrorRepository, YamlFileProxy):
     '''
     The YAML proxy for the error repository
     '''
@@ -47,7 +47,7 @@ class ErrorYamlProxy(ErrorRepository, YamlConfigurationProxy):
     def load_yaml(
             self,
             start_node: Callable = lambda data: data,
-            create_data: Callable = lambda data: data
+            data_factory: Callable = lambda data: data
         ) -> Any:
         '''
         Load data from the YAML configuration file.
@@ -63,15 +63,15 @@ class ErrorYamlProxy(ErrorRepository, YamlConfigurationProxy):
         try:
             return super().load_yaml(
                 start_node=start_node,
-                create_data=create_data
+                data_factory=data_factory
             )
 
         # Raise an error if the loading fails.
         except Exception as e:
             raise_error.execute(
                 'ERROR_CONFIG_LOADING_FAILED',
-                f'Unable to load error configuration file {self.config_file}: {e}.',
-                self.config_file,
+                f'Unable to load error configuration file {self.yaml_file}: {e}.',
+                self.yaml_file,
                 str(e)
             )
 
@@ -106,13 +106,20 @@ class ErrorYamlProxy(ErrorRepository, YamlConfigurationProxy):
         '''
 
         # Load the error data from the yaml configuration file.
-        errors: ErrorData = self.load_yaml(
-            create_data=lambda data: ErrorData.from_data(
-                id=id, **data),
-            start_node=lambda data: data.get('errors').get(id))
+        error_data = self.load_yaml(
+            start_node=lambda data: data.get('errors').get(id)
+        )
 
-        # Return the error object.
-        return errors.map() if errors else None
+        # If no data is found, return None.
+        if not error_data:
+            return None
+        
+        # Map the error data to the error object and return it.
+        return DataObject.from_data(
+            ErrorConfigData,
+            id=id,
+            **error_data
+        ).map()
 
     # * method: list
     def list(self) -> List[ErrorContract]:
@@ -124,9 +131,14 @@ class ErrorYamlProxy(ErrorRepository, YamlConfigurationProxy):
         '''
 
         # Load the error data from the yaml configuration file.
-        errors: Dict[str, ErrorData] = self.load_yaml(
-            create_data=lambda data: {id: ErrorData.from_data(
-                id=id, **error_data) for id, error_data in data.items()},
+        errors: Dict[str, ErrorConfigData] = self.load_yaml(
+            data_factory=lambda data: {
+                id: DataObject.from_data(
+                    ErrorConfigData,
+                    id=id, 
+                    **error_data
+                ) for id, error_data in data.items()
+            },
             start_node=lambda data: data.get('errors'))
 
         # Return the error object.
@@ -142,11 +154,36 @@ class ErrorYamlProxy(ErrorRepository, YamlConfigurationProxy):
         '''
 
         # Create updated error data.
-        error_data = ErrorData.from_model(ErrorData, error)
+        error_data = DataObject.from_model(
+            ErrorConfigData, 
+            error
+        )
 
         # Update the error data.
-        yaml_client.save(
-            yaml_file=self.config_file,
+        self.save_yaml(
             data=error_data.to_primitive(),
-            data_save_path=f'errors/{error.id}',
+            data_yaml_path=f'errors/{error.id}',
+        )
+
+    # * method: delete
+    def delete(self, id: str):
+        '''
+        Delete the error.
+
+        :param id: The error id.
+        :type id: str
+        '''
+
+        # Retrieve the errors data from the yaml file.
+        errors_data = self.load_yaml(
+            start_node=lambda data: data.get('errors', {})
+        )
+
+        # Pop the error data whether it exists or not.
+        errors_data.pop(id, None)
+
+        # Save the updated errors data back to the yaml file.
+        self.save_yaml(
+            data=errors_data,
+            data_yaml_path='errors'
         )
