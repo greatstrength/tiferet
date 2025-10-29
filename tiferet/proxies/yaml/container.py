b4055a414886dfc144028102081db1078c725c56
Andrew <andrew@greatstrength.me>
2025-10-29 06:02:46 -0700

    Tiferet - Read/Write Enhancements for Container Repository & YAML Proxy - 18215474721 (#354)
    * Proxies - Container - Reformatting & Updates
    
    * Contracts - Container - Add Save Methods to Repository
    
    * Proxies - Container (Tests) - Reformatting & Updates
    
    * Misc - Remove Old Test Module

--- tiferet/proxies/yaml/container.py
+++ tiferet/proxies/yaml/container.py
@@ -1,18 +1,30 @@
+"""Tiferet Container YAML Proxy"""
+
 # *** imports
 
 # ** core
-from typing import Any, List, Tuple, Dict
+from typing import (
+    Any, 
+    List,
+    Tuple,
+    Dict,
+    Callable
+)
 
 # ** app
-from .core import *
-from ...data.container import ContainerAttributeYamlData
-from ...contracts.container import ContainerRepository, ContainerAttribute
-
+from ...commands import raise_error
+from ...data import (
+    DataObject,
+    ContainerAttributeConfigData,
+    FlaggedDependencyConfigData
+)
+from ...contracts import ContainerRepository, ContainerAttributeContract
+from .settings import YamlFileProxy
 
 # *** proxies
 
 # ** proxy: container_yaml_proxy
-class ContainerYamlProxy(ContainerRepository, YamlConfigurationProxy):
+class ContainerYamlProxy(ContainerRepository, YamlFileProxy):
     '''
     Yaml proxy for container attributes.
     '''
@@ -30,13 +42,17 @@ class ContainerYamlProxy(ContainerRepository, YamlConfigurationProxy):
         super().__init__(container_config_file)
 
     # * method: load_yaml
-    def load_yaml(self, start_node: callable = lambda data: data, create_data: callable = lambda data: data) -> Any:
+    def load_yaml(
+            self, 
+            start_node: Callable = lambda data: data,
+            data_factory: Callable = lambda data: data
+        ) -> Any:
         '''
         Load data from the YAML configuration file.
         :param start_node: The starting node in the YAML file.
         :type start_node: str
-        :param create_data: A callable to create data objects from the loaded data.
-        :type create_data: callable
+        :param data_factory: A callable to create data objects from the loaded data.
+        :type data_factory: callable
         :return: The loaded data.
         :rtype: Any
         '''
@@ -45,60 +61,62 @@ class ContainerYamlProxy(ContainerRepository, YamlConfigurationProxy):
         try:
             return super().load_yaml(
                 start_node=start_node,
-                create_data=create_data
+                data_factory=data_factory
             )
-        
+
         # Raise an error if the loading fails.
-        except (Exception, TiferetError) as e:
+        except Exception as e:
             raise_error.execute(
                 'CONTAINER_CONFIG_LOADING_FAILED',
-                f'Unable to load container configuration file {self.config_file}: {e}.',
-                self.config_file,
+                f'Unable to load container configuration file {self.yaml_file}: {e}.',
+                self.yaml_file,
                 str(e)
             )
 
     # * method: get_attribute
-    def get_attribute(self, attribute_id: str) -> ContainerAttribute:
+    def get_attribute(self, attribute_id: str) -> ContainerAttributeContract:
         '''
         Get the attribute from the yaml file.
 
         :param attribute_id: The attribute id.
         :type attribute_id: str
         :return: The container attribute.
-        :rtype: ContainerAttribute
+        :rtype: ContainerAttributeContract
         '''
 
         # Load the attribute data from the yaml configuration file.
-        data = self.load_yaml(
-            create_data=lambda data: ContainerAttributeYamlData.from_data(
-                id=attribute_id, **data),
+        attribute_data = self.load_yaml(
             start_node=lambda data: data.get('attrs').get(attribute_id),
         )
 
         # If the data is None or the type does not match, return None.
-        # Remove the type logic later, as the type parameter will be removed in v2 (obsolete).
-        if data is None:
+        if attribute_data is None:
             return None
-        
+
         # Return the attribute.
-        return data.map()
+        return DataObject.from_data(
+            ContainerAttributeConfigData,
+            id=attribute_id,
+            **attribute_data
+        ).map()
 
     # * method: list_all
-    def list_all(self) -> Tuple[List[ContainerAttribute], Dict[str, str]]:
+    def list_all(self) -> Tuple[List[ContainerAttributeContract], Dict[str, str]]:
         '''
         List all the container attributes and constants.
 
         :return: The list of container attributes and constants.
-        :rtype: Tuple[List[ContainerAttribute], Dict[str, str]]
+        :rtype: Tuple[List[ContainerAttributeContract], Dict[str, str]]
         '''
 
         # Define create data function to parse the YAML file.
-        def create_data(data):
+        def data_factory(data):
             
             # Create a list of ContainerAttributeYamlData objects from the YAML data.
             attrs = [
-                ContainerAttributeYamlData.from_data(id=id, **attr_data)
-                for id, attr_data in data.get('attrs', {}).items()
+                ContainerAttributeConfigData.from_data(id=id, **attr_data)
+                for id, attr_data
+                in data.get('attrs', {}).items()
             ] if data.get('attrs') else []
 
             # Get the constants from the YAML data.
@@ -109,7 +127,7 @@ class ContainerYamlProxy(ContainerRepository, YamlConfigurationProxy):
 
         # Load the attribute data from the yaml configuration file.
         attr_data, consts = self.load_yaml(
-            create_data=create_data
+            data_factory=data_factory
         )
 
         # Return the list of container attributes.
@@ -117,3 +135,74 @@ class ContainerYamlProxy(ContainerRepository, YamlConfigurationProxy):
             [data.map() for data in attr_data],
             consts
         )
+    
+    # * method: save_attribute
+    def save_attribute(self, attribute: ContainerAttributeContract):
+        '''
+        Save the container attribute.
+
+        :param attribute: The container attribute to save.
+        :type attribute: ContainerAttributeContract
+        '''
+
+        # Create flagged dependency data from the container attribute.
+        dependencies_data = {
+            dep.flag: DataObject.from_model(
+                FlaggedDependencyConfigData,
+                dep,
+                id=dep.flag
+            ) for dep in attribute.dependencies
+        }
+
+        # Create the attribute data for the container attribute from the container attribute contract.
+        attribute_data = DataObject.from_model(
+            ContainerAttributeConfigData,
+            attribute,
+            id=attribute.id,
+            dependencies=dependencies_data
+        )
+
+        # Save the attribute data to the YAML file.
+        self.save_yaml(
+            attribute_data.to_primitive(self.default_role),
+            data_yaml_path=f'attrs/{attribute.id}'
+        )
+
+    # * method: delete_attribute
+    def delete_attribute(self, attribute_id: str):
+        '''
+        Delete the container attribute.
+
+        :param attribute_id: The attribute id.
+        :type attribute_id: str
+        '''
+
+        # Retrieve the full list of attribute data.
+        attrs_data = self.load_yaml(
+            start_node=lambda data: data.get('attrs', {})
+        )
+
+        # Pop the attribute to delete regardless of its existence.
+        attrs_data.pop(attribute_id, None)
+
+        # Save the updated attributes data back to the YAML file.
+        self.save_yaml(
+            attrs_data,
+            data_yaml_path='attrs'
+        )
+
+    # * method: save_constants
+    def save_constants(self, constants: Dict[str, str]):
+        '''
+        Save the container constants.
+
+        :param constants: The container constants to save.
+        :type constants: Dict[str, str]
+        '''
+
+        # Save the constants data to the YAML file.
+        self.save_yaml(
+            constants,
+            data_yaml_path='const'
+        )
+
--- tiferet/proxies/yaml/tests/test_container.py
+++ tiferet/proxies/yaml/tests/test_container.py
@@ -1,62 +1,82 @@
-# *** imports
+"""Tiferet Container YAML Proxy Tests Exports"""
 
-# ** core
-import os
+# *** imports
 
 # ** infra
 import pytest
+import yaml
 
 # ** app
+from ....commands import TiferetError
+from ....data import DataObject, ContainerAttributeConfigData
 from ..container import ContainerYamlProxy
-from ....configs import TiferetError
-from ....models.container import *
-
 
 # *** fixtures
 
-# ** fixture: read_config_file_path
+# ** fixture: container_config_file
 @pytest.fixture
-def read_config_file_path():
-    """Fixture to provide the path to the container configuration file."""
+def container_config_file(tmp_path) -> str:
+    '''
+    A fixture for the container configuration file path.
 
-    return 'tiferet/configs/tests/test.yml'
+    :return: The container configuration file path.
+    :rtype: str
+    '''
 
+    # Create a temporary YAML file with sample container configuration content.
+    file_path = tmp_path / 'test.yml'
 
-# ** fixture: container_attribute
-@pytest.fixture
-def container_attribute():
-    """Fixture to create a container attribute for testing."""
-    
-    return ModelObject.new(
-        ContainerAttribute,
-        id='test_container',
-        module_path='tiferet.containers.tests',
-        class_name='TestContainer',
-        dependencies=[
-            ModelObject.new(
-                FlaggedDependency,
-                module_path='tiferet.proxies.tests',
-                class_name='TestProxy',
-                flag='test',
-                parameters={'param1': 'value1'}
-            )
-        ]
-    )
+    # Write the sample container configuration to the YAML file.
+    with open(file_path, 'w', encoding='utf-8') as f:
+        yaml.safe_dump({
+            'attrs': {
+                'test_container': {
+                    'module_path': 'tiferet.containers.tests',
+                    'class_name': 'TestContainer',
+                    'dependencies': {
+                        'test': {
+                            'module_path': 'tiferet.proxies.tests',
+                            'class_name': 'TestProxy',
+                            'parameters': {
+                                'param1': 'value1'
+                            }
+                        }
+                    },
+                }
+            },
+            'const': {
+                'config_file': 'tiferet/configs/tests/test.yml'
+            }
+        }, f)
 
+    # Return the file path as a string.
+    return str(file_path)
 
 # ** fixture: container_yaml_proxy
 @pytest.fixture
-def container_yaml_proxy(read_config_file_path):
-    """Fixture to create an instance of the ContainerYamlProxy."""
-    
-    return ContainerYamlProxy(read_config_file_path)
+def container_yaml_proxy(container_config_file: str) -> ContainerYamlProxy:
+    '''
+    A fixture for the container YAML proxy.
 
+    :param read_config_file_path: The container configuration file path.
+    :type read_config_file_path: str
+    :return: The container YAML proxy.
+    :rtype: ContainerYamlProxy
+    '''
+
+    # Create and return the container YAML proxy.
+    return ContainerYamlProxy(container_config_file)
 
 # *** tests
 
 # ** test: container_yaml_proxy_load_yaml
-def test_container_yaml_proxy_load_yaml(container_yaml_proxy):
-    """Test the load_yaml method of the ContainerYamlProxy."""
+def test_container_yaml_proxy_load_yaml(container_yaml_proxy: ContainerYamlProxy):
+    '''
+    Test the load_yaml method of the ContainerYamlProxy.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    '''
 
     # Load the YAML file.
     data = container_yaml_proxy.load_yaml()
@@ -68,13 +88,17 @@ def test_container_yaml_proxy_load_yaml(container_yaml_proxy):
     assert data.get('const')
     assert isinstance(data['const'], dict)
 
-
 # ** test: container_yaml_proxy_load_yaml_file_not_found
-def test_container_yaml_proxy_load_yaml_file_not_found(container_yaml_proxy):
-    """Test the load_yaml method with a file not found error."""
+def test_container_yaml_proxy_load_yaml_file_not_found(container_yaml_proxy: ContainerYamlProxy):
+    '''
+    Test the load_yaml method with a file not found error.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    '''
 
     # Set a non-existent configuration file.
-    container_yaml_proxy.config_file = 'non_existent_file.yml'
+    container_yaml_proxy.yaml_file = 'non_existent_file.yml'
 
     # Attempt to load the YAML file.
     with pytest.raises(TiferetError) as exc_info:
@@ -84,18 +108,25 @@ def test_container_yaml_proxy_load_yaml_file_not_found(container_yaml_proxy):
     assert exc_info.value.error_code == 'CONTAINER_CONFIG_LOADING_FAILED'
     assert 'Unable to load container configuration file' in str(exc_info.value)
 
-
 # ** test: container_yaml_proxy_list_all_empty
-def test_container_yaml_proxy_list_all_empty(container_yaml_proxy):
+def test_container_yaml_proxy_list_all_empty(container_yaml_proxy: ContainerYamlProxy, tmp_path):
+    '''
+    Test the list_all method with an empty configuration.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    :param tmp_path: The temporary path fixture.
+    :type tmp_path: pathlib.Path
+    '''
 
     # Create new config file with no container attributess.
-    file_path = 'tiferet/configs/tests/test_empty.yml'
+    file_path = tmp_path / 'test_empty.yml'
     with open(file_path, 'w') as f:
         f.write('attrs:\n')
         f.write('const:\n')
 
     # Replace the config file path in the proxy.
-    container_yaml_proxy.config_file = file_path
+    container_yaml_proxy.yaml_file = file_path
 
     # List all the container attributes.
     container_attributes, constants = container_yaml_proxy.list_all()
@@ -104,12 +135,18 @@ def test_container_yaml_proxy_list_all_empty(container_yaml_proxy):
     assert container_attributes == []
     assert constants == {}
 
-    # Clean up the empty config file.
-    os.remove(file_path)
-
-
 # ** test: container_yaml_proxy_list_all
-def test_container_yaml_proxy_list_all(container_yaml_proxy, container_attribute):
+def test_container_yaml_proxy_list_all(
+        container_yaml_proxy: ContainerYamlProxy
+    ):
+    '''
+    Test the list_all method of the ContainerYamlProxy.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    :param container_attribute: The container attribute fixture.
+    :type container_attribute: ContainerAttribute
+    '''
 
     # List all the container attributes.
     container_attributes, constants = container_yaml_proxy.list_all()
@@ -120,35 +157,157 @@ def test_container_yaml_proxy_list_all(container_yaml_proxy, container_attribute
     # Check the container attributes.
     assert container_attributes
     assert len(container_attributes) == 1
-    assert container_attributes[0].id == container_attribute.id
-    assert container_attributes[0].module_path == container_attribute.module_path
-    assert container_attributes[0].class_name == container_attribute.class_name
-    assert len(container_attributes[0].dependencies) == len(container_attribute.dependencies)
+    assert container_attributes[0].id == 'test_container'
+    assert container_attributes[0].module_path == 'tiferet.containers.tests'
+    assert container_attributes[0].class_name == 'TestContainer'
+    assert len(container_attributes[0].dependencies) == 1
+
 
     # Check the dependencies.
-    for i, dependency in enumerate(container_attributes[0].dependencies):
-        assert dependency.module_path == container_attribute.dependencies[i].module_path
-        assert dependency.class_name == container_attribute.dependencies[i].class_name
-        assert dependency.flag == container_attribute.dependencies[i].flag
-        assert dependency.parameters == container_attribute.dependencies[i].parameters
-
+    dependency = container_attributes[0].dependencies[0]
+    assert dependency.module_path == 'tiferet.proxies.tests'
+    assert dependency.class_name == 'TestProxy'
+    assert dependency.flag == 'test'
+    assert dependency.parameters == {'param1': 'value1'}
 
 # ** test: container_yaml_proxy_get_attribute
-def test_container_yaml_proxy_get_attribute(container_yaml_proxy, container_attribute):
+def test_container_yaml_proxy_get_attribute(
+        container_yaml_proxy: ContainerYamlProxy,
+    ):
+    '''
+    Test the get_attribute method of the ContainerYamlProxy.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    :param container_attribute: The container attribute fixture.
+    :type container_attribute: ContainerAttribute
+    '''
 
     # Get the container attribute.
-    test_container_attribute = container_yaml_proxy.get_attribute(container_attribute.id)
+    test_container_attribute = container_yaml_proxy.get_attribute('test_container')
 
     # Check the container attribute.
     assert test_container_attribute
-    assert test_container_attribute.id == container_attribute.id
-    assert test_container_attribute.module_path == container_attribute.module_path
-    assert test_container_attribute.class_name == container_attribute.class_name
-    assert len(test_container_attribute.dependencies) == len(container_attribute.dependencies)
+    assert test_container_attribute.id == 'test_container'
+    assert test_container_attribute.module_path == 'tiferet.containers.tests'
+    assert test_container_attribute.class_name == 'TestContainer'
+    assert len(test_container_attribute.dependencies) == 1
 
     # Check the dependencies.
-    for i, dependency in enumerate(test_container_attribute.dependencies):
-        assert dependency.module_path == container_attribute.dependencies[i].module_path
-        assert dependency.class_name == container_attribute.dependencies[i].class_name
-        assert dependency.flag == container_attribute.dependencies[i].flag
-        assert dependency.parameters == container_attribute.dependencies[i].parameters
+    dependency = test_container_attribute.dependencies[0]
+    assert dependency.module_path == 'tiferet.proxies.tests'
+    assert dependency.class_name == 'TestProxy'
+    assert dependency.flag == 'test'
+    assert dependency.parameters == {'param1': 'value1'}
+
+# ** test: container_yaml_proxy_get_attribute_not_found
+def test_container_yaml_proxy_get_attribute_not_found(
+        container_yaml_proxy: ContainerYamlProxy,
+    ):
+    '''
+    Test the get_attribute method of the ContainerYamlProxy for a non-existent attribute.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    '''
+
+    # Get the container attribute.
+    test_container_attribute = container_yaml_proxy.get_attribute('not_found')
+
+    # Check the container attribute.
+    assert not test_container_attribute
+
+# ** test: container_yaml_proxy_save_attribute
+def test_container_yaml_proxy_save_attribute(
+        container_yaml_proxy: ContainerYamlProxy,
+    ):
+    '''
+    Test the save_attribute method of the ContainerYamlProxy.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    '''
+
+    # Create a new container attribute.
+    new_attribute = DataObject.from_data(
+        ContainerAttributeConfigData,
+        id='new_container',
+        module_path='tiferet.containers.new',
+        class_name='NewContainer',
+        deps={
+            'new_flag': {
+                'module_path': 'tiferet.proxies.new',
+                'class_name': 'NewProxy',
+                'parameters': {
+                    'paramA': 'valueA'
+                }
+            }
+        }
+    ).map()
+
+    # Save the updated container attribute.
+    container_yaml_proxy.save_attribute(new_attribute)
+
+    # Retrieve the saved container attribute.
+    saved_attribute = container_yaml_proxy.get_attribute('new_container')
+
+    # Check the saved container attribute.
+    assert saved_attribute
+    assert saved_attribute.id == 'new_container'
+    assert saved_attribute.module_path == 'tiferet.containers.new'
+    assert saved_attribute.class_name == 'NewContainer'
+    assert len(saved_attribute.dependencies) == 1
+
+    # Check the dependencies.
+    dependency = saved_attribute.dependencies[0]
+    assert dependency.module_path == 'tiferet.proxies.new'
+    assert dependency.class_name == 'NewProxy'
+    assert dependency.flag == 'new_flag'
+    assert dependency.parameters == {'paramA': 'valueA'}
+
+# ** test: container_yaml_proxy_delete_attribute
+def test_container_yaml_proxy_delete_attribute(
+        container_yaml_proxy: ContainerYamlProxy,
+    ):
+    '''
+    Test the delete_attribute method of the ContainerYamlProxy.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    '''
+
+    # Delete the existing container attribute.
+    container_yaml_proxy.delete_attribute('test_container')
+
+    # Attempt to retrieve the deleted container attribute.
+    deleted_attribute = container_yaml_proxy.get_attribute('test_container')
+
+    # Check that the container attribute is None.
+    assert not deleted_attribute
+
+# ** test: container_yaml_proxy_save_constants
+def test_container_yaml_proxy_save_constants(
+        container_yaml_proxy: ContainerYamlProxy,
+    ):
+    '''
+    Test the save_constants method of the ContainerYamlProxy.
+
+    :param container_yaml_proxy: The container YAML proxy.
+    :type container_yaml_proxy: ContainerYamlProxy
+    '''
+
+    # Define new constants to save.
+    new_constants = {
+        'new_const1': 'value1',
+        'new_const2': 'value2'
+    }
+
+    # Save the new constants.
+    container_yaml_proxy.save_constants(new_constants)
+
+    # List all the container attributes to retrieve the updated constants.
+    _, constants = container_yaml_proxy.list_all()
+
+    # Check the saved constants.
+    assert constants.get('new_const1') == 'value1'
+    assert constants.get('new_const2') == 'value2'
