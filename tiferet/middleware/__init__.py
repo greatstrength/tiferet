68e3554b2661355bcdda8f14938ebba816f69ec5
Ashatz <andrew@greatstrength.me>
2025-11-19 06:52:55 -0700

    Middleware - Add JSON Loader to Package Export Module

--- tiferet/middleware/__init__.py
+++ tiferet/middleware/__init__.py
@@ -5,3 +5,4 @@
 # ** app
 from .file import FileLoaderMiddleware, FileLoaderMiddleware as File
 from .yaml import YamlLoaderMiddleware, YamlLoaderMiddleware as Yaml
+from .json import JsonLoaderMiddleware, JsonLoaderMiddleware as Json
