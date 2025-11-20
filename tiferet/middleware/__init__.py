e073fe9c26b929dd26f74b093429b4c24463fbaf
Andrew <andrew@greatstrength.me>
2025-11-20 09:47:10 -0700

    Tiferet - Add CSV File Loader Middleware & Tests - 18286618051 (#373)
    * Middleware - CSV - Add File Loader
    
    * Middleware - CSV - Add Tests for File Loader
    
    * Middleware - Add CSV File Loaders to Package Export Module

--- tiferet/middleware/__init__.py
+++ tiferet/middleware/__init__.py
@@ -6,3 +6,9 @@
 from .file import FileLoaderMiddleware, FileLoaderMiddleware as File
 from .yaml import YamlLoaderMiddleware, YamlLoaderMiddleware as Yaml
 from .json import JsonLoaderMiddleware, JsonLoaderMiddleware as Json
+from .csv import (
+    CsvLoaderMiddleware, 
+    CsvLoaderMiddleware as Csv,
+    CsvDictLoaderMiddleware,
+    CsvDictLoaderMiddleware as CsvDict
+)
