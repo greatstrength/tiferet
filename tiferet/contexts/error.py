8208413b84429c80ae35054b6fe809b7e408206b
Andrew <andrew@greatstrength.me>
2025-12-18 11:15:21 -0700

    Contexts - Error - Initial Update Pre-testing

--- tiferet/contexts/error.py
+++ tiferet/contexts/error.py
@@ -1,7 +1,7 @@
 # *** imports
 
 # ** core
-from typing import Any, List
+from typing import Any, Callable
 
 # ** app
 from .cache import CacheContext
@@ -12,9 +12,8 @@ from ..assets import (
     DEFAULT_ERRORS
 )
 from ..configs.error import ERRORS
-from ..handlers.error import ErrorService
-from ..models.error import Error, ModelObject
-from ..commands import raise_error
+from ..models.error import Error
+from ..commands.error import GetError
 from ..configs import TiferetError as LegacyTiferetError
 
 
@@ -27,13 +26,13 @@ class ErrorContext(object):
     '''
 
     # * attribute: error_service
-    error_service: ErrorService
+    get_error_handler: Callable
 
     # * attribute: cache
     cache: CacheContext
 
     # * method: init
-    def __init__(self, error_service: ErrorService, cache: CacheContext = None):
+    def __init__(self, get_error_cmd: GetError, cache: CacheContext = None):
         '''
         Initialize the error context.
 
@@ -44,11 +43,15 @@ class ErrorContext(object):
         '''
 
         # Assign the attributes.
-        self.error_service = error_service
+        self.get_error_handler = get_error_cmd.execute
         self.cache = cache if cache else CacheContext()
 
+        # Load errors into the cache.
+        # - obsolete with on-demand loading.
+        self.load_errors()
+
     # * method: load_errors
-    def load_errors(self) -> List[Error]:
+    def load_errors(self):
         '''
         Load errors from the error service.
 
@@ -56,16 +59,18 @@ class ErrorContext(object):
         :rtype: List[Error]
         '''
 
-        # Convert configured errors to a list of error objects.
-        configured_errors = [
-            ModelObject.new(
-                Error,
-                **error_data
-        ) for error_data in ERRORS]
+        # Load temporary configured errors and add them to the cache.
+        configured_errors = [Error.new(**error_data) for error_data in ERRORS]
 
-        # Load errors from the error service, including any configured errors.
-        return self.error_service.load_errors(configured_errors)
+        # Add the configured errors to the cache.
+        error_map: dict = self.cache.get('error_map')
 
+        # Add configured errors to the error map if not already present.
+        if not error_map:
+            error_map = {error.id: error for error in configured_errors}
+
+        # Update the cache with the error map.
+        self.cache.set('error_map', error_map)
     
     # * method: get_error_by_code
     def get_error_by_code(self, error_code: str) -> Error:
@@ -79,42 +84,33 @@ class ErrorContext(object):
         '''
 
         # Check to see if there is an error map in the cache.
-        error_map = self.cache.get('error_map')
+        error_map: dict = self.cache.get('error_map')
 
-        # If there is no error map or the error code does not exist in the map,
-        # we need to load the errors from the error service and create a new error map.
-        # This is to ensure that we always have the latest errors available.
-        if not error_map or not error_map.get(error_code):
-            errors = self.load_errors()
-            error_map = {error.error_code: error for error in errors}
-            self.cache.set('error_map', error_map)
+        # Pull the error code from the error map if it exists.
+        error = error_map.get(error_code)
 
-        # Look for the error in the default errors if it was not found in the map.
-        if not error_map or error_code not in error_map:
-            error_data = DEFAULT_ERRORS.get(error_code, None)
+        # If the error does not exist in the map, attempt to load it using the get_error_handler.
+        if not error:
+            error = self.get_error_handler(error_code, include_defaults=True)
 
-        # Return the error if it was found in the default errors.
-        if error_data:
-            return Error.new(**error_data)
-        
-        # Raise an error if the default error does not exist.
-        if not error_data:
-            
-            # Retrieve the "error not found" error to use its details.
-            not_found_error = Error.new(
-                **DEFAULT_ERRORS.get(ERROR_NOT_FOUND_ID)
-            )
-            
-            # Raise a Tiferet API Error.
+        # If the error was not found, raise an API error with the error not found details.
+        if not error:
+
+            # Retrieve and raise the "error not found" error to use its details.
+            error = Error.new(**DEFAULT_ERRORS.get(ERROR_NOT_FOUND_ID))
             raise TiferetAPIError(
                 error_code=ERROR_NOT_FOUND_ID,
-                name=not_found_error.name,
-                message=not_found_error.format_message(
+                name=error.name,
+                message=error.format_message(
                     'en_US',
                     id=error_code
                 ),
                 id=error_code
             )
+        
+        # Add the error to the error map and update the cache.
+        error_map[error_code] = error
+        self.cache.set('error_map', error_map)
 
         # Return the error object from the error map.
         return error_map.get(error_code)
