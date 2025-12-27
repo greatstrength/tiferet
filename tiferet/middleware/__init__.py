f4bcd8a41968fbbb3dabffb0932f9a344042d17f
Andrew <andrew@greatstrength.me>
2025-12-27 11:35:23 -0700

    Middleware - Add TiferetError & App Constants from Commands

--- tiferet/middleware/__init__.py
+++ tiferet/middleware/__init__.py
@@ -3,6 +3,7 @@
 # *** exports
 
 # ** app
+from ..commands import TiferetError, const
 from .file import FileLoaderMiddleware, FileLoaderMiddleware as File
 from .yaml import YamlLoaderMiddleware, YamlLoaderMiddleware as Yaml
 from .json import JsonLoaderMiddleware, JsonLoaderMiddleware as Json
