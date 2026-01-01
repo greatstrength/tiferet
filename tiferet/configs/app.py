cc11c6945217acdeb6ec0408519bc84d212f2da1
Andrew <andrew@greatstrength.me>
2025-12-31 22:25:04 -0700

    Enhancement - Remove Container Handler from Container Context (#409)
    * Assets - Constants - Update App Dependency Attributes
    
    * Context - Container - Update Context & Tests
    
    * Handlers - Container - Remove Container Handler
    
    * App - Update Tests & Reinstitute Integration Tests

--- tiferet/configs/app.py
+++ tiferet/configs/app.py
@@ -2,14 +2,14 @@
 
 # ** config: default_attributes
 DEFAULT_ATTRIBUTES = [
-    dict(
-        attribute_id='container_repo',
-        module_path='tiferet.proxies.yaml.container',
-        class_name='ContainerYamlProxy',
-        parameters=dict(
-            container_config_file='app/configs/container.yml'
-        )
-    ),
+    {
+        'attribute_id': 'container_service',
+        'module_path': 'tiferet.repos.config.container',
+        'class_name': 'ContainerConfigurationRepository',
+        'parameters': {
+            'container_config_file': 'app/configs/container.yml'
+        }
+    },
     dict(
         attribute_id='feature_repo',
         module_path='tiferet.proxies.yaml.feature',
@@ -18,14 +18,14 @@ DEFAULT_ATTRIBUTES = [
             feature_config_file='app/configs/feature.yml'
         )
     ),
-    dict(
-        attribute_id='error_service',
-        module_path='tiferet.repos.config.error',
-        class_name='ErrorConfigurationRepository',
-        parameters=dict(
-            error_config_file='app/configs/error.yml'
-        )
-    ),
+    {
+        'attribute_id': 'error_service',
+        'module_path': 'tiferet.repos.config.error',
+        'class_name': 'ErrorConfigurationRepository',
+        'parameters': {
+            'error_config_file': 'app/configs/error.yml'
+        }
+    },
     dict(
         attribute_id='logging_repo',
         module_path='tiferet.proxies.yaml.logging',
@@ -33,11 +33,6 @@ DEFAULT_ATTRIBUTES = [
         parameters=dict(
             logging_config_file='app/configs/logging.yml'
         )
-    ),
-    dict(
-        attribute_id='container_service',
-        module_path='tiferet.handlers.container',
-        class_name='ContainerHandler',
     ),
     dict(
         attribute_id='feature_service',
@@ -49,26 +44,31 @@ DEFAULT_ATTRIBUTES = [
         module_path='tiferet.commands.error',
         class_name='GetError',
     ),
+    {
+        'attribute_id': 'container_list_all_cmd',
+        'module_path': 'tiferet.commands.container',
+        'class_name': 'ListAllSettings',
+    },
     dict(
         attribute_id='logging_service',
         module_path='tiferet.handlers.logging',
         class_name='LoggingHandler',
     ),
-    dict(
-        attribute_id='container',
-        module_path='tiferet.contexts.container',
-        class_name='ContainerContext',
-    ),
+    {
+        'attribute_id': 'container',
+        'module_path': 'tiferet.contexts.container',
+        'class_name': 'ContainerContext',
+    },
     dict(
         attribute_id='features',
         module_path='tiferet.contexts.feature',
         class_name='FeatureContext',
     ),
-    dict(
-        attribute_id='errors',
-        module_path='tiferet.contexts.error',
-        class_name='ErrorContext',
-    ),
+    {
+        'attribute_id': 'errors',
+        'module_path': 'tiferet.contexts.error',
+        'class_name': 'ErrorContext',
+    },
     dict(
         attribute_id='logging',
         module_path='tiferet.contexts.logging',
